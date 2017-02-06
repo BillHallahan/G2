97ec6b16c3f369613d93d5708bd5f216981b40f8
Bill Hallahan <billhallahan@juno.com>
2017-02-06 18:57:39 -0500

    Shortened lines

--- src/Core/Utils.hs
+++ src/Core/Utils.hs
@@ -7,8 +7,11 @@ import qualified Data.List as L
 import qualified Data.Map as M
 
 mkStateStr :: State -> String
-mkStateStr (tv, ev, expr, pc) = L.intercalate "\n\n" ["Type Env: " ++ ts, "Expr Env: " ++ es, "Curr Expr: " ++ xs, "Path Constraints: " ++ ps]
-  where ts = mkTypeEnvStr tv--show tv
+mkStateStr (tv, ev, expr, pc) = L.intercalate "\n\n" li
+  where 
+        li = ["Type Env: " ++ ts, "Expr Env: " ++ es
+            , "Curr Expr: " ++ xs, "Path Constraints: " ++ ps]
+        ts = mkTypeEnvStr tv--show tv
         es = mkExprEnvStr ev
         xs = mkExprStr expr
         ps = show pc
@@ -36,16 +39,31 @@ mkExprStr e = mkExprStr' e 0
     where
         mkExprStr' :: Expr -> Int -> String
         mkExprStr' (Var n t) i = off i ++ "Var " ++ n ++ " (" ++ mkTypeStr t (i + 1) ++")"
-        mkExprStr' l@(Lam n e t) i = off i ++  "Lam (" ++ show n ++ "\n" ++ mkExprStr' e (i + 1) ++ " " ++ mkTypeStr t i ++")"
-        mkExprStr' a@(App e1 e2) i = off i ++ "App (\n" ++ mkExprStr' e1 (i + 1) ++ "\n" ++ mkExprStr' e2 (i + 1) ++ "\n" ++ off i ++ ")" 
-        mkExprStr' c@(Case e1 ae t) i = off i ++ "Case (\n" ++ mkExprStr' e1 (i + 1) ++ "\n" ++ mkAltExprStr ae (i + 1) ++ " " ++ mkTypeStr t i ++ ")"
+        mkExprStr' (Lam n e t) i = 
+            let
+                e' = mkExprStr' e (i + 1)
+            in
+            off i ++  "Lam (" ++ show n ++ "\n" ++ e' ++ " " ++ mkTypeStr t i ++")"
+        mkExprStr' (App e1 e2) i = 
+            let
+                e1' = mkExprStr' e1 (i + 1)
+                e2' = mkExprStr' e2 (i + 1)
+            in
+            off i ++ "App (\n" ++ e1' ++ "\n" ++ e2' ++ "\n" ++ off i ++ ")"
+        mkExprStr' (Case e1 ae t) i = 
+            let
+                e1' = mkExprStr' e1 (i + 1)
+                ae' = mkAltExprStr ae (i + 1)
+            in
+            off i ++ "Case (\n" ++ e1' ++ "\n" ++ ae' ++ " " ++ mkTypeStr t i ++ ")"
         mkExprStr' (Type t) i = off i ++ "Type (" ++ mkTypeStr t (i + 1) ++ ")"
         mkExprStr' x i = off i ++ show x
 
 
         mkAltExprStr :: [(Alt, Expr)] -> Int -> String
         mkAltExprStr [] i = ""
-        mkAltExprStr ((a, e):xs) i = off i ++ "(" ++ show a ++ ",\n" ++ mkExprStr' e (i + 1) ++ ")\n" ++ mkAltExprStr xs i
+        mkAltExprStr ((a, e):xs) i = off i ++ "(" ++ show a ++ ",\n" ++ 
+                                        mkExprStr' e (i + 1) ++ ")\n" ++ mkAltExprStr xs i
 
         off :: Int -> String
         off i = duplicate "   " i
@@ -59,11 +77,14 @@ mkTypeStr t i = mkTypeStr' t i False
         mkTypeStr' (TyConApp n tx) i b = 
             let li = L.intercalate ", " . map (\t' -> mkTypeStr' t' (i + 1) b) $ tx in
                 off i b ++ "TyConApp " ++ show n ++ " [" ++ li ++ "]"
-        mkTypeStr' (TyForAll n t) i b = off i b ++ "TyForAll " ++ show n ++ "(" ++ mkTypeStr' t (i + 1) b ++ ")"
+        mkTypeStr' (TyForAll n t) i b = off i b ++ "TyForAll " ++ show n ++
+                                        "(" ++ mkTypeStr' t (i + 1) b ++ ")"
         mkTypeStr' t i b = (if b then " " else "") ++ show t
 
         tPat :: Type -> Type -> String -> Int -> Bool -> String
-        tPat t1 t2 s i b= off i b ++ s ++ " (" ++ mkTypeStr' t1 (i + 1) True ++ mkTypeStr' t2 (i + 1) True ++ off i True ++  ")"
+        tPat t1 t2 s i b = off i b ++ s ++ " (" 
+                            ++ mkTypeStr' t1 (i + 1) True 
+                            ++ mkTypeStr' t2 (i + 1) True ++ off i True ++  ")"
 
         off :: Int -> Bool -> String
         off i b = if b then "\n" ++ duplicate "   " i else ""
