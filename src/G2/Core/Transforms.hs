df22e8172dd25de41507a60075ff6eb03a6262af
Anton Xue <antonxue1@gmail.com>
2017-05-21 14:56:16 -0400

    Partial renaming done for things lke Lam. TODO: Case

--- src/G2/Core/Transforms.hs
+++ src/G2/Core/Transforms.hs
@@ -102,4 +102,87 @@ freshSeededNameList seeds state = fst $ foldl freshAndBind ([], state) seeds
                                     in (acc', st')
 -- | Rename
 --   Rename all variables of form (Var n) with (Var n').
+rename :: Name -> Name -> State -> State
+rename old new state = case curr_expr state of
+    -- If it matches, we update, and add an entry to the symbolic link table.
+    Var n t -> let sym_links' = M.insert new (old,t,Nothing) (sym_links state) 
+               in if n == old
+                 then state { curr_expr = Var new t, sym_links = sym_links'}
+                 else state
+
+    -- Stop adding things if the lambda's binder shadows the old name, as the
+    -- transformation would be completely redundant otherwise. However, if the
+    -- new name is equal to the current lambda binder, we need to completely
+    -- replace the lambda expression first before proceeding.
+    --
+    -- To do this, we first make a completely different b as b', which makes it
+    -- so that b == new, but b' /= new. Following this we rename all instances
+    -- of b to b' within the lambda's body. Then we are safe :)
+    Lam b e t -> if old == b
+                   then state
+                   else if new == b
+                     then let b' = freshSeededName b state  -- b' /= new
+                              state' = rename b b' (state {curr_expr = e})
+                              e' = curr_expr state'
+                              sym_links' = sym_links state'
+                          in rename old new (state' { curr_expr = Lam b' e' t
+                                                    , sym_links = sym_links' })
+                     else let state' = rename old new (state {curr_expr = e})
+                          in state { curr_expr = Lam b (curr_expr state') t
+                                   , sym_links = sym_links state' }
+
+    -- This is more straightforward than Lam. Branch and union the sym links.
+    App f a -> let lhs = rename old new (state {curr_expr = f})
+                   rhs = rename old new (state {curr_expr = a})
+               in state { curr_expr = App (curr_expr lhs) (curr_expr rhs)
+                        , sym_links = M.union (sym_links lhs) (sym_links rhs) }
+
+    -- This is annoying and complicated. For each alt, we do the same check as
+    -- we did for the lambda case, since Alts are semantically equivalent to
+    -- multi-argument function applications. Thankfully there was foresight to
+    -- write a exprBindList function :)
+    --
+    -- First we apply a rename on the matching expression.
+    --
+    -- Then we have a function that makes a new renamed state for each Alt. We
+    -- then of course need to map this function over all the Alts. Internally,
+    -- this function examines whehter or not the old name exists in the params
+    -- of the data constructor. As with in the Lam case, if the old is, we just
+    -- return the state as is. If it is not, and the new is instead one of the
+    -- params, we must perform a renaming of all (or just the one) of the names
+    -- within the rest of the alt expression. This guarantees lack of conflict
+    -- and allows us to recursively call rename again once we have "adjusted"
+    -- the current state accordingly.
+    --
+    -- Finally, we must merge together the symbolic links 
+    {-
+    Case m as t ->
+        let m_state = rename old new (state {curr_expr = m})
+            altState ((dc, params), aexp) =
+              if old `elem` params
+                then state {curr_expr = aexp}
+                else if new `elem` params
+                  then let params' = freshSeededNameList params state
+                           zipd = zip params params'
+                           ae_st' = renameList zipd (state {curr_expr = aexp})
+                           aexp' = curr_expr ae_st'
+                           sym_links' = sym_links ae_st'
+                       in rename old new (ae_st' { curr_expr = aexp'
+                                                 , sym_links = sym_links' })
+                  else rename old new (state {curr_expr = aexp})
+            alt_states = map altState as
+            alt_state_alts = map (\(((dc, params),_), a_state) ->
+                                    ((dc, params), curr_expr a_state))
+                                 (zip as alt_states)
+            combslts slts = foldl M.union M.empty slts  -- Technically a bug.
+            slts = combslts $ [sym_links m_state] ++ (map sym_links alt_states)
+        in state { curr_expr = Case (curr_expr m_state) alt_state_alts t
+                 , sym_links = slts }
+    -}
+    _ -> state
+
+-- | Rename List
+--   Rename, but with a list instead.
+renameList :: [(Name, Name)] -> State -> State
+renameList = undefined
 
