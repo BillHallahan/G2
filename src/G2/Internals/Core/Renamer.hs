cc11a19d48140cf45b4f80dc4d32b53176331f5a
Anton Xue <antonxue1@gmail.com>
2017-06-17 03:21:49 -0400

    Refactored renaming

--- src/G2/Internals/Core/Renamer.hs
+++ src/G2/Internals/Core/Renamer.hs
@@ -4,7 +4,12 @@
 --   all "fresh" -- that is, it is possible for lambda binders to have a
 --   conflicting name with that of the current environment bindings. Thus, we
 --   must be careful to avoid accidentally capturing free variables.
-module G2.Internals.Core.Renamer where
+module G2.Internals.Core.Renamer
+    ( allNames
+    , freshSeededName
+    , freshSeededNameList
+    , rename
+    , renameList          ) where
 
 import G2.Internals.Core.AST
 import G2.Internals.Core.Environment
@@ -65,7 +70,113 @@ freshSeededNameList (n:ns) s = n':freshSeededNameList ns s'
   where n' = freshSeededName n s
         s' = bindExpr n' BAD s  -- Conflict
 
--- | Rename
+-- | Join Symbolic Link Tables
+-- Can only safely join them if their new entries are disjoint!!
+joinSLTs :: [SymLinkTable] -> SymLinkTable
+joinSLTs slts = foldl M.union M.empty slts
+ 
+-- | Rename Var
+-- If it matches, we update, and add an entry to the symbolic link table.
+renameVar :: Name -> Name -> State -> State
+renameVar old new state = if n == old
+    then state { curr_expr = Var new t, sym_links = slt' }
+    else state
+  where Var n t = curr_expr state
+        slt' = M.insert new (old, t, Nothing) (sym_links state) 
+
+-- | Rename Lam
+-- Must check for cases where the lambda's binder might be old or new.
+renameLam :: Name -> Name -> State -> State
+renameLam old new state = if b == old
+    then state  -- Shadowing occurs
+    else e_state {curr_expr = Lam b (curr_expr e_state) t}
+  where Lam b e t = curr_expr state
+        e_state = rename old new (state {curr_expr = e})
+
+-- | Rename Let
+renameLet :: Name -> Name -> State -> State
+renameLet old new state = if old `elem` b_lhs
+    then state  -- Shadowing occurs.
+    else e_state { curr_expr = cexpr', sym_links = slt' }
+  where Let bs e = curr_expr state
+        b_lhs = map fst bs
+        b_rhs = map snd bs
+        e_state = rename old new (state {curr_expr = e})
+        b_sts = tail $ foldl rawFoldFunRHS [("_", e_state)] bs
+        slt'  = joinSLTs $ [sym_links e_state] ++ map (sym_links . snd) b_sts
+        b_norm = map (\(n, s) -> (n, curr_expr s)) b_sts
+        cexpr' = Let b_norm (curr_expr e_state)
+
+        -- When we bind let statements, we have to be aware of potentially
+        -- changing symbolic link tables, which is why we fold over states
+        -- in order to ensure disjointness when renaming. Only call if old
+        -- and new both do not appear in lhs! Super important!! >:(
+        rawFoldFunRHS :: [(Name, State)] -> (Name, Expr) -> [(Name, State)]
+        rawFoldFunRHS acc (n, bexp) =
+            let acc_alls = map fst acc ++ (concatMap allNames (map snd acc))
+                c_env = M.fromList $ zip acc_alls (repeat BAD)
+                eenv' = M.union (expr_env state) c_env
+                b_st  = state { expr_env = eenv', curr_expr = bexp }
+            in acc ++ [(n, rename old new b_st)]
+
+-- | Rename App
+renameApp :: Name -> Name -> State -> State
+renameApp old new state = state_a {curr_expr = App f' a'}
+  where App f a = curr_expr state
+        state_f = rename old new (state {curr_expr = f})
+        state_a = rename old new (state_f {curr_expr = a})
+        f' = curr_expr state_f
+        a' = curr_expr state_a
+
+-- | Rename Case
+renameCase :: Name -> Name -> State -> State
+renameCase old new state = state { curr_expr = cexpr', sym_links = slt' }
+  where Case m as t = curr_expr state
+        -- Renaming the matching expression while we can.
+        m_state = rename old new (state {curr_expr = m})
+        -- Create the alt states that we can use for merging later. Make
+        -- sure to include the m_state, since that may also have new slts!
+        -- We take the tail because we need to zip it later. Very precise!
+        alt_states = tail $ foldl altState [m_state] as
+        -- Extraction.
+        alt_state_alts = map (\((Alt (dc, params), _), a_state) ->
+                                  (Alt (dc, params), curr_expr a_state))
+                             (zip as alt_states)
+        -- Join them all together.
+        slt' = joinSLTs $ [sym_links m_state] ++ (map sym_links alt_states)
+        cexpr' = Case (curr_expr m_state) alt_state_alts t
+
+        -- Function that allows us to express each Alt as a separate State
+        -- that we can then later merge. We keep a list of previous States
+        -- from folding so that we can use them to make conflict lists.
+        altState :: [State] -> (Alt, Expr) -> [State]
+        altState acc (Alt (dc, params), aexp) =
+          let acc_alls = concatMap allNames acc  -- Super slow :)
+              c_env = M.fromList $ zip acc_alls (repeat BAD)
+              eenv' = M.union (expr_env state) c_env
+              alt_st = state { expr_env = eenv', curr_expr = aexp }
+          in if old `elem` params
+              then acc ++ [alt_st]
+              else acc ++ [rename old new alt_st]
+
+-- | Rename Assume
+renameAssume :: Name -> Name -> State -> State
+renameAssume old new state = state_e {curr_expr = Assume c' e'}
+  where Assume c e = curr_expr state
+        state_c = rename old new (state {curr_expr = c})
+        state_e = rename old new (state_c {curr_expr = e})
+        c' = curr_expr state_c
+        e' = curr_expr state_e
+
+-- | Rename Assert
+renameAssert :: Name -> Name -> State -> State
+renameAssert old new state = state_e {curr_expr = Assert c' e'}
+  where Assert c e = curr_expr state
+        state_c = rename old new (state {curr_expr = c})
+        state_e = rename old new (state_c {curr_expr = e})
+        c' = curr_expr state_c
+        e' = curr_expr state_e
+
 --   Rename all variables of form (Var n) with (Var n'). We make the assumption
 --   that the new name passed in is fresh with respect to the current state.
 --   This allows us to prevent checking to see if the new name happens to equal
@@ -73,111 +184,14 @@ freshSeededNameList (n:ns) s = n':freshSeededNameList ns s'
 --   effort of having to perform sub-expression renaming.
 rename :: Name -> Name -> State -> State
 rename old new state = case curr_expr state of
-  -- If it matches, we update, and add an entry to the symbolic link table.
-  Var n t ->
-      let sym_links' = M.insert new (old,t,Nothing) (sym_links state) 
-      in if n == old
-          then state { curr_expr = Var new t, sym_links = sym_links' }
-          else state
-
-  -- Must check for cases where the lambda's binder might be old or new.
-  Lam b e t -> if old == b
-      then state
-      else let state' = rename old new (state {curr_expr = e})
-           in state' {curr_expr = Lam b (curr_expr state') t}
-
-  -- Let expressions. A huge mess apparently.
-  Let bs e ->
-      let b_lhs = map fst bs
-          b_rhs = map snd bs
-
-          -- When we bind let statements, we have to be aware of potentially
-          -- changing symbolic link tables, which is why we fold over states
-          -- in order to ensure disjointness when renaming. Only call if old
-          -- and new both do not appear in lhs! Super important!! >:(
-          rawFoldFunRHS :: [(Name, State)] -> (Name, Expr) -> [(Name, State)]
-          rawFoldFunRHS acc (n, bexp) =
-              let acc_alls = map fst acc ++ (concatMap allNames (map snd acc))
-                  c_env = M.fromList $ zip acc_alls (repeat BAD)
-                  b_st = state { expr_env = M.union (expr_env state) c_env
-                               , curr_expr = bexp }
-              in acc ++ [(n, rename old new b_st)]
-
-          -- Can only safely join them if their new entries are disjoint!!
-          joinSLTs :: [SymLinkTable] -> SymLinkTable
-          joinSLTs slts = foldl M.union M.empty slts
-      in if old `elem` b_lhs
-          then state
-          else let e_state = rename old new (state {curr_expr = e})
-                   b_sts = tail $ foldl rawFoldFunRHS [("_", e_state)] bs
-                   slts = joinSLTs $ [sym_links e_state] ++
-                                     map (sym_links . snd) b_sts
-                   b_norm = map (\(n, s) -> (n, curr_expr s)) b_sts
-               in state { curr_expr = Let b_norm (curr_expr e_state)
-                        , sym_links = slts }
-
-  -- This is more straightforward than Lam. Branch and union the sym links.
-  App f a ->
-      let state_f = rename old new (state {curr_expr = f})
-          state_a = rename old new (state_f {curr_expr = a})
-          f' = curr_expr state_f
-          a' = curr_expr state_a
-      in state_a {curr_expr = App f' a'}
-
-  Case m as t ->
-          -- Function that allows us to express each Alt as a separate State
-          -- that we can then later merge. We keep a list of previous States
-          -- from folding so that we can use them to make conflict lists.
-      let altState :: [State] -> (Alt, Expr) -> [State]
-          altState acc (Alt (dc, params), aexp) =
-            let acc_alls = concatMap allNames acc  -- Super slow :)
-                c_env = M.fromList $ zip acc_alls (repeat BAD)
-                alt_st = state { expr_env = M.union (expr_env state) c_env
-                               , curr_expr = aexp }
-            in if old `elem` params
-                then acc ++ [alt_st]
-                else acc ++ [rename old new alt_st]
-
-          -- Fold over the slt's to join them. It is only safe to do so if we
-          -- can guarantee some level of mutual exclusion in terms of the
-          -- names added, hence why altState requires the previous states in
-          -- order to make an effective conflict list.
-          joinSLTs :: [SymLinkTable] -> SymLinkTable
-          joinSLTs slts = foldl M.union M.empty slts
-
-          -- Renaming the matching expression while we can.
-          m_state = rename old new (state {curr_expr = m})
-          -- Create the alt states that we can use for merging later. Make
-          -- sure to include the m_state, since that may also have new slts!
-          -- We take the tail because we need to zip it later. Very precise!
-          alt_states = tail $ foldl altState [m_state] as
-          -- Extraction.
-          alt_state_alts = map (\((Alt (dc, params), _), a_state) ->
-                                    (Alt (dc, params), curr_expr a_state))
-                               (zip as alt_states)
-          -- Join them all together.
-          slts = joinSLTs $ [sym_links m_state] ++ (map sym_links alt_states)
-      in state { curr_expr = Case (curr_expr m_state) alt_state_alts t
-               , sym_links = slts }
-
-  -- Rename the LHS, then RHS. Similar to App.
-  Assume cond exp ->
-      let state_c = rename old new (state {curr_expr = cond})
-          state_e = rename old new (state_c {curr_expr = exp})
-          cond' = curr_expr state_c
-          exp'  = curr_expr state_e
-      in state_e {curr_expr = Assume cond' exp'}
-
-  -- Just like Assume, which is just like App :)
-  Assert cond exp ->
-      let state_c = rename old new (state {curr_expr = cond})
-          state_e = rename old new (state_c {curr_expr = exp})
-          cond' = curr_expr state_c
-          exp'  = curr_expr state_e
-      in state_e {curr_expr = Assert cond' exp'}
-
-  -- Everything else.
-  _ -> state
+    Var _ _    -> renameVar    old new state
+    Lam _ _ _  -> renameLam    old new state
+    Let _ _    -> renameLet    old new state
+    App _ _    -> renameApp    old new state
+    Case _ _ _ -> renameCase   old new state
+    Assume _ _ -> renameAssume old new state
+    Assert _ _ -> renameAssert old new state
+    otherwise  -> state
 
 -- | Rename List
 --   Rename, but with a list instead. If we pipe in the input from some fresh
