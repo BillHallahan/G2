9761180273df6787e2a00c30fa5c7a53b294a765
AntonXue <antonxue1@gmail.com>
2017-07-05 05:41:20 -0400

    Changed calling convention of main

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,8 @@ import HscTypes
 import TyCon
 import GHC hiding (Name, Type, exprType)
 
-import qualified Data.Map as M
+import qualified Data.Map  as M
+import qualified Data.List as L
 
 import G2.Lib.Utils
 import G2.Lib.Printers
@@ -18,27 +19,49 @@ import G2.Internals.Preprocessing
 import G2.Internals.Symbolic
 import G2.Internals.SMT
 
+
+nVal :: [String] -> Int
+nVal args = case L.elemIndex "--n" args of
+            Nothing -> 250
+            Just id -> if id >= length args
+                           then 250
+                           else read (args !! (id + 1)) :: Int
+
+mAssume :: [String] -> Maybe String
+mAssume args = case L.elemIndex "--assume" args of
+               Nothing -> Nothing
+               Just id -> if id >= length args
+                              then error "Invalid index for m_assume"
+                              else Just (args !! (id + 1))
+
+mAssert :: [String] -> Maybe String
+mAssert args = case L.elemIndex "--assert" args of
+               Nothing -> Nothing
+               Just id -> if id >= length args
+                              then error "Invalid index for m_assert"
+                              else Just (args !! (id + 1))
+
 main = do
-    (num:args) <- getArgs
-    let filepath:mod:entry:args' = args
+    (proj:src:entry:tail_args) <- getArgs
 
-    raw_core <- mkGHCCore filepath
-    let (rt_env, re_env) = mkG2Core raw_core
+    let n_val = nVal tail_args
+    let m_assume = mAssume tail_args
+    let m_assert = mAssert tail_args
 
-    -- putStrLn "RAW CORE"
-    -- putStrLn =<< outStr raw_core
-    let t_env' = M.union rt_env (M.fromList prelude_t_decls)
-    let e_env' = re_env
-    let init_state = initState t_env' e_env' Nothing Nothing entry
-    -- let init_state = if num == "1" then initState t_env' e_env' mod Nothing Nothing entry else initState t_env' e_env' mod (Just entry) (Just $ args' !! 0) (args' !! 1)
-        
+    (rtenv, reenv) <- hskToG2 proj src
+
+    -- Inject whatever extra information from custom "prelude"
+    let tenv' = M.union rtenv (M.fromList prelude_t_decls)
+    let eenv' = reenv
+    let init_state = initState tenv' eenv' m_assume m_assert entry
+
     hhp <- getZ3ProcessHandles
 
     in_out <- run smt2 hhp init_state
 
     mapM_ (\(inArg, ex) -> do
             putStrLn . mkExprHaskell 
-                . foldl (\a a' -> App a a') (Var (args' !! 0) TyBottom) $ inArg
+                . foldl (\a a' -> App a a') (Var entry TyBottom) $ inArg
 
             putStrLn .  mkExprHaskell $ ex
         ) in_out
