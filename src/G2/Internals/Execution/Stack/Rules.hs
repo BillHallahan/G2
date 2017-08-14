eb87579cce569f5c5dadeb4c86c2131ac158255d
AntonXue <antonxue1@gmail.com>
2017-08-14 17:13:17 -0400

    Some work on Case rules

--- src/G2/Internals/Execution/Stack/Rules.hs
+++ src/G2/Internals/Execution/Stack/Rules.hs
@@ -7,12 +7,24 @@ import G2.Internals.Language
 
 import G2.Internals.Execution.Support
 
-data StackRule = StkRuleVal
-               | StkRuleVar | StkRuleUnInt
-               | StkRuleApp
-               | StkRuleLet
-               | StkRuleCase
+data StackRule = StkEvalRuleVal
+               | StkEvalRuleVar | StkEvalRuleUnInt
+               | StkEvalRuleApp
+               | StkEvalRuleLet
+               | StkEvalRuleCaseData | StkEvalRuleCaseLit
+                                     | StkEvalRuleCaseOther
+                                     | StkEvalRuleCaseDefault
+               | StkEvalRuleCaseNonVal
                deriving (Show, Eq, Read)
+
+-- | Unravels the application spine.
+unApp :: Expr -> [Expr]
+unApp (App f a) = unApp f ++ [a]
+unApp expr = [expr]
+
+isDataCon :: Expr -> Bool
+isDataCon (Data _) = True
+isDataCon _ = False
 
 -- | If something is in "value form", then it is essentially ready to be
 -- returned and popped off the heap. This will be the SSTG equivalent of having
@@ -27,19 +39,37 @@ isValueForm :: Expr -> Scope -> Bool
 isValueForm (Var var) scope = case vlookupScope var scope of
     Just (ExprObj _) -> False
     _ -> True
-isValueForm (App _ _) _ = False
+isValueForm (App f a) _ = case unApp (App f a) of
+  (x:_) -> isDataCon x
+  _ -> False
 isValueForm (Let _ _) _ = False
 isValueForm (Case _ _ _) _ = False
 isValueForm _ _ = True
 
+-- | Convert bind objects of `(Id, Expr)` pairs into `[(Name, EnvObj)]`s.
 bindsToEnvObjList :: [(Id, Expr)] -> [(Name, EnvObj)]
 bindsToEnvObjList kvs = map (\(k, v) -> (idName k, ExprObj v)) kvs
 
+-- | Inject binds into the scope.
 liftBinds :: [(Id, Expr)] -> Scope -> Scope
 liftBinds kvs scope = insertEnvObjs eobjs scope
   where
     eobjs = map (\(k, v) -> (idName k, ExprObj v)) kvs
 
+-- | DEFAULT `Alt`s.
+defaultAlts :: [Alt] -> [Alt]
+defaultAlts alts = [a | a @ (Alt Default _) <- alts]
+
+-- | Match data constructor based `Alt`s.
+matchDataAlts :: DataCon -> [Alt] -> [Alt]
+matchDataAlts dc alts = [a | a @ (Alt (DataAlt adc _) _) <- alts , dc == adc]
+
+-- | Match literal constructor based `Alt`s.
+matchLitAlts :: Lit -> [Alt] -> [Alt]
+matchLitAlts lit alts = [a | a @ (Alt (LitAlt alit) _) <- alts, lit == alit]
+
+-- | Funciton for performing rule reductions based on stack based evaluation
+-- semantics with heap memoization.
 stackReduce :: ExecState -> Maybe (StackRule, [ExecState])
 stackReduce state @ ExecState { exec_stack = stack
                               , exec_scope = scope
@@ -52,54 +82,102 @@ stackReduce state @ ExecState { exec_stack = stack
 -- returned from the heap. In SSTG, you return either literals or pointers.
 -- The distinction is less clear here. For now :)
 
--- Evaluation form expressions.
-
-  -- | Value form returning.
+  -- Our current thing is a value form, which means we can return it.
   | Evaluate expr <- code
   , isValueForm expr scope =
-    Just (StkRuleVal
+    Just (StkEvalRuleVal
          ,[state { exec_code = Return expr }])
 
-  -- | Downcasting expression from the environment.
+  -- If our variable points to something on the heap, we first push the current
+  -- name of the variable onto the stack and evaluate the expression that it
+  -- points to. After the latter is done evaluating, we pop the stack to add
+  -- a redirection pointer into the heap.
   | Evaluate (Var var) <- code
   , Just (ExprObj expr) <- vlookupScope var scope =
     let frame = UpdateFrame (idName var)
-    in Just (StkRuleVar
+    in Just (StkEvalRuleVar
             ,[state { exec_stack = pushStack frame stack
                     , exec_code = Evaluate expr }])
 
-  -- | Uninterpreted variable lifting.
+  -- If we encounter a vairable that is in scope, we treated it as a symbolic
+  -- object and subject it to uninterpreted evaluation later on. This is done
+  -- by injecting it into the heap and then evaluating the same expression
+  -- again. This is the essense of memoization / single-evaluation in laziness.
   | Evaluate (Var var) <- code
   , Nothing <- vlookupScope var scope =
     let (sname, re) = freshSeededName (idName var) confs
         svar = Id sname (idType var)
+        -- Nothing denotes that this is a "base" symbolic value that is not
+        -- dependent on any other expressions.
         sym = Symbol svar Nothing
-    in Just (StkRuleUnInt
+    in Just (StkEvalRuleUnInt
             ,[state { exec_scope = insertEnvObj (idName var, SymObj sym) scope
                     , exec_code = Evaluate (Var var)
                     , exec_names = confs}])
 
-  -- Push application RHS onto the stack.
+  -- Push application RHS onto the stack. This is essentially the same as the
+  -- original STG rules, but we pretend that every function is (appropriately)
+  -- single argument. However one problem is that scope sharing has a redundant
+  -- representation because long `App` chains will all share the same scope.
+  -- However given actual lazy evaluations within Haskell, all the `Scope`s at
+  -- each frame would really be stored in a single location on the actuall
+  -- Haskell heap during execution.
   | Evaluate (App fexpr aexpr) <- code =
     let frame = ApplyFrame aexpr scope
-    in Just (StkRuleApp
+    in Just (StkEvalRuleApp
             ,[state { exec_stack = pushStack frame stack
                     , exec_code = Evaluate fexpr }])
 
-  -- Bind let stuff into the environment.
+  -- Lift all the let bindings into the environment and continue with scope and
+  -- continue with evaluation of the let expression.
   | Evaluate (Let binds expr) <- code =
-    Just (StkRuleLet
+    Just (StkEvalRuleLet
          ,[state { exec_scope = liftBinds binds scope
                  , exec_code = Evaluate expr }])
 
-  -- Do case stuff magic shove the staccccck
-  | Evaluate (Case mexpr cvar alts) <- code =
+  -- Is the current expression able to match a data consturctor based `Alt`? If
+  -- so, then we bind all the parameters to the appropriate arguments and
+  -- proceed with the evaluation of the `Alt`'s expression. We also make sure
+  -- to perform the cvar binding.
+  | Evaluate (Case mexpr cvar alts) <- code
+  , (Data dcon):args <- unApp mexpr
+  , (Alt (DataAlt _ params) expr):_ <- matchDataAlts dcon alts
+  , length params == length args =
+    let binds = (cvar, mexpr) : zip params args
+    in Just (StkEvalRuleCaseData
+            ,[state { exec_scope = liftBinds binds scope
+                    , exec_code = Evaluate expr }])
+
+  -- | Is the current expression able to match with a literal based `Alt`? If
+  -- so, we do the cvar binding, and proceed with evaluation of the body.
+  | Evaluate (Case (Lit lit) cvar alts) <- code
+  , (Alt (LitAlt _) expr):_ <- matchLitAlts lit alts =
+    let binds = [(cvar, Lit lit)]
+    in Just (StkEvalRuleCaseLit
+            ,[state { exec_scope = liftBinds binds scope
+                    , exec_code = Evaluate expr }])
+
+  -- | We are not able to match any of the stuff, and hit a DEFAULT instead? If
+  -- so, we just perform the cvar binding and proceed with the alt expression.
+  | Evaluate (Case mexpr cvar alts) <- code
+  , (Alt _ expr):_ <- defaultAlts alts =
+    let binds = [(cvar, mexpr)]
+    in Just (StkEvalRuleCaseDefault
+            ,[state { exec_scope = liftBinds binds scope
+                    , exec_code = Evaluate expr }])
+
+  -- Case evaluation also uses the stack in graph reduction based evaluation
+  -- semantics. The case's binding variable and alts are pushed onto the stack
+  -- as a `CaseFrame` along with their appropriate `Scope`. However this is
+  -- only done when the matching expression is NOT in value form. Value forms
+  -- should be handled by other StkEvalRuleCase* rules.
+  | Evaluate (Case mexpr cvar alts) <- code
+  , not (isValueForm mexpr scope) =
     let frame = CaseFrame cvar alts scope
-    in Just (StkRuleCase
+    in Just (StkEvalRuleCaseNonVal
             ,[state { exec_stack = pushStack frame stack
                     , exec_code = Evaluate mexpr }])
 
--- Return form expressions.
-  
+  -- Return forms the `Code`.
 
 
