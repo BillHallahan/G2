d18adf1684df0e52bf957dc40f7428fe9fe81a96
AntonXue <antonxue1@gmail.com>
2017-11-10 14:31:23 -0500

    cabal package parser. TODO: networking

--- src/G2/Internals/Translation.hs
+++ src/G2/Internals/Translation.hs
@@ -1,11 +1,14 @@
 -- | Translation
 --   Export module for G2.Internals.Translation.
 module G2.Internals.Translation
-    ( module G2.Internals.Translation.Haskell
+    ( module G2.Internals.Translation.Cabal
+    , module G2.Internals.Translation.Haskell
     , module G2.Internals.Translation.Interface
     , module G2.Internals.Translation.PrimInject
     ) where
 
+import G2.Internals.Translation.Cabal
 import G2.Internals.Translation.Haskell
 import G2.Internals.Translation.PrimInject
 import G2.Internals.Translation.Interface
+
---
+++ src/G2/Internals/Translation/Cabal.hs
@@ -0,0 +1,6 @@
+module G2.Internals.Translation.Cabal
+  ( module G2.Internals.Translation.Cabal.Parser
+  ) where
+
+import G2.Internals.Translation.Cabal.Parser
+
