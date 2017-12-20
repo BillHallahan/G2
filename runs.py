b18ab9d1a62b8ead1a19cdaaece6e0047027b0b1
AntonXue <antonxue1@gmail.com>
2017-12-20 12:13:54 -0500

    added dumper tool

---
+++ runs.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import sys
+
+proj  = sys.argv[1]
+src   = sys.argv[2]
+entry = sys.argv[3]
+ticks = int(sys.argv[4])
+
+dumps = "dumps"
+
+if not os.path.exists(dumps):
+  os.makedirs(dumps)
+
+for i in range(0, ticks):
+  run = "cabal run G2 -- {0} {1} defs/PrimDefs.hs {2} --n {3} > {4}/hue-{5}.hs".format(proj, src, entry, str(ticks), dumps, str(i))
+  print run
+  proc = subprocess.Popen(run, shell=True)
+  proc.wait()
+
