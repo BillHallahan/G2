a278e9ac5a88b8486788cc303e5976a98c68193e
Bill Hallahan <billhallahan@juno.com>
2018-02-20 13:29:31 -0500

    Fix to config file crashes

--- src/G2/Internals/Config/Interface.hs
+++ src/G2/Internals/Config/Interface.hs
@@ -4,14 +4,33 @@ import G2.Internals.Config.Config
 import G2.Internals.Config.ParseConfig
 
 import qualified Data.Map as M
+import System.Directory
+
+configName :: FilePath
+configName = "g2.cfg"
 
 getConfig :: [String] -> IO Config
 getConfig ars = do
-    conf <- parseConfig "g2.cfg"
+    ex <- configExists
 
-    case conf of
-        Right conf' -> return (mkConfig ars conf')
-        Left e -> do
-            putStrLn "Configuration file parsing error:"
-            print e
-            return (mkConfig ars M.empty)
+    case ex of
+        True -> do
+            conf <- parseConfig configName
+
+            case conf of
+                Right conf' -> return (mkConfig ars conf')
+                Left e -> do
+                    putStrLn "Configuration file parsing error:"
+                    print e
+                    return (mkConfig ars M.empty)
+        False -> return (mkConfig ars M.empty)
+
+configExists :: IO Bool
+configExists = do
+    ex <- doesFileExist configName
+
+    case ex of
+        True -> return ()
+        False -> putStrLn $ "Configuration file " ++ configName ++ " missing"
+
+    return ex
