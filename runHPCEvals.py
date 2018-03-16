10e2fdfbbbd5209cb4b885113eb73ea41e663128
Bill Hallahan <billhallahan@juno.com>
2018-03-15 23:34:29 -0400

    More HPC

--- runHPCEvals.py
+++ runHPCEvals.py
@@ -4,12 +4,12 @@ import re
 import subprocess
 import sys
 import time
-NVALUE = 1200
+NVALUE = 1000
 TIMEOUT = 300 # seconds
 
 projDir = "../liquidhaskell-study/wi15/smallcheck/benchmarks/"
 
-listDir = "eval-list/"
+listDir = "" #"eval-list/"
 listTargets = [
     ("Catch.hs", "prop"),
     ("Mux.hs", "prop_encDec"),
@@ -34,7 +34,7 @@ listTargets = [
 def runEval(evalDir, evalList, runStats):
   for (file, funs) in evalList:
     for f in funs:
-      command = "cabal run G2 -- {0} --liquid {1} --liquid-func {2} --n {3} --time {4}"\
+      command = "cabal run G2 -- {0}  {1}  {2} --n {3} --time {4}"\
                 .format(projDir + evalDir, projDir + evalDir + file, f, NVALUE, TIMEOUT)
       # print(command)
       startTime = time.time()
@@ -43,15 +43,17 @@ def runEval(evalDir, evalList, runStats):
       p_status = p.wait()
       deltaTime = time.time() - startTime
 
-      if re.search("violating ([^f]*[^i]*[^x]*[^m]*[^e])\'s refinement type", output) is not None:
-        hasConcrete = re.search("violating ([^f]*[^i]*[^x]*[^m]*[^e])\'s refinement type\nConcrete",
-                                output) is not None
-        hasAbstract = "Abstract" in output
-        runStats.append((file, f, hasConcrete, hasAbstract, deltaTime))
-        print((file, f, hasConcrete, hasAbstract, deltaTime))
-      else:
-        runStats.append((file, f, False, False, deltaTime))
-        print((file, f, False, False, deltaTime))
+      runStats.append((file, f, hasConcrete, hasAbstract, deltaTime))
+      print((file, f, hasConcrete, hasAbstract, deltaTime))
+      # if re.search("violating ([^f]*[^i]*[^x]*[^m]*[^e])\'s refinement type", output) is not None:
+      #   hasConcrete = re.search("violating ([^f]*[^i]*[^x]*[^m]*[^e])\'s refinement type\nConcrete",
+      #                           output) is not None
+      #   hasAbstract = "Abstract" in output
+      #   runStats.append((file, f, hasConcrete, hasAbstract, deltaTime))
+      #   print((file, f, hasConcrete, hasAbstract, deltaTime))
+      # else:
+      #   runStats.append((file, f, False, False, deltaTime))
+      #   print((file, f, False, False, deltaTime))
 
 # Get all the RHS of the file-functions pairs.
 rhs = [x for xs in (map(lambda p: p[1], listTargets + mapreduceTargets + kmeansTargets))
