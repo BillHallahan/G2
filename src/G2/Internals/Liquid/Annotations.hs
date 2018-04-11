b192409ae7762109a4196335800958bfffd46e28
Bill Hallahan <billhallahan@juno.com>
2018-04-11 15:33:22 -0700

    Cleanup

--- src/G2/Internals/Liquid/Annotations.hs
+++ src/G2/Internals/Liquid/Annotations.hs
@@ -11,6 +11,7 @@ import G2.Internals.Liquid.TCValues
 import Language.Haskell.Liquid.Constraint.Types
 import Language.Haskell.Liquid.Types hiding (Loc)
 
+import Data.Coerce
 import qualified Data.HashMap.Lazy as HM
 import Data.Maybe
 import qualified Data.Text as T
@@ -22,23 +23,15 @@ newtype AnnotMap = AM (HM.HashMap (Loc, Maybe T.Text) (Maybe Expr)) deriving (Eq
 amKeys (AM am) = HM.keys am
 
 lookupAnnot :: Name -> AnnotMap -> Maybe Expr
-lookupAnnot (Name n (Just m) _ (Just l)) (AM am) =
-    case HM.lookup (l, Just $ m `T.append` "." `T.append` n) am of
-        Just e -> e
-        Nothing -> Nothing
-lookupAnnot (Name n Nothing _ (Just l)) (AM am) =
-    case HM.lookup (l, Just n) am of
-        Just e -> e
-        Nothing -> Nothing
-lookupAnnot _ _ = Nothing
+lookupAnnot (Name n (Just m) _ (Just l)) =
+    maybe Nothing id . HM.lookup (l, Just $ m `T.append` "." `T.append` n) . coerce
+lookupAnnot (Name n Nothing _ (Just l)) =
+    maybe Nothing id . HM.lookup (l, Just n) . coerce
+lookupAnnot _ = const Nothing
 
 getAnnotMap :: TCValues -> State t -> [CGInfo] -> AnnotMap
 getAnnotMap tcv s cgi =
-    let
-        am = mconcat $ map annotMap cgi
-        am' = annotMapToExpr tcv s am
-    in
-    am'
+    annotMapToExpr tcv s $ mconcat $ map annotMap cgi
 
 -- From https://github.com/ucsd-progsys/liquidhaskell/blob/75184064eed475289648ead76e3e9d370b168e66/src/Language/Haskell/Liquid/Types.hs
 -- newtype AnnInfo a = AI (M.HashMap SrcSpan [(Maybe Text, a)])
