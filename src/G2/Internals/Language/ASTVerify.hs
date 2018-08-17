e7e66d50b8ec8eed4f2da3b909ac11b76fff07e6
Maxwell Bland <mbland@ucsd.edu>
2018-08-17 01:33:38 -0700

    Modify letsTypeValid to return App under which the Let failed as well as the Binds

--- src/G2/Internals/Language/ASTVerify.hs
+++ src/G2/Internals/Language/ASTVerify.hs
@@ -2,39 +2,38 @@
 
 {-# LANGUAGE FlexibleContexts #-}
 
-module G2.Internals.Language.ASTVerify (letsTypeValid, caseTypeValid) where
+module G2.Internals.Language.ASTVerify (letsTypeValid) where
 import G2.Internals.Language.Syntax
 import G2.Internals.Language.AST
 import G2.Internals.Language.Typing
-import G2.Internals.Language
-
--- | checkProperties
--- TODO: Top-level function which handles checking properties of the AST for
--- validity.
-
--- | ErrorSource
--- TODO: Container for various type errors; function paired with data on what
--- errored?
 
 -- | typeMatch
 -- Checks if a typed class matches the type of another typed class
 typeMatch :: (Typed a, Typed b) => a -> b -> Bool
-typeMatch x y = typeOf x == typeOf y
+typeMatch x y = typeOf x .::. typeOf y
+
+-- | bindsTypeValid
+-- Takes a list of Binds and returns those which have invalid types
+bindsTypeValid :: Binds -> Binds
+bindsTypeValid bs = foldr bindCheckAcc [] bs
+    where
+    bindCheckAcc b acc = if typeMatch (fst b) (snd b) then acc else b:acc
 
 -- | letsTypeValid
--- All Ids bound in Let statements should have the same type as the expression
--- they are bound to.
--- TODO:
---  Convert this bool into a monoid
---  Returning a list of ErrorSource ...
---      (list of the functions the Let's were in, along with the
---       Id and Expr with non-matching types.)
-letsTypeValid :: (ASTContainer m Expr) => m -> Bool
-letsTypeValid = evalASTs letsTypeValid'
+-- Takes an ASTContainer and returns a list of all the Lets for which the
+-- types were invalid, paired with the most Recent App in which they occurred
+letsTypeValid :: (ASTContainer m Expr) => m -> [(Expr,Binds)]
+letsTypeValid = evalContainedASTs (letsTypeValid' Nothing)
 
-letsTypeValid' :: Expr -> Bool
-letsTypeValid' (Let bs e) = (foldr ((&&) . \x -> typeMatch (fst x) (snd x)) True bs)
-letsTypeValid' e = letsTypeValid $ children e
+letsTypeValid' :: Maybe Expr -> Expr -> [(Expr, Binds)]
+letsTypeValid' (Just a) (Let bs e) = case badBinds of
+    [] -> []
+    _  -> (letsTypeValid' (Just a) e) ++ [(a,badBinds)]
+    where
+    badBinds = bindsTypeValid bs
+letsTypeValid' _ e@(App e' e'') = letsTypeValid' (Just e) e' ++ letsTypeValid' (Just e) e''
+letsTypeValid' m e = (evalContainedASTs (letsTypeValid' m)) $ children e
+
 
 -- | caseTypeValid
 -- In all case expressions, the types of the Expr and Id, should match, and
