dbba222992e95d077bd41da9be213a41c39441df
Maxwell Bland <mbland@ucsd.edu>
2018-08-20 15:01:23 -0700

    Add in function to verify casts, fix bug in caseTypeVerify

--- src/G2/Internals/Language/ASTVerify.hs
+++ src/G2/Internals/Language/ASTVerify.hs
@@ -30,9 +30,23 @@ caseTypeValid :: (ASTContainer m Expr) => m -> [(Id, Either Expr Alt)]
 caseTypeValid = evalASTs caseTypeValid'
 
 caseTypeValid' :: Expr -> [(Id, Either Expr Alt)]
-caseTypeValid' (Case e i alts) = concat [[ (i, Left e) | typeMatch i e ] , altMisMatches]
+caseTypeValid' (Case e i alts) = concat [[ (i, Left e) | not $ typeMatch i e ] , altMisMatches]
   where
     -- Filter alts by AltMatch not matching the Id of the case, then pair with Id
     altMisMatches = map (\a -> (i,(Right a))) (filter (\(Alt am _) -> (not $ typeMatch am i)) alts)
 caseTypeValid' _ = []
+
+-- | castTypeValid
+-- In a Cast the type of the expression and the LHS of the Coercion should match. Returns
+-- the list of Casts which fail to meet this specification
+castTypeValid :: (ASTContainer m Expr) => m -> [Expr]
+castTypeValid = evalASTs castTypeValid'
+
+castTypeValid' :: Expr -> [Expr]
+castTypeValid' c@(Cast e (lhs :~ _)) = []
+--   | not $ (typeMatch lhs e) = [c]
+--   | _ = []
+castTypeValid' _ = []
+
+
 
