34c167d674d760436f06f8c4551cf2f93030488b
Maxwell Bland <mbland@ucsd.edu>
2018-08-20 13:05:58 -0700

    Add in function to verify Case Expr, typeOf AltMatch, simplify Let type validation

--- src/G2/Internals/Language/ASTVerify.hs
+++ src/G2/Internals/Language/ASTVerify.hs
@@ -2,7 +2,7 @@
 
 {-# LANGUAGE FlexibleContexts #-}
 
-module G2.Internals.Language.ASTVerify (letsTypeValid) where
+module G2.Internals.Language.ASTVerify (letsTypeValid, caseTypeValid) where
 import G2.Internals.Language.Syntax
 import G2.Internals.Language.AST
 import G2.Internals.Language.Typing
@@ -19,21 +19,20 @@ letsTypeValid :: (ASTContainer m Expr) => m -> Binds
 letsTypeValid = evalASTs letsTypeValid'
 
 letsTypeValid' :: Expr -> Binds
-letsTypeValid' (Let bs e) = foldr bindCheckAcc [] bs
-    where
-    bindCheckAcc b acc = if typeMatch (fst b) (snd b) then acc else b:acc
-letsTypeValid' e = []
+letsTypeValid' (Let bs _) = filter (\b -> not $ typeMatch (fst b) (snd b)) bs
+letsTypeValid' _ = []
 
 
 -- | caseTypeValid
 -- In all case expressions, the types of the Expr and Id, should match, and
 -- they should also correspond to either the DataCon or Lit in the AltMatches.
--- Returns the App under which the case fails to typematch paired with a list of
--- pairs of expressions under that case which fails
--- caseTypeValid :: m -> [(Expr, [(Expr, Expr)])]
--- caseTypeValid = evalASTs caseTypeValid'
+caseTypeValid :: (ASTContainer m Expr) => m -> [(Id, Either Expr Alt)]
+caseTypeValid = evalASTs caseTypeValid'
 
--- caseTypeValid' :: Expr -> [(Expr, ]
--- caseTypeValid' c@(Case e i alts) = foldr ((&&) . (typeMatch i)) (typeMatch i e) (alts)
--- caseTypeValid' e = caseTypeValid $ children e
+caseTypeValid' :: Expr -> [(Id, Either Expr Alt)]
+caseTypeValid' (Case e i alts) = concat [[ (i, Left e) | typeMatch i e ] , altMisMatches]
+  where
+    -- Filter alts by AltMatch not matching the Id of the case, then pair with Id
+    altMisMatches = map (\a -> (i,(Right a))) (filter (\(Alt am _) -> (not $ typeMatch am i)) alts)
+caseTypeValid' _ = []
 
