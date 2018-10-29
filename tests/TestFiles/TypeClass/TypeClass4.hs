c75707532d9f54bede7fd480f40d2ba9da786cfd
Bill Hallahan <billhallahan@juno.com>
2018-10-28 20:50:47 -0400

    Fixed some tests

--- tests/TestFiles/TypeClass/TypeClass4.hs
+++ tests/TestFiles/TypeClass/TypeClass4.hs
@@ -1,9 +1,6 @@
 module TypeClass4 where
 
 import qualified Data.Map as M
-import Prelude hiding (map, reduce, concat, foldr, foldr1)
-
-import Data.Coerce
 
 f :: Int
 f = length (M.empty :: M.Map Int Int)
