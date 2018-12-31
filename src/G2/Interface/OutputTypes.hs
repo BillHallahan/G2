0b12f5f76ad256a03d6d989e661f0e87b6e22689
Bill Hallahan <billhallahan@juno.com>
2018-12-31 00:40:30 -0500

    Removed an unused type

--- src/G2/Interface/OutputTypes.hs
+++ src/G2/Interface/OutputTypes.hs
@@ -1,14 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module G2.Interface.OutputTypes ( ExecOut (..)
-                                , ExecRes (..)) where
+module G2.Interface.OutputTypes ( ExecRes (..)) where
 
 import G2.Language
-
-data ExecOut t = ExecOut { exec_res :: [ExecRes t] -- ^ Fully executed states
-                         , in_progress :: [State t] -- ^ States that were generated but not fully executed
-                         }
 
 -- | A fully executed State
 data ExecRes t = ExecRes { final_state :: State t
@@ -16,32 +11,6 @@ data ExecRes t = ExecRes { final_state :: State t
                          , conc_out :: Expr
                          , violated :: Maybe FuncCall -- ^ A violated assertion
                          } deriving (Show, Read)
-
-instance Named t => Named (ExecOut t) where
-    names (ExecOut { exec_res = er, in_progress = ip }) =
-        names er ++ names ip
-
-    rename old new (ExecOut { exec_res = er, in_progress = ip }) =
-        ExecOut { exec_res = rename old new er, in_progress = rename old new ip }
-
-    renames hm (ExecOut { exec_res = er, in_progress = ip }) =
-        ExecOut { exec_res = renames hm er, in_progress = renames hm ip }
-
-instance ASTContainer t Expr => ASTContainer (ExecOut t) Expr where
-    containedASTs (ExecOut { exec_res = er, in_progress = ip }) =
-        containedASTs er ++ containedASTs ip
-
-    modifyContainedASTs f (ExecOut { exec_res = er, in_progress = ip }) =
-        ExecOut { exec_res = modifyContainedASTs f er
-                , in_progress = modifyContainedASTs f ip }
-
-instance ASTContainer t Type => ASTContainer (ExecOut t) Type where
-    containedASTs (ExecOut { exec_res = er, in_progress = ip }) =
-        containedASTs er ++ containedASTs ip
-
-    modifyContainedASTs f (ExecOut { exec_res = er, in_progress = ip }) =
-        ExecOut { exec_res = modifyContainedASTs f er
-                , in_progress = modifyContainedASTs f ip }
 
 instance Named t => Named (ExecRes t) where
     names (ExecRes { final_state = s
