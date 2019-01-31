0a40b3a46dff913e7d904cf313ed9ac170fc617e
Bill Hallahan <billhallahan@juno.com>
2019-01-31 18:02:37 -0500

    Reformatting

--- src/G2/Translation/Haskell.hs
+++ src/G2/Translation/Haskell.hs
@@ -499,39 +499,40 @@ mkTyCon nm tm t = case dcs of
 
     bv = map (mkId tm) $ tyConTyVars t
 
-    (nm'', tm'', dcs, dcsf) = case isAlgTyCon t of 
-                            True -> case algTyConRhs t of
-                                            DataTyCon { data_cons = dc } -> 
-                                                ( nm'
-                                                , tm'
-                                                , Just $ G2.DataTyCon bv $ map (mkData nm' tm) dc
-                                                , Just $ map (mkId tm'' . dataConWorkId) dc)
-                                            NewTyCon { data_con = dc
-                                                     , nt_rhs = rhst} -> 
-                                                     ( nm'
-                                                     , tm'
-                                                     , Just $ G2.NewTyCon { G2.bound_ids = bv
-                                                                          , G2.data_con = mkData nm' tm dc
-                                                                          , G2.rep_type = mkType tm rhst}
-                                                     , Just $ [(mkId tm'' . dataConWorkId) dc])
-                                            AbstractTyCon {} -> error "Unhandled TyCon AbstractTyCon"
-                                            -- TupleTyCon {} -> error "Unhandled TyCon TupleTyCon"
-                                            TupleTyCon { data_con = dc, tup_sort = ts } ->
-                                              ( nm'
-                                              , tm'
-                                              , Just $ G2.DataTyCon bv $ [mkData nm' tm dc]
-                                              , Nothing)
-                                            SumTyCon {} -> error "Unhandled TyCon SumTyCon"
-                            False -> case isTypeSynonymTyCon t of
-                                    True -> 
-                                        let
-                                            (tv, st) = fromJust $ synTyConDefn_maybe t
-                                            st' = mkType tm st
-                                            tv' = map (mkId tm) tv
-                                        in
-                                        (nm, tm', Just $ G2.TypeSynonym { G2.bound_ids = tv'
-                                                                         , G2.synonym_of = st'}, Nothing)
-                                    False -> (nm, tm, Nothing, Nothing)
+    (nm'', tm'', dcs, dcsf) =
+        case isAlgTyCon t of 
+            True -> case algTyConRhs t of
+                            DataTyCon { data_cons = dc } -> 
+                                ( nm'
+                                , tm'
+                                , Just $ G2.DataTyCon bv $ map (mkData nm' tm) dc
+                                , Just $ map (mkId tm'' . dataConWorkId) dc)
+                            NewTyCon { data_con = dc
+                                     , nt_rhs = rhst} -> 
+                                     ( nm'
+                                     , tm'
+                                     , Just $ G2.NewTyCon { G2.bound_ids = bv
+                                                          , G2.data_con = mkData nm' tm dc
+                                                          , G2.rep_type = mkType tm rhst}
+                                     , Just $ [(mkId tm'' . dataConWorkId) dc])
+                            AbstractTyCon {} -> error "Unhandled TyCon AbstractTyCon"
+                            -- TupleTyCon {} -> error "Unhandled TyCon TupleTyCon"
+                            TupleTyCon { data_con = dc, tup_sort = ts } ->
+                              ( nm'
+                              , tm'
+                              , Just $ G2.DataTyCon bv $ [mkData nm' tm dc]
+                              , Nothing)
+                            SumTyCon {} -> error "Unhandled TyCon SumTyCon"
+            False -> case isTypeSynonymTyCon t of
+                    True -> 
+                        let
+                            (tv, st) = fromJust $ synTyConDefn_maybe t
+                            st' = mkType tm st
+                            tv' = map (mkId tm) tv
+                        in
+                        (nm, tm', Just $ G2.TypeSynonym { G2.bound_ids = tv'
+                                                        , G2.synonym_of = st'}, Nothing)
+                    False -> (nm, tm, Nothing, Nothing)
     -- dcs = if isDataTyCon t then map mkData . data_cons . algTyConRhs $ t else []
 
 mkTyConName :: G2.TypeNameMap -> TyCon -> G2.Name
