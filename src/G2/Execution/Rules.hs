813c275767b3cb50af4272f3973414959be162b8
Maganty Rushyendra <mrushyendra@yahoo.com.sg>
2019-04-20 17:23:54 -0400

    moved functions to Language/Typing.hs, updated guards in mexprTyToExpr to group all non NewTyCon DataCons in 1 case

--- src/G2/Execution/Rules.hs
+++ src/G2/Execution/Rules.hs
@@ -29,7 +29,6 @@ import G2.Solver hiding (Assert)
 
 import Control.Monad.Extra
 import Data.Maybe
-import qualified Data.List as L
 
 stdReduce :: Solver solver => solver -> State t -> Bindings -> IO (Rule, [(State t, ())], Bindings)
 stdReduce solver s b@(Bindings {name_gen = ng}) = do
@@ -451,45 +450,21 @@ concretizeVarExpr' s@(State {expr_env = eenv, type_env = tenv, symbolic_ids = sy
     binds = [(cvar, (Var mexpr_id))]
     aexpr'' = liftCaseBinds binds aexpr'
 
--- Ugly solution, todo: improve
-
 -- | Given the Type of the matched Expr, looks for Type in the TypeEnv, and returns Expr level representation of the Type
 mexprTyToExpr :: Type -> TypeEnv -> [Expr]
 mexprTyToExpr mexpr_t tenv 
-    | Just (algDataTy, bindings) <- getAlgDataTy mexpr_t tenv
+    -- special case for NewTyCon, involves looking up tyVars and binding them to concrete types specified by mexpr_t
+    | Just (algDataTy, bindings) <- getAlgDataTy mexpr_t tenv     
     , (isNewTyCon algDataTy) = dconTyToExpr (data_con algDataTy) bindings
-    | Just (_,_) <- getAlgDataTy mexpr_t tenv = typeToExpr mexpr_t -- is DataTyCon or TypeSynonym
-    | otherwise = []
+    | otherwise = typeToExpr mexpr_t
 
--- | Given DataCon, and an (Id, Type) mapping, returns list of Expression level Type Arguments to DataCon
+-- | Given a DataCon, and an (Id, Type) mapping, returns list of Expression level Type Arguments to DataCon
 dconTyToExpr :: DataCon -> [(Id, Type)] -> [Expr]
 dconTyToExpr (DataCon _ t) bindings =
     case (getTyApps t) of
         (Just tApps) -> tyAppsToExpr tApps bindings
         Nothing -> []
 
--- | Find nested tyApps, if any, in the given Type
-getTyApps :: Type -> Maybe Type
-getTyApps (TyForAll _ t) = getTyApps t
-getTyApps (TyFun t _) = getTyApps t
-getTyApps t@(TyApp _ _) = Just t
-getTyApps _ = Nothing -- what about TyCon
-
--- | Given sequence of nested tyApps e.g. tyApp (tyApp ...) ...), returns list of expr level Types, searching through [Id,Type] list in the process
-tyAppsToExpr :: Type -> [(Id, Type)] -> [Expr]
-tyAppsToExpr (TyApp t (TyVar tVarId)) bindings = exprs ++ newTyExpr
-    where
-        newTyExpr = 
-            case (L.find (\(i, _) -> (tVarId == i)) bindings) of -- search list of (Id, Type) to find corresponding Type, and convert to expr
-                (Just (_, ty)) -> [Type ty]
-                Nothing -> []
-        exprs = tyAppsToExpr t bindings
-tyAppsToExpr (TyApp t1 t2) bindings = exprs ++ newTyExpr
-    where
-        newTyExpr = [Type t2]
-        exprs = tyAppsToExpr t1 bindings
-tyAppsToExpr _ _ = []
-                                           
 createExtConds :: State t -> NameGen -> Expr -> Id -> [(DataCon, [Id], Expr)] -> ([NewPC t], NameGen)
 createExtConds _ ng _ _ [] = ([], ng)
 createExtConds s ng mexpr cvar (x:xs) = 
