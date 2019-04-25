ec6e759f615942e83eaa2b4e4ba417e0362eaa7f
Bill Hallahan <billthallahan@gmail.com>
2019-04-25 13:04:31 -0400

    Work on QuasiQuotes

--- src/G2/QuasiQuotes/FloodConsts.hs
+++ src/G2/QuasiQuotes/FloodConsts.hs
@@ -1,16 +1,25 @@
 -- Tries to eliminate a symbolic variable by replacing it with a constant.
 
-module G2.QuasiQuotes.FloodConsts ( floodConstants
+module G2.QuasiQuotes.FloodConsts ( floodConstantsChecking
+                                  , floodConstants
                                   , floodConstant) where
 
+import G2.Execution.PrimitiveEval
 import G2.Language
 import qualified G2.Language.ExprEnv as E
+
+-- | Tries to eliminate a symbolic variable by replacing them with constants.
+-- Returns Maybe a State, if the variables are replacable, and don't make the
+-- path constraints obviously false
+floodConstantsChecking :: [(Name, Expr)] -> State t -> Maybe (State t)
+floodConstantsChecking ne s =
+    case floodConstants ne s of
+        Just s' -> Just s'
+        Nothing -> Nothing
 
 floodConstants :: [(Name, Expr)] -> State t -> Maybe (State t)
 floodConstants ne s = foldr (\(n, e) s' -> floodConstant n e =<< s') (Just s) ne
 
--- | Tries to eliminate a symbolic variable by replacing them with constants.
--- Returns Maybe a State, if the variable is replacable
 floodConstant :: Name -> Expr -> State t -> Maybe (State t)
 floodConstant n e s
     | E.isSymbolic n (expr_env s) =
@@ -38,3 +47,19 @@ floodConstantList s  ((e1, e2):xs)
     | e1 == e2 = floodConstantList s xs
 floodConstantList s [] = Just s
 floodConstantList _ _ = Nothing
+
+-- Attempts to determine if a PathCond is satisfiable.  A return value of False
+-- means the PathCond is definitely unsatisfiable.  A return value of True means
+-- the PathCond may or may not be satisfiable. 
+pathCondMaybeSatisfiable :: KnownValues -> TypeEnv -> PathCond -> Bool
+pathCondMaybeSatisfiable kv tenv (AltCond l1 (Lit l2) b) = (l1 == l2) == b
+pathCondMaybeSatisfiable kv tenv (ExtCond e b) =
+    let
+        r = evalPrims kv tenv e
+        
+        tr = mkBool kv tenv True
+        fal = mkBool kv tenv False
+    in
+    if (r == tr && not b) || (r == fal && b) then False else True
+pathCondMaybeSatisfiable kv tenv (ConsCond dc1 (Data dc2) b) = dc1 == dc2
+pathCondMaybeSatisfiable _ _ (PCExists _) = True
