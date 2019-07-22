8143f80a41dc5a2bfef6642cb8e600cf235ed9d4
Maganty Rushyendra <mrushyendra@yahoo.com.sg>
2019-07-21 21:39:00 -0400

    changed compareWithErrMsgs in mergeStateUnitTests to take predicate when comparing actual and expected values

--- tests/MergeStateUnitTests.hs
+++ tests/MergeStateUnitTests.hs
@@ -20,6 +20,7 @@ import qualified G2.Language.TypeClasses.TypeClasses as TC
 import G2.Solver
 import G2.Config
 
+import Data.Maybe
 import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.HashSet as S
@@ -33,9 +34,10 @@ mergeCurrExprTests = if not (null errs)
             then Left (foldr concatErrMsgs "" errs)
             else Right True
         where
-            errs = filter selectErrors $ zipWith (compareWithErrMsg "Error merging CurrExprs. Expected: ")
-                [(1, expectedVal1)]
-                [g2Val1]
+            errs = filter selectErrors $ zipWith (compareWithErrMsg "Error merging CurrExprs. ")
+                [(1, (== expectedVal1)), (2, (== expectedVal2)), (3, (== expectedVal3)), (4, (== expectedVal4))
+                , (5,(== expectedVal5)), (6, check6)]
+                [g2Val1, g2Val2, g2Val3, g2Val4, g2Val5, g2Val6]
 
 checkADTNumericalTests :: IO (Either String Bool)
 checkADTNumericalTests = do
@@ -87,100 +89,126 @@ concatErrMsgs :: Either String Bool -> String -> String
 concatErrMsgs (Left e') e = e ++ "\n" ++ e'
 concatErrMsgs (Right _) _ = "Error. No error message to concatenate"
 
-compareWithErrMsg :: Eq a => Show a => String -> (TestNum, a) -> a -> Either String Bool
-compareWithErrMsg errMsg (i, expected) actual =  if expected == actual
+compareWithErrMsg :: Show a => String -> (TestNum, (a -> Bool)) -> a -> Either String Bool
+compareWithErrMsg errMsg (i, f) actual =  if f actual
     then Right True
-    else Left (errMsg ++ " Test number " ++ (show i) ++ " - Expected: " ++ (show expected) ++ "\n Got: " ++ (show actual))
+    else Left (errMsg ++ " Test number " ++ (show i) ++ " Got: " ++ (show actual))
 
 -- Following functions return individual test outputs or expected output values
 
 -- mergeCurrExpr Tests
-g2Val1 :: CurrExpr -- simple test
+
+--simple test
+g2Val1 :: CurrExpr
 g2Val1 = snd $ SM.mergeCurrExpr ctxt
     where
         ce1 = (CurrExpr Evaluate (App e1 e2))
         ce2 = (CurrExpr Evaluate (App e1 e2'))
-        ctxt = createContext ce1 ce2
+        ctxt = createContext ce1 ce2 Nothing Nothing
 
--- g2Val2 :: CurrExpr
--- g2Val2 = SM.mergeCurrExpr kv idX
--- (CurrExpr Evaluate (App e1 (App e2 e3)))
--- (CurrExpr Evaluate (App e1 (App e2' e3')))
---     where
---         kv = simpleKV
+-- force NonDet further down
+g2Val2 :: CurrExpr
+g2Val2 = snd $ SM.mergeCurrExpr ctxt
+    where
+        ctxt = createContext ce1 ce2 Nothing Nothing
+        ce1 = (CurrExpr Evaluate (App e1 (App e2 e3)))
+        ce2 = (CurrExpr Evaluate (App e1 (App e2 e3')))
 
--- g2Val3 :: CurrExpr -- force NonDet further down
--- g2Val3 = SM.mergeCurrExpr kv idX
---     (CurrExpr Evaluate (App e1 (App e2 e3)))
---     (CurrExpr Evaluate (App e1 (App e2 e3')))
---     where
---         kv = simpleKV
+-- Identical
+g2Val3 :: CurrExpr
+g2Val3 = snd $ SM.mergeCurrExpr ctxt
+    where
+        ctxt = createContext ce1 ce2 Nothing Nothing
+        ce1 = (CurrExpr Evaluate (App e1 (App e2 e3)))
+        ce2 = (CurrExpr Evaluate (App e1 (App e2 e3)))
 
--- g2Val4 :: CurrExpr -- identical expressions
--- g2Val4 = SM.mergeCurrExpr kv idX
---     (CurrExpr Evaluate (App e1 (App e2 e3)))
---     (CurrExpr Evaluate (App e1 (App e2 e3)))
---     where
---         kv = simpleKV
+-- Identical expressions 2
+g2Val4 :: CurrExpr
+g2Val4 = snd $ SM.mergeCurrExpr ctxt
+    where
+        ctxt = createContext ce1 ce2 Nothing Nothing
+        ce1 = (CurrExpr Evaluate (App e1 (App e4 e3)))
+        ce2 = (CurrExpr Evaluate (App e1 (App e4 e3')))
 
--- g2Val5 :: CurrExpr -- identical expressions 2
--- g2Val5 = SM.mergeCurrExpr kv idX
---     (CurrExpr Evaluate (App e1 (App e4 e3)))
---     (CurrExpr Evaluate (App e1 (App e4 e3')))
---     where
---         kv = simpleKV
+-- Nested (App...) in first argument
+g2Val5 :: CurrExpr
+g2Val5 = snd $ SM.mergeCurrExpr ctxt
+    where
+        ctxt = createContext ce1 ce2 Nothing Nothing
+        ce1 = (CurrExpr Evaluate (App (App e1 e2) (App e4 e3)))
+        ce2 = (CurrExpr Evaluate (App (App e1 e2') (App e4 e3')))
 
--- g2Val6 :: CurrExpr -- Nested (App ...) in first argument
--- g2Val6 = SM.mergeCurrExpr kv idX
---     (CurrExpr Evaluate (App (App e1 e2) (App e4 e3)))
---     (CurrExpr Evaluate (App (App e1 e2') (App e4 e3')))
---     where
- --        kv = simpleKV
+-- merge inner case expr in SMNF
+g2Val6 :: CurrExpr
+g2Val6 = snd $ SM.mergeCurrExpr ctxt
+    where
+        ctxt = createContext ce1 ce2 Nothing Nothing
+        ce1 = (CurrExpr Evaluate
+                (App (App e1 e2)
+                     (Case varfs1 idfs1 [Alt (LitAlt (LitInt 1)) e2', Alt (LitAlt (LitInt 2)) e3'])
+                )
+              )
+        ce2 = (CurrExpr Evaluate
+                (App (App e1 e2')
+                     (Case varfs2 idfs2 [Alt (LitAlt (LitInt 1)) e3, Alt (LitAlt (LitInt 2)) e4])
+                )
+              )
 
 expectedVal1 :: CurrExpr
 expectedVal1 = CurrExpr Evaluate
     (App e1
-        (Case (Var idX) idX [Alt (LitAlt (LitInt 1)) e2, Alt (LitAlt (LitInt 2)) e2'])
+        (Case (varX) idX [Alt (LitAlt (LitInt 1)) e2, Alt (LitAlt (LitInt 2)) e2'])
     )
 
 expectedVal2 :: CurrExpr
 expectedVal2 = CurrExpr Evaluate
     (App e1
-        (App
-            (NonDet [(Assume Nothing xEq1 e2), (Assume Nothing xEq2 e2')])
-            (NonDet [(Assume Nothing xEq1 e3), (Assume Nothing xEq2 e3')])
+        (App e2
+            (Case (varX) idX
+                [ Alt (LitAlt (LitInt 1)) e3
+                , Alt (LitAlt (LitInt 2)) e3'])
         )
     )
 
 expectedVal3 :: CurrExpr
-expectedVal3 = CurrExpr Evaluate
-    (App e1
-        (App e2
-            (NonDet [(Assume Nothing xEq1 e3), (Assume Nothing xEq2 e3')])
-        )
-    )
+expectedVal3 = CurrExpr Evaluate (App e1 (App e2 e3))
 
 expectedVal4 :: CurrExpr
-expectedVal4 = CurrExpr Evaluate (App e1 (App e2 e3))
+expectedVal4 = CurrExpr Evaluate
+    (App e1
+        (App e4
+            (Case (varX) idX
+                [ Alt (LitAlt (LitInt 1)) e3
+                , Alt (LitAlt (LitInt 2)) e3'])
+        )
+    )
 
 expectedVal5 :: CurrExpr
 expectedVal5 = CurrExpr Evaluate
-    (App e1
+    (App
+        (App e1
+            (Case varX idX
+                [ Alt (LitAlt (LitInt 1)) e2
+                , Alt (LitAlt (LitInt 2)) e2'])
+        )
         (App e4
-            (NonDet [(Assume Nothing xEq1 e3), (Assume Nothing xEq2 e3')])
+            (Case varX idX
+                [ Alt (LitAlt (LitInt 1)) e3
+                , Alt (LitAlt (LitInt 2)) e3'])
         )
     )
 
-expectedVal6 :: CurrExpr
-expectedVal6 = CurrExpr Evaluate
-    (App
-        (App e1
-            (NonDet [(Assume Nothing xEq1 e2), (Assume Nothing xEq2 e2')])
+check6 :: CurrExpr -> Bool
+check6 (CurrExpr Evaluate (App a1 (Case (Var (Id _ TyLitInt)) (Id _ TyLitInt) alts))) =
+    a1 == (App e1
+            (Case (varX) idX
+                [ Alt (LitAlt (LitInt 1)) e2
+                , Alt (LitAlt (LitInt 2)) e2']
+            )
         )
-        (App e4
-            (NonDet [(Assume Nothing xEq1 e3), (Assume Nothing xEq2 e3')])
-        )
-    )
+    && (length alts == 4)
+    && (S.fromList (map (\(Alt (LitAlt _) e) -> e) alts) == S.fromList [e2', e3', e3, e4])
+check6 _ = False
 
 -- mergeCurrExpr helpers
 
@@ -211,21 +239,32 @@ idX = (Id xN TyLitInt)
 varX :: Expr
 varX = (Var idX)
 
-eqTo :: Expr
-eqTo = (Prim Eq (TyFun TyLitInt (TyFun TyLitInt (TyCon (Name "Bool" Nothing 0 Nothing) TYPE))))
+fs1N :: Name
+fs1N = (Name "fs" Nothing 0 Nothing)
 
-xEq1 :: Expr
-xEq1 = (App (App eqTo varX) (Lit (LitInt 1)))
+idfs1 :: Id
+idfs1 = (Id fs1N TyLitInt)
 
-xEq2 :: Expr
-xEq2 = (App (App eqTo varX) (Lit (LitInt 2)))
+varfs1 :: Expr
+varfs1 = (Var idfs1)
 
-createContext :: CurrExpr -> CurrExpr -> Context ()
-createContext ce1 ce2 = emptyContext s1 s2 ng idX
+fs2N :: Name
+fs2N = (Name "fs" Nothing 0 Nothing)
+
+idfs2 :: Id
+idfs2 = (Id fs2N TyLitInt)
+
+varfs2 :: Expr
+varfs2 = (Var idfs2)
+
+createContext :: CurrExpr -> CurrExpr -> Maybe EE.ExprEnv -> Maybe EE.ExprEnv -> Context ()
+createContext ce1 ce2 mEenv1 mEenv2 = emptyContext s1 s2 ng idX
     where
         kv = simpleKV
-        s1 = createTestState kv (M.fromList []) ce1 (PC.fromList [])
-        s2 = createTestState kv (M.fromList []) ce2 (PC.fromList [])
+        eenv1 = fromMaybe EE.empty mEenv1
+        eenv2 = fromMaybe EE.empty mEenv2
+        s1 = createTestState kv (M.fromList []) ce1 eenv1 (PC.fromList [])
+        s2 = createTestState kv (M.fromList []) ce2 eenv2 (PC.fromList [])
         ng = mkNameGen [s1, s2]
 
 
@@ -286,21 +325,21 @@ createTestPCs kv = [
                 PC.fromList [ -- simple test
                     AssumePC idX 1 
                     (ConsCond dconA var1 True)
-                    , ExtCond (mkEqIntExpr kv (Var idX) 1) True
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True]
+                    , ExtCond (mkEqIntExpr kv (varX) 1) True
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True]
               , PC.fromList [
                     AssumePC idX 1
                     (ConsCond dconA var1 False)
                     , AssumePC idX 1
                     (ConsCond dconB var1 False)
-                    , ExtCond (mkEqIntExpr kv (Var idX) 1) True
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True]
+                    , ExtCond (mkEqIntExpr kv (varX) 1) True
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True]
                , PC.fromList [ -- combination of AssumePCs and other PathCond-s
                     ConsCond dconA var1 False
                     , (ConsCond dconB var1 False)
                     , AssumePC idX 1
                     (ConsCond dconA var1 False)
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True]
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True]
                , PC.fromList [ -- Set of (AssumePCs id i pc) with i == 2 should be satisfiable, hence entire PathConds should be satisfiable
                     AssumePC idX 1
                     (ConsCond dconA var1 False)
@@ -310,7 +349,7 @@ createTestPCs kv = [
                     (ConsCond dconA var1 True)
                     , AssumePC idX 2
                     (ConsCond dconB var1 False)
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True]
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True]
                , PC.fromList [
                     AssumePC idX 1
                         (ConsCond dconA var1 False)
@@ -318,8 +357,8 @@ createTestPCs kv = [
                         (ConsCond dconB var1 False)
                     , AssumePC idY 1
                         (ConsCond dconA var1 True)
-                    , ExtCond (mkEqIntExpr kv (Var idX) 1) True
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True
+                    , ExtCond (mkEqIntExpr kv (varX) 1) True
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True
                     , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idY) 1) (mkEqIntExpr kv (Var idY) 2)) True]
                , PC.fromList [ -- simple nested AssumePCs
                     AssumePC idX 1
@@ -328,7 +367,7 @@ createTestPCs kv = [
                     , AssumePC idX 1
                     (AssumePC idY 1
                         (ConsCond dconB var1 False))
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True
                     , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idY) 1) (mkEqIntExpr kv (Var idY) 2)) True]
                , PC.fromList [ -- simple nested AssumePCs (negative test)
                     AssumePC idX 1
@@ -337,9 +376,9 @@ createTestPCs kv = [
                     , AssumePC idX 1
                     (AssumePC idY 1
                         (ConsCond dconB var1 False))
-                    , ExtCond (mkEqIntExpr kv (Var idX) 1) True
+                    , ExtCond (mkEqIntExpr kv (varX) 1) True
                     , ExtCond (mkEqIntExpr kv (Var idY) 1) True
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True
                     , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idY) 1) (mkEqIntExpr kv (Var idY) 2)) True]
                 -- more complex nested AssumePCs. Equivalent to (x=1 => ((y=1 => B) || (y=2 => not A && not B))) || (x=2 => ((y=1 => not A && not B))) 
                , PC.fromList [
@@ -358,14 +397,14 @@ createTestPCs kv = [
                     , AssumePC idX 2
                     (AssumePC idY 1
                         (ConsCond dconB var1 False))
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True
                     , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idY) 1) (mkEqIntExpr kv (Var idY) 2)) True]
                , PC.fromList [ -- Multiple solutions possible
                     AssumePC idX 1
                         (ConsCond dconB var1 True)
                     , AssumePC idX 2
                         (ConsCond dconA var1 True)
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True]
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True]
                , PC.fromList [ -- Multiple Data types
                     AssumePC idX 1
                         (ConsCond dconB var1 True)
@@ -375,11 +414,12 @@ createTestPCs kv = [
                         (ConsCond dconA var1 False)
                     , AssumePC idX 2
                         (ConsCond dconA var1 True)
-                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (Var idX) 1) (mkEqIntExpr kv (Var idX) 2)) True]
+                    , ExtCond (mkOrExpr kv (mkEqIntExpr kv (varX) 1) (mkEqIntExpr kv (varX) 2)) True]
                ]
 
-checkADTNumExpected :: [(TestNum, Result)]
-checkADTNumExpected = [(1, SAT), (2, UNSAT), (3, UNSAT), (4, SAT), (5, UNSAT), (6, SAT), (7, UNSAT), (8, SAT), (9, SAT), (10, SAT)]
+checkADTNumExpected :: [(TestNum, Result -> Bool)]
+checkADTNumExpected = [(1, (== SAT)), (2, (== UNSAT)), (3, (== UNSAT)), (4, (== SAT)), (5, (== UNSAT)), (6, (== SAT))
+                      , (7, (== UNSAT)), (8, (== SAT)), (9, (== SAT)), (10, (== SAT))]
 
 -- solveRelAssumeTests
 createTestIds :: [[Id]]
@@ -396,18 +436,18 @@ createTestIds = [ [Id var1N ty1T]
                 ]
 
 -- TODO: multiple solutions possible, add all of them
-solveADTNumExpected :: [(TestNum, (Result, Maybe Model))]
-solveADTNumExpected = [ (1, (SAT, Just (M.fromList [(var1N, Data dconA),(xN, Lit (LitInt 1))])))
-                      , (2, (UNSAT, Nothing))
-                      , (3, (UNSAT, Nothing))
-                      , (4, (SAT, Just (M.fromList [(var1N, Data dconA), (xN, Lit (LitInt 2))])))
-                      , (5, (UNSAT, Nothing))
-                      , (6, (SAT, Just (M.fromList [(var1N, Data dconB), (xN, Lit (LitInt 2)), (yN, Lit (LitInt 1))])))
-                      , (7, (UNSAT, Nothing))
-                      , (8, (SAT, Just (M.fromList [(var1N, Data dconA), (xN, Lit (LitInt 2)), (yN, Lit (LitInt 2))])))
-                      , (9, (SAT, Just (M.fromList [(var1N, Data dconB), (xN, Lit (LitInt 1))])))
-                      , (10, (SAT, Just (M.fromList [(var1N, Data dconB), (var2N, Data dconD)
-                        , (xN, Lit (LitInt 1))])))
+solveADTNumExpected :: [(TestNum, (Result, Maybe Model) -> Bool)]
+solveADTNumExpected = [ (1, (== (SAT, Just (M.fromList [(var1N, Data dconA),(xN, Lit (LitInt 1))]))))
+                      , (2, (== (UNSAT, Nothing)))
+                      , (3, (== (UNSAT, Nothing)))
+                      , (4, (== (SAT, Just (M.fromList [(var1N, Data dconA), (xN, Lit (LitInt 2))]))))
+                      , (5, (== (UNSAT, Nothing)))
+                      , (6, (== (SAT, Just (M.fromList [(var1N, Data dconB), (xN, Lit (LitInt 2)), (yN, Lit (LitInt 1))]))))
+                      , (7, (== (UNSAT, Nothing)))
+                      , (8, (== (SAT, Just (M.fromList [(var1N, Data dconA), (xN, Lit (LitInt 2)), (yN, Lit (LitInt 2))]))))
+                      , (9, (== (SAT, Just (M.fromList [(var1N, Data dconB), (xN, Lit (LitInt 1))]))))
+                      , (10, (== (SAT, Just (M.fromList [(var1N, Data dconB), (var2N, Data dconD)
+                        , (xN, Lit (LitInt 1))]))))
                       ]
 
 -- Helper Functions
@@ -477,9 +517,9 @@ createInitState src entry config = do
     let (init_state, _ , _) = initState exg2 False (T.pack entry) mb_modname (mkCurrExpr Nothing Nothing) config
     return (init_state)
 
-createTestState :: KnownValues -> TypeEnv -> CurrExpr -> PathConds -> State ()
-createTestState kv tenv currExpr pc = State {
-      expr_env = EE.empty
+createTestState :: KnownValues -> TypeEnv -> CurrExpr -> EE.ExprEnv -> PathConds -> State ()
+createTestState kv tenv currExpr eenv pc = State {
+      expr_env = eenv
     , type_env = tenv
     , curr_expr = currExpr
     , path_conds = pc
@@ -498,7 +538,7 @@ createTestState kv tenv currExpr pc = State {
     }
 
 createTestStates :: KnownValues -> [PathConds] -> [State ()]
-createTestStates kv pcs = map (createTestState kv (M.fromList [(ty1N, ty1), (ty2N, ty2)]) defaultCE) pcs
+createTestStates kv pcs = map (createTestState kv (M.fromList [(ty1N, ty1), (ty2N, ty2)]) defaultCE EE.empty) pcs
     where defaultCE = (CurrExpr Evaluate (Lit (LitInt 0)))
 
 createTestBindings :: Bindings
