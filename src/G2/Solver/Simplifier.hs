795bcc17ea9f9819d48cd214fefe77ae36c45d4a
Bill Hallahan <billthallahan@gmail.com>
2019-07-26 18:02:02 -0400

    Comments

--- src/G2/Solver/Simplifier.hs
+++ src/G2/Solver/Simplifier.hs
@@ -6,8 +6,10 @@ module G2.Solver.Simplifier ( Simplifier (..)
 import G2.Language
 
 class Simplifier simplifier where
+	-- | Simplifies a PC, by converting it to a form that is easier for the Solver's to handle
 	simplifyPC :: forall t . simplifier -> State t -> PathCond -> [PathCond]
 
+	-- | Reverses the affect of simplification in the model, if needed.
 	reverseSimplification :: forall t . simplifier -> State t -> Model -> Model
 
 -- | A simplifier that does no simplification
