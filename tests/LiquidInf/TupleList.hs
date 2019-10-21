312a2b9248bfde5bb67f9d729a261db55611c860
Bill Hallahan <billthallahan@gmail.com>
2019-10-21 18:27:20 -0400

    New test

---
+++ tests/LiquidInf/TupleList.hs
@@ -0,0 +1,8 @@
+module TupleList where
+
+{-@ sumNonNeg :: [({ x:Int | x >= 0}, Int)] -> { y:Int | y >= 0} @-}
+sumNonNeg :: [(Int, Int)] -> Int
+sumNonNeg = sumFst
+
+sumFst :: [(Int, Int)] -> Int
+sumFst = foldr (+) 0 . map fst
