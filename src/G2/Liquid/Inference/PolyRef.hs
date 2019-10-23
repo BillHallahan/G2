f68b70e04257f0ad7d8daafbc5e4884eaa8f7dcd
Bill Hallahan <billthallahan@gmail.com>
2019-10-23 01:34:56 -0400

    PolyRef now recursively gathers bound arguments

--- src/G2/Liquid/Inference/PolyRef.hs
+++ src/G2/Liquid/Inference/PolyRef.hs
@@ -12,18 +12,51 @@ import Debug.Trace
 
 import Debug.Trace
 
-extractPolyBound :: Expr -> HM.HashMap Id [Expr]
-extractPolyBound e = extractPolyBound' e
+data PolyBound = PolyBound [Expr] [PolyBound] deriving (Read, Show)
+
+
+extractPolyBound :: Expr -> [PolyBound] -- HM.HashMap Id [Expr]
+extractPolyBound e
+    | Data dc:_ <- unApp e =
+        let
+            bound = leadingTyForAllBindings dc
+            m = extractPolyBound' e
+
+            bound_es = map (\i -> HM.lookupDefault [] i m) bound
+        in
+        map (\es -> PolyBound es (mergePolyBound . transpose $ map extractPolyBound es)) bound_es
+    | otherwise = []
+
+
+mergePolyBound :: [[PolyBound]] -> [PolyBound]
+mergePolyBound = mapMaybe (\pb -> case pb of
+                                (p:pbb) -> Just $ foldr mergePolyBound' p pbb
+                                [] -> Nothing)
+
+mergePolyBound' :: PolyBound -> PolyBound -> PolyBound
+mergePolyBound' (PolyBound es1 pb1) (PolyBound es2 pb2) =
+    PolyBound (es1 ++ es2) (map (uncurry mergePolyBound') $ zip pb1 pb2)
+
+-- extractPolyBound :: Expr -> [PolyBound] -- HM.HashMap Id [Expr]
+-- extractPolyBound e
+--     | Data dc:_ <- unApp e =
+--         let
+--             bound = leadingTyForAllBindings dc
+--             m = extractPolyBound' e
+
+--             bound_es = map (\i -> HM.lookupDefault [] i m) bound
+--         in
+--         map (\es -> PolyBound es (concatMap extractPolyBound es)) bound_es
+--     | otherwise = []
+
+
 
 extractPolyBound' :: Expr -> HM.HashMap Id [Expr]
 extractPolyBound' e
     | Data dc:es <- unApp e =
     let
-        (tyses, es') = partition (isType) es
-        tyses' = map (\(Type t) -> t) tyses
+        es' = filter (not . isType) es
 
-        bound = leadingTyForAllBindings dc
-        bound_tyses = zip bound tyses'
         argtys = argumentTypes . PresType . inTyForAlls $ typeOf dc
 
         argtys_es = zip argtys es'
@@ -52,6 +85,7 @@ substTypes t e
     | t':ts <- unTyApp t
     , e':es <- unApp e =
         mkApp $ e':substTypes' ts es
+substTypes _ e = e
 
 substTypes' :: [Type] -> [Expr] -> [Expr]
 substTypes' (t:ts) (Type _:es) = Type t:substTypes' ts es
