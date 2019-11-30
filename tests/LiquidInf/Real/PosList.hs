ed9709bb7dcbc243a03a7804218641d9b7aacc13
Bill Hallahan <billthallahan@gmail.com>
2019-11-30 13:51:56 -0500

    Moving files

---
+++ tests/LiquidInf/Real/PosList.hs
@@ -0,0 +1,23 @@
+{-@ LIQUID "--no-termination" @-}
+
+module PosList where
+
+data List a = Emp
+            | (:+:) a (List a)
+              deriving (Eq, Ord, Show)
+
+{-@ filterAndSum :: List { x:Int | x >= 0 } -> { y:Int | y >= 0  } @-}
+filterAndSum :: List Int -> Int
+filterAndSum xs = sumList (filterPos xs)
+
+{-@ sumList :: List { x:Int | x >= 0 } -> Int @-}
+sumList :: List Int -> Int
+sumList Emp = 0
+sumList (x :+: xs) = x + sumList xs
+
+{-@ filterPos :: List Int -> List Int @-}
+filterPos :: List Int -> List Int
+filterPos Emp = Emp
+filterPos (x :+: xs)
+	| x >= 0 =  x :+: filterPos xs
+	| otherwise = filterPos xs
