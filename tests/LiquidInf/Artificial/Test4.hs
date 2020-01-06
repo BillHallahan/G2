61975f0be51526d89387c1de2884cf6819bfc672
Bill Hallahan <billthallahan@gmail.com>
2020-01-05 22:50:29 -0500

    New test

---
+++ tests/LiquidInf/Artificial/Test4.hs
@@ -0,0 +1,17 @@
+{-@ LIQUID "--short-names" @-}
+{-@ LIQUID "--no-termination" @-}
+{-@ LIQUID "--prune-unsorted" @-}
+
+module Combined where
+
+{-@ f :: {v:Int | v == 0} @-}
+f :: Int
+f = g h 0
+
+{-@ g :: (a -> a) -> a -> a @-}
+g :: (a -> a) -> a -> a
+g _ x = x
+
+{-@ h :: Int -> Int @-}
+h   :: Int -> Int
+h x = x
