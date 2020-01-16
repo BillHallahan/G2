b534ec41f4cb58cb2269b22e3fe5d6671417548f
Bill Hallahan <billthallahan@gmail.com>
2020-01-16 00:42:43 -0500

    New test case

---
+++ tests/LiquidInf/Artificial/Pos/Test8.hs
@@ -0,0 +1,25 @@
+{-@ LIQUID "--short-names" @-}
+{-@ LIQUID "--no-termination" @-}
+{-@ LIQUID "--prune-unsorted" @-}
+
+module Combined where
+
+import qualified Data.Map as M
+
+data D a = Emp
+         | R a
+         deriving (Eq, Ord, Show)
+
+{-@ f :: D { x:Int | 0 <= x} -> M.Map {v:Int | 0 <= v } Int @-}
+f :: D Int -> M.Map Int Int
+f xs = M.map (\x -> 1) (g h M.empty xs)
+
+g :: (a -> b -> b) -> b -> D a -> b
+g _  b Emp = b
+g op b (R x) = x `op` b
+
+{-@ h :: { x:Int | 0 <= x}
+	  -> M.Map { x:Int | 0 <= x} (D Int)
+	  -> M.Map Int (D Int) @-}
+h :: Int -> M.Map Int (D Int) -> M.Map Int (D Int)
+h k m = M.insert k (R 1) m
