97b9ed4d99fe2bc001dd8539f85f76f3254dc364
Bill Hallahan <billthallahan@gmail.com>
2020-10-29 00:36:46 -0400

    Minor

--- src/G2/Liquid/Inference/G2Calls.hs
+++ src/G2/Liquid/Inference/G2Calls.hs
@@ -255,7 +255,7 @@ inferenceReducerHalterOrderer infconfig config solver simplifier entry mb_modnam
                   -- :<~> searched_below
                   :<~> lh_max_outputs
                   :<~> SwitchEveryNHalter (switch_after config)
-                  :<~> LHLimitSameAbstractedHalter 5
+                  :<~> LHLimitSameAbstractedHalter 10
                   :<~> AcceptIfViolatedHalter
                   :<~> timer_halter)
         , SomeOrderer (ToOrderer $ IncrAfterN 1000 ADTHeightOrderer))
