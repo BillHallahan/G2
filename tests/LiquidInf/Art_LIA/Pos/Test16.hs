5f6a7ed4595c0b8bbdd9403a20d3019e0cdbd35c
Bill Hallahan <billthallahan@gmail.com>
2020-11-06 00:46:52 -0500

    New test

---
+++ tests/LiquidInf/Art_LIA/Pos/Test16.hs
@@ -0,0 +1,12 @@
+{-@ LIQUID "--short-names" @-}
+{-@ LIQUID "--no-termination" @-}
+{-@ LIQUID "--prune-unsorted" @-}
+
+module Combined (f) where
+
+{-@ f :: [{ xs : Int | xs > 0 }] @-}
+f :: [Int]
+f = empty (0, 0)
+
+empty :: (Ord k) => (k, Int) -> [Int]
+empty _ = []
