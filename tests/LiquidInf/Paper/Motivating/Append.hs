2d2512b4233b01615d2d8df6c923835329c995f6
Bill Hallahan <billthallahan@gmail.com>
2020-11-07 15:09:15 -0500

    New motivating example

--- tests/LiquidInf/Paper/Motivating/Append.hs
+++ tests/LiquidInf/Paper/Motivating/Append.hs
@@ -14,25 +14,13 @@ size (x:xs) = 1 + size xs
 {-@ invariant {v:[a] | 0 <= size v} @-}
 {-@ invariant {v:[a] | len v == size v} @-}
 
-{-@ measure sumsize @-}
-sumsize :: [[a]] -> Int
-sumsize [] = 0
-sumsize (xs:xss)   = size xs + sumsize xss
-    
-concat :: [[a]] -> [a]
-concat [] = []
-concat [xs] = xs
-concat (xs:ys:xss) = concat ((append xs ys):xss)
-
+{-@ append :: xs:[a] -> ys:[a] -> { zs:[a] | size xs == size zs } @-}
 append :: [a] -> [a] -> [a]
 append xs [] = xs
 append [] ys = ys
 append (x:xs) ys = x:append xs ys
 
-{-@ prop_concat :: [a] -> [a] -> { v:Bool | v } @-}
-prop_concat :: [a] -> [a] -> Bool
-prop_concat xs ys =
-    let
-        xss = [xs, ys]
-    in
-    size (concat xss) == size xs + size ys
+{-@ prop_append :: [a] -> [a] -> { v:Bool | v } @-}
+prop_append :: [a] -> [a] -> Bool
+prop_append xs ys =
+    size (append xs ys) >= size xs
