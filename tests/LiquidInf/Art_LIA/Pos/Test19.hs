4f6c25cf1f75fef934ff1f9075969b23ef63b22b
Bill Hallahan <billthallahan@gmail.com>
2020-11-10 05:52:52 -0500

    Passing test with measure

--- tests/LiquidInf/Art_LIA/Pos/Test19.hs
+++ tests/LiquidInf/Art_LIA/Pos/Test19.hs
@@ -1,7 +1,7 @@
 {-@ LIQUID "--no-termination" @-}
 {-@ LIQUID "--maxparams=3"    @-}
 
-module LazyQueue (Q, t, size, qsize, size) where
+module LazyQueue (Q, t, size, qsize, size, backSize) where
 
 {-@ die :: {v:String | false} -> a @-}
 die x = error x
@@ -14,11 +14,10 @@ size      :: [a] -> Int
 size []     = 0
 size (_:xs) = 1 + size xs
 
-{-@ data Q a = Q {
-      front :: [a]
-    , back  :: {v:[a] | size v == 0}
-    }
-@-}
+{-@ measure backSize @-}
+backSize :: Q a -> Int
+backSize (Q { back = b}) = size b
+
 data Q a = Q
   { front :: [a]
   , back  :: [a]
@@ -30,10 +29,11 @@ qsize (Q { front = f, back = b}) = size f + size b
 
 data X = X
 
-{-@ t :: Int -> { q:Q X | qsize q > 0 } -> { q2:[X] | size q2 <= qsize q } @-}
+{-@ t :: Int -> { q:Q X | qsize q > 0 && backSize q == 0 } -> { q2:[X] | size q2 <= qsize q } @-}
 t :: Int -> Q X -> [X]
 t n  (Q f b) = c (tl f)
 
+-- {-@ c :: [a] -> { xs:[a] | size xs == 0 } @-} 
 c :: [a] -> [a]
 c f = []
 
