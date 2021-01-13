2da08d980fdf8078912f7016283c1ff4338413aa
Bill Hallahan <billthallahan@gmail.com>
2021-01-13 13:00:55 -0500

    Adjusts test

--- tests/Liquid/Polymorphism/Poly6.hs
+++ tests/Liquid/Polymorphism/Poly6.hs
@@ -1,15 +1,8 @@
 module Prop6 where
 
-{-@ type TRUE = {v:Bool | v} @-}
-
 data List a = Emp | C a
 
-{-@ measure size :: List a -> Int
-    size (Emp) = 0
-    size (C x) = 1
-  @-}
-
-{-@ invariant {v:List a | 0 <= size v} @-}
+{-@ type TRUE = {v:Bool | v} @-}
 
 {-@ f :: List Int -> TRUE @-}
 f :: List Int -> Bool
@@ -20,11 +13,6 @@ f xs =
     case xs of
         Emp -> case l2 of { 0 -> True; _ -> False}
         C _ -> case l2 of { 1 -> True; _ -> False}
-
-{-@ len :: xs:List a -> { r:Int | size xs == r } @-}
-len :: List a -> Int
-len Emp = 0
-len (C x) = 1
 
 g :: List a -> List a
 g x = x
