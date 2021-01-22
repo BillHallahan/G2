7998301fd3f56546d6ad214986a682dfbcf1fa08
John Kolesar <jck248@cornell.edu>
2021-01-22 15:59:44 -0500

    Minor change to test file

--- tests/RewriteRules/SimpleRules.hs
+++ tests/RewriteRules/SimpleRules.hs
@@ -7,6 +7,6 @@ negation :: Int -> Int
 negation n = -n
 
 {-# RULES
-  "addOneCommutative" forall n . addOne n = 1 + n
-  "negationIdempotent" forall n . negation (negation n) = n
+"addOneCommutative" forall n . addOne n = 1 + n
+"doubleNegative" forall n . negation (negation n) = n
   #-}
