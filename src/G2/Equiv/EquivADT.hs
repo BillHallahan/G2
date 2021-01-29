f5fb9f9c4ce54b13c95f39fb22b9912113d8ffbb
John Kolesar <jck248@cornell.edu>
2021-01-29 16:28:56 -0500

    Reworked exprPairing

--- src/G2/Equiv/EquivADT.hs
+++ src/G2/Equiv/EquivADT.hs
@@ -12,14 +12,15 @@ findEquivVars :: State t ->
                  State t ->
                  Expr ->
                  Expr ->
-                 Maybe (HS.HashSet (Name, Name))
+                 Maybe (HS.HashSet (Expr, Expr))
 -- get ExprEnv from both states
 -- look up symbolic vars in the ExprEnv
 -- check concretizations for each of them
 findEquivVars s1 s2 e1 e2 =
   -- TODO need anything else from the state, or just ExprEnv?
-  namePairing s1 s2 e1 e2 HS.empty
+  exprPairing s1 s2 e1 e2 HS.empty
 
+{-
 namePairing :: State t ->
                State t ->
                Expr ->
@@ -52,8 +53,9 @@ namePairing s1@(State {expr_env = h1}) s2@(State {expr_env = h2}) e1 e2 pairs =
     (App _ _, _) -> error "not fully evaluated"
     (_, App _ _) -> error "not fully evaluated"
     _ -> error "catch-all case"
+-}
 
-idPairing :: State t -> State t -> (Id, Id) -> Maybe (HS.HashSet (Name, Name))
+idPairing :: State t -> State t -> (Id, Id) -> Maybe (HS.HashSet (Expr, Expr))
 idPairing s1 s2 (i1, i2) =
   findEquivVars s1 s2 (Var i1) (Var i2)
 
@@ -64,6 +66,7 @@ idPairing s1 s2 (i1, i2) =
 -- for the Just case, it returns a set of proof obligations
 -- the returned pairs need to be shown to be equivalent
 -- TODO does this need an extra argument to be an accumulator?
+{-
 exprPairing :: State t ->
                State t ->
                Expr ->
@@ -87,6 +90,41 @@ exprPairing s1@(State {expr_env = h1}) s2@(State {expr_env = h2}) e1 e2 =
     (App _ _, _) -> HS.insert (e1, e2) HS.empty
     (_, App _ _) -> HS.insert (e1, e2) HS.empty
     _ -> HS.empty
+-}
+
+-- TODO new version
+exprPairing :: State t ->
+               State t ->
+               Expr ->
+               Expr ->
+               HS.HashSet (Expr, Expr) ->
+               Maybe HS.HashSet (Expr, Expr)
+exprPairing s1@(State {expr_env = h1}) s2@(State {expr_env = h2}) e1 e2 pairs =
+  case (e1, e2) of
+    (Data d1, Data d2) | d1 == d2 -> Just pairs
+                       | otherwise -> Nothing
+    (Data _, _) -> Nothing
+    (_, Data _) -> Nothing
+    (Var i, _) | E.isSymbolic i -> Just (HS.insert (e1, e2) pairs)
+               | Just e <- E.lookup (idName i) h1 -> exprPairing s1 s2 e e2 pairs
+               | otherwise -> Nothing
+    (_, Var i) | E.isSymbolic i -> Just (HS.insert (e1, e2) pairs)
+               | Just e <- E.lookup (idName i) h2 -> exprPairing s1 s2 e1 e pairs
+               | otherwise -> Nothing
+    (Lit l1, Lit l2) | l1 == l2 -> Just pairs
+                     | otherwise -> Nothing
+    (Lit _, _) -> Nothing
+    (_, Lit _) -> Nothing
+    (Prim _ _, Prim _ _) -> Just (HS.insert (e1, e2) pairs)
+    (Prim _ _, _) -> Nothing
+    (_, Prim _ _) -> Nothing
+    (App _ _, _) -> Just (HS.insert (e1, e2) pairs)
+    (_, App _ _) -> Just (HS.insert (e1, e2) pairs)
+    -- TODO is this sufficient for the Lam case?
+    (Lam _ _ _, Lam _ _ _) -> Just (HS.insert (e1, e2) pairs)
+    (Lam _ _ _, _) -> Nothing
+    (_, Lam _ _ _) -> Nothing
+    _ -> error "catch-all case"
 
 matchAll :: [(Id, Id)] ->
             (State t, State t) ->
