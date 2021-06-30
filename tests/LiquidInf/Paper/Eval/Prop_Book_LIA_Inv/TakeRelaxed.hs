f2bdc16307920fff465b7bd1f4ef6c16e47ce60b
Bill Hallahan <billthallahan@gmail.com>
2021-06-29 20:33:38 -0400

    More benchmarks

---
+++ tests/LiquidInf/Paper/Eval/Prop_Book_LIA_Inv/TakeRelaxed.hs
@@ -0,0 +1,14 @@
+{-@ LIQUID "--no-termination" @-}
+
+module TakeRelaxed (test) where
+
+import Prelude hiding (take)
+
+take :: Int -> [a] -> [a]
+take 0 _       = []
+take _ []      = []
+take n (x:xs)  = x : take (n-1) xs
+
+{-@ test :: [{ xs:[String] | len xs == 2 }] @-}
+test = [ take 2  ["cat", "dog", "mouse"]
+       , take 20 ["cow", "goat"]        ]
