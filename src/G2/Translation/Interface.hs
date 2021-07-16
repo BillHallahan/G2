bf407bb31759d115ace4527cac3d3f66830e101b
Bill Hallahan <billthallahan@gmail.com>
2021-07-15 21:29:14 -0400

    Adjusted method of finding inital function

--- src/G2/Translation/Interface.hs
+++ src/G2/Translation/Interface.hs
@@ -90,7 +90,7 @@ translateLoaded proj src libs tr_con config = do
 
   -- Now the stuff with the actual target
   (_, _, exg2) <- hskToG2ViaEMS tr_con tar_ems lib_nm lib_tnm
-  let mb_modname = Just . last . map snd $ exg2_mod_names exg2
+  let mb_modname = lookup (head src) $ exg2_mod_names exg2
   let h_exp = exg2_exports exg2
 
   -- putStrLn $ "exg2_deps = " ++ show (exg2_deps exg2)
