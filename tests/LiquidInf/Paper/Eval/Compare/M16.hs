b1abb9beda3161d481155d52c7df92a9d24c2db4
Bill Hallahan <billthallahan@gmail.com>
2021-10-08 23:25:26 -0400

    More converted benchmarks

---
+++ tests/LiquidInf/Paper/Eval/Compare/M16.hs
@@ -0,0 +1,11 @@
+{-@ LIQUID "--no-termination" @-}
+
+module M16 (main) where
+
+{-@ main :: Int -> Int -> { b:Bool | b } @-}
+main :: Int -> Int -> Bool
+main i j = case while (i, j, i, j) of
+                (i', j', x, y) -> if i' == j' then y == 0 else True
+
+while :: (Int, Int, Int, Int) -> (Int, Int, Int, Int)
+while (i, j, x, y) = if x /= 0 then while (i, j, x - 1, y - 1) else (i, j, x, y)
