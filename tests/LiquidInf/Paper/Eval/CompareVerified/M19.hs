23955a37f5d165719bbc457a10ec644d5a8e2287
Bill Hallahan <billthallahan@gmail.com>
2021-10-11 01:17:53 -0400

    Minor changes

--- tests/LiquidInf/Paper/Eval/CompareVerified/M19.hs
+++ tests/LiquidInf/Paper/Eval/CompareVerified/M19.hs
@@ -8,7 +8,11 @@ main n m =
     case while n m (0, m) of
         (_, y') -> n == y'
 
-{-@ while :: Int -> Int -> (Int, Int) -> (Int, Int) @-}
+{-@ while :: n:Int
+          -> m:{ m:Int | m < n }
+          -> t:{ t:({x:Int | x <= n }, Int) | (fst t < m => snd t = m)
+                                           && (fst t >= m => snd t == fst t)  }
+          -> (Int, { y:Int | y == n }) @-}
 while :: Int -> Int -> (Int, Int) -> (Int, Int)
 while n m (x, y) =
     if x < n then while n m (x + 1, if x + 1 > m then y + 1 else y) else (x, y)
