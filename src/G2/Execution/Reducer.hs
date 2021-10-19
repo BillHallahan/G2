b2fd9dd1add000e200a746926541f21a7e7eea56
Bill Hallahan <billthallahan@gmail.com>
2021-10-19 14:13:06 -0400

    Cleanup

--- src/G2/Execution/Reducer.hs
+++ src/G2/Execution/Reducer.hs
@@ -85,8 +85,6 @@ module G2.Execution.Reducer ( Reducer (..)
 import G2.Config.Config
 import qualified G2.Language.ExprEnv as E
 import G2.Execution.Rules
-import G2.Execution.Merging.Zipper hiding (Status (..))
-import qualified G2.Execution.Merging.Zipper as Z
 import G2.Language
 import qualified G2.Language.Monad as MD
 import qualified G2.Language.PathConds as PC
@@ -1659,7 +1657,7 @@ runReducerMerge red hal simplifier s b = do
     let res = mapProcessed state pr'
     return (res, b')
 
--- | work_func for both WorkGraph and ZipperTree. Reduces the ExState `exS` and returns the reduceds along with the appropriate Status
+-- | work_func for both WorkGraph. Reduces the ExState `exS` and returns the reduceds along with the appropriate Status
 runReducerMerge' :: (Eq t, Named t, Reducer r rv t, Halter h hv t, Simplifier simplifier)
                  => ExState rv hv sov t
                  -> (r, h, simplifier, Bindings, Processed (ExState rv hv sov t))
@@ -1717,55 +1715,7 @@ updateOrdererEvery n ord_f ex_s b =
             (m_next, map (\s -> s { order_val = (0, m_next)}) m_exs_s)
         else (m_curr, map (\s -> s { order_val = (m_up + 1, m_curr)}) ex_s)
 
--- runReducerMerge2 :: (Show t, Eq t, Named t, Reducer r rv t, Halter h hv t, Simplifier simplifier)
---                  => r -> h -> simplifier -> State t -> Bindings
---                  -> IO (Processed (State t), Bindings)
--- runReducerMerge2 red hal simplifier s b = do
---     let pr = Processed {accepted = [], discarded = []}
---         s' = ExState {state = s, halter_val = initHalt hal s, reducer_val = initReducer red s, order_val = Nothing}
---         -- workGraph = ZinitGraph s' (red, hal, simplifier, b, pr) runReducerMerge' mergeStates resetSaturated logState
---         zipper = initZipper s' (red, hal, simplifier, b, pr) runReducerMerge2' mergeStates resetMergingZipper -- logState
-
---     -- (_, (_, _, _, b', pr')) <- Zwork workGraph
---     (_, _, _, b', pr') <- evalZipper zipper
---     let res = mapProcessed state pr'
---     return (res, b')
-
--- -- | work_func for both WorkGraph and ZipperTree. Reduces the ExState `exS` and returns the reduceds along with the appropriate Status
--- runReducerMerge2' :: (Show t, Eq t, Named t, Reducer r rv t, Halter h hv t, Simplifier simplifier)
---                  => ExState rv hv sov t
---                  -> (r, h, simplifier, Bindings, Processed (ExState rv hv sov t))
---                  -> IO ([(ExState rv hv sov t)], (r, h, simplifier, Bindings, Processed (ExState rv hv sov t)), Z.Status)
--- runReducerMerge2' exS (red, hal, simplifier, bdg, pr) = do
---     let rs@(ExState {state = s, halter_val = h_val, reducer_val = r_val}) = exS
---         ps = processedToState pr -- need to deal with processed
---         hc = stopRed hal h_val ps s
---     case hc of
---         Accept -> return ([rs], (red, hal, simplifier, bdg, pr {accepted = rs:accepted pr}), Z.Accept)
---         Discard -> return ([rs], (red, hal, simplifier, bdg, pr {discarded = rs:discarded pr}), Z.Discard)
---         _ -> do
---             (reducerRes, reduceds, bdg', red') <- redRules red r_val s bdg
---             let reduceds' = map (\(r, rv) -> (r {num_steps = num_steps r + 1}, rv)) reduceds
---             let r_vals = updateWithAll red reduceds' ++ error "List returned by updateWithAll is too short.."
---             let new_states = map fst reduceds'
---             let reduceds'' = map (\(s', r_val') -> rs { state = s'
---                                                       , reducer_val = r_val'
---                                                       , halter_val = stepHalter hal h_val ps new_states s'})
---                                                       $ zip new_states r_vals
---             let status = case reducerRes of
---                                 MergePoint -> Z.Mergeable
---                                 MaxDepth -> Z.WorkSaturated
---                                 Split -> Z.Split
---                                 _ -> Z.WorkNeeded
---             return (reduceds'', (red', hal, simplifier, bdg', pr), status)
-
--- -- | Reset counts of all unmerged Case Expressions to 0
--- resetSaturated :: (ExState rv hv sov t) -> (ExState rv hv sov t)
--- resetSaturated rs@(ExState {state = s}) =
---     let s' = s {cases = M.empty, depth_exceeded = False}
---     in rs {state = s'}
-
--- | Merge Func for WorkGraph and Tree Zipper. Attempts to merge 2 states
+-- | Merge Func for WorkGraph. Attempts to merge 2 states
 mergeStates :: (Eq t, Named t, Reducer r rv t, Halter h hv t, Simplifier simplifier)
             => (ExState rv hv sov t)
             -> (ExState rv hv sov t)
@@ -1793,26 +1743,6 @@ switchStates ex_s b@(_, hal, _, _, pr) =
     if not $ discardOnStart hal (halter_val ex_s') ps (state ex_s')
         then Just (ex_s', b)
         else Nothing
-
--- -- | Function for Logging State
--- logState :: Show t => ExState rv hv sov t -> (r,h,s,Bindings,p) -> String
--- logState (ExState { state = s }) (_,_,_,b,_) = pprExecStateStr s b
-
--- -- | Remove any MergePtFrame-s in the exec_stack of the ExState. Called when we float states to Root when tree grows too deep
--- resetMergingZipper :: (ExState rv hv sov t) -> (ExState rv hv sov t)
--- resetMergingZipper rs@(ExState {state = s}) =
---     let st = exec_stack s
---         st' = delMergePtFrames st
---         s' = s {exec_stack = st', cases = M.empty, depth_exceeded = False}
---     in rs {state = s'}
-
--- delMergePtFrames :: Stck.Stack Frame -> Stck.Stack Frame
--- delMergePtFrames st =
---     let xs = Stck.toList st
---         xs' = filter (\fr -> case fr of
---                                 MergePtFrame _ -> False
---                                 _ -> True) xs
---     in Stck.fromList xs'
 
 numStates :: M.Map b [ExState rv hv sov t] -> Int
 numStates = sum . map length . M.elems
