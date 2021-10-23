a887a067b7650811e9275b0f33e96749cf989692
Bill Hallahan <billthallahan@gmail.com>
2021-10-23 12:10:34 -0400

    Split up G2 State Monad into smaller pieces

--- src/G2/Language/Monad/Naming.hs
+++ src/G2/Language/Monad/Naming.hs
@@ -17,38 +17,38 @@ import G2.Language.Monad.Support
 
 import qualified Data.Text as T
 
-doRenameN :: (ExState s m, Named a) => Name -> a -> m a
+doRenameN :: (NamingM s m, Named a) => Name -> a -> m a
 doRenameN n a = withNG $ \ng -> doRename n ng a
 
-doRenamesN :: (ExState s m, Named a) => [Name] -> a -> m a
+doRenamesN :: (NamingM s m, Named a) => [Name] -> a -> m a
 doRenamesN ns a = withNG $ \ng -> doRenames ns ng a
 
-renameAllN :: (ExState s m, Named a) => a -> m a
+renameAllN :: (NamingM s m, Named a) => a -> m a
 renameAllN a = withNG (renameAll a)
 
-freshSeededStringN :: ExState s m => T.Text -> m Name
+freshSeededStringN :: NamingM s m => T.Text -> m Name
 freshSeededStringN t = withNG (freshSeededString t)
 
-freshSeededStringsN :: ExState s m => [T.Text] -> m [Name]
+freshSeededStringsN :: NamingM s m => [T.Text] -> m [Name]
 freshSeededStringsN t = withNG (freshSeededStrings t)
 
-freshSeededNameN :: ExState s m => Name -> m Name
+freshSeededNameN :: NamingM s m => Name -> m Name
 freshSeededNameN n = withNG (freshSeededName n)
 
-freshSeededNamesN :: ExState s m => [Name] -> m [Name]
+freshSeededNamesN :: NamingM s m => [Name] -> m [Name]
 freshSeededNamesN ns = withNG (freshSeededNames ns)
 
-freshNameN :: ExState s m => m Name
+freshNameN :: NamingM s m => m Name
 freshNameN = withNG (freshName)
 
-freshNamesN :: ExState s m => Int -> m [Name]
+freshNamesN :: NamingM s m => Int -> m [Name]
 freshNamesN i = withNG (freshNames i)
 
-freshIdN :: ExState s m => Type -> m Id
+freshIdN :: NamingM s m => Type -> m Id
 freshIdN t = withNG (freshId t)
 
-freshSeededIdN :: (Named n, ExState s m) => n -> Type -> m Id
+freshSeededIdN :: (Named n, NamingM s m) => n -> Type -> m Id
 freshSeededIdN n t = withNG (freshSeededId n t)
 
-freshIdsN :: ExState s m => [Type] -> m [Id]
+freshIdsN :: NamingM s m => [Type] -> m [Id]
 freshIdsN ts = withNG (freshIds ts)
