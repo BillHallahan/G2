1311424fe2dcd8a9ec80641cd4589422af9c96c8
Bill Hallahan <billthallahan@gmail.com>
2021-10-24 13:16:48 -0400

    Cleanup

--- src/G2/Execution/Merging/StateMerging.hs
+++ src/G2/Execution/Merging/StateMerging.hs
@@ -204,7 +204,6 @@ isMergeable s1 s2 =
     && (tags s1 == tags s2)
     && (track s1 == track s2)
     && (type_classes s1 == type_classes s2)
-    -- && (cases s1) == (cases s2)
     && (isEmpty $ model s1)
     && (isEmpty $ model s2)
 
@@ -280,25 +279,9 @@ newMergeCurrExpr = do
         let CurrExpr er1 e1 = curr_expr s1
             CurrExpr er2 e2 = curr_expr s2
 
-        e <- newMergeCurrExpr' (known_values s1) e1 e2
+        e <- newCaseExpr e1 e2
         
         assert (er1 == er2) . return $ CurrExpr er1 e
-
-newMergeCurrExpr' :: KnownValues -> Expr -> Expr -> MergeM t Expr
--- newMergeCurrExpr' kv v1@(Var vi1@(Id n1 t)) v2@(Var vi2@(Id n2 _))
---     | isPrimType t = newMergeExpr kv v1 v2
---     | otherwise = do
---         e1 <- arbDCCase1 vi1
---         e2 <- arbDCCase2 vi2
-
---         insertNewExprEnv n1 e1
---         insertExprEnv1 n1 e1
-
---         insertNewExprEnv n2 e2
---         insertExprEnv2 n2 e2
-
---         newMergeExpr kv e1 e2
-newMergeCurrExpr' kv e1 e2 = newMergeExpr kv e1 e2
 
 ------------------------------------------------
 -- Merging expressions
@@ -326,7 +309,7 @@ newMergeEnvObj kv n eObj1 eObj2
     | E.EqFast eObj1 == E.EqFast eObj2 = return eObj1
     | E.ExprObj _ e1 <- eObj1
     , E.ExprObj _ e2 <- eObj2 = do
-        m_e <- newMergeExpr kv e1 e2
+        m_e <- newCaseExpr e1 e2
         return $ E.ExprObj Nothing m_e
     | (E.SymbObj i) <- eObj1
     , (E.ExprObj _ e2) <- eObj2 = do
@@ -336,7 +319,7 @@ newMergeEnvObj kv n eObj1 eObj2
             Just (E.SymbObj _) -> do
                 e_s <- arbDCCase1 i
                 insertExprEnv1 (idName i) e_s
-                e <- newMergeExpr kv e_s e2
+                e <- newCaseExpr e_s e2
                 deleteSymbolic1 i
                 return $ E.ExprObj Nothing e
             Just obj' -> newMergeEnvObj kv n obj' eObj2
@@ -349,7 +332,7 @@ newMergeEnvObj kv n eObj1 eObj2
             Just (E.SymbObj _) -> do
                 e_s <- arbDCCase2 i
                 insertExprEnv2 (idName i) e_s
-                e <- newMergeExpr kv e1 e_s
+                e <- newCaseExpr e1 e_s
                 deleteSymbolic2 i
                 return $ E.ExprObj Nothing e
             Just obj' -> newMergeEnvObj kv n eObj1 obj'
@@ -357,18 +340,18 @@ newMergeEnvObj kv n eObj1 eObj2
     | (E.RedirObj n) <- eObj1
     , (E.ExprObj _ e2) <- eObj2 = do
         i1 <- nameToId1 n
-        e <- newMergeExpr kv (Var i1) e2
+        e <- newCaseExpr (Var i1) e2
         return $ E.ExprObj Nothing e
     | (E.ExprObj _ e1) <- eObj1
     , (E.RedirObj n) <- eObj2 = do
         i2 <- nameToId2 n
-        e <- newMergeExpr kv e1 (Var i2)
+        e <- newCaseExpr e1 (Var i2)
         return $ E.ExprObj Nothing e
     | (E.RedirObj n1) <- eObj1
     , (E.RedirObj n2) <- eObj2 = do
         i1 <- nameToId1 n
         i2 <- nameToId2 n
-        e <- newMergeExpr kv  (Var i1) (Var i2)
+        e <- newCaseExpr (Var i1) (Var i2)
         return $ E.ExprObj Nothing e
     | (E.SymbObj i1) <- eObj1
     , (E.SymbObj i2) <- eObj2
@@ -433,7 +416,6 @@ arbDCCase insertSym i@(Id _ t) = do
                     return e
                 Nothing -> error $ "arbDCCase: type not found"
         | otherwise -> error $ "arbDCCase: type not found"
-arbDCCase _ _ = error $ "arbDCCase: type not found"
 
 nameToId1 :: Name -> MergeM t Id
 nameToId1 n = do
@@ -448,160 +430,6 @@ nameToId2 n = do
     case m_e of
         Just e -> return $ Id n (typeOf e)
         Nothing -> error "nameToId2: name not found"
-
--- Takes two expressions to merge.  In addition, takes a map from pairs of
--- variables names to a single name, which can be substituted in place of
--- the variables.
--- Returns the results of merging the expressions, as well as a HashMap of
--- new name pairs which must be merged.
-newMergeExpr :: KnownValues -> Expr -> Expr -> MergeM t Expr
--- newMergeExpr kv v1@(Var i1@(Id n1 t)) v2@(Var i2@(Id n2 _)) = do
---     m_i <- lookupMergedIds n1 n2
-
---     eenv1 <- exprEnv1
---     eenv2 <- exprEnv2
-
---     if  | n1 == n2 -> return v1
---         | Just i <- m_i -> return (Var i)
---         | E.isSymbolic n1 eenv1
---         , E.isSymbolic n2 eenv2
---         , isPrimType t -> do
---             m_id <- splitId
---             i <- freshIdM t
---             insertNewSymbolic i
---             let pc = [ PC.mkAssumePC m_id 1 $ ExtCond (mkEqPrimExpr t kv (Var i) v1) True
---                      , PC.mkAssumePC m_id 2 $ ExtCond (mkEqPrimExpr t kv (Var i) v2) True ]
---             addPC pc
---             return (Var i)
---         | E.isSymbolic n1 eenv1
---         , E.isSymbolic n2 eenv2
---         , n1 `E.member` eenv1
---         , not (n1 `E.member` eenv2) -> do
---             insertNewExprEnv n2 v1
---             insertExprEnv2 n2 v1
---             deleteSymbolic2 i2
---             return v1
---         | E.isSymbolic n1 eenv1
---         , E.isSymbolic n2 eenv2
---         , n2 `E.member` eenv2
---         , not (n2 `E.member` eenv1) -> do
---             insertNewExprEnv n1 v2
---             insertExprEnv1 n1 v2
---             deleteSymbolic1 i1
---             return v2
---         | Just e1 <- smnfVal eenv1 v1
---         , Just e2 <- smnfVal eenv2 v2 -> do
---             i <- freshIdM t
---             insertNewMergedIds n1 n2 i
---             e <- newMergeExpr kv e1 e2
---             insertNewExprEnv (idName i) e
---             insertExprEnv1 (idName i) e
---             insertExprEnv2 (idName i) e
---             return e
---         | otherwise -> do
---             i <- freshIdM t
---             insertNewMergedIds n1 n2 i
---             e <- newCaseExpr' v1 v2
---             insertNewExprEnv (idName i) e
---             insertExprEnv1 (idName i) e
---             insertExprEnv2 (idName i) e
---             return (Var i)
-
--- newMergeExpr kv v1@(Var (Id _ t)) l@(Lit _) = do
---     m_id <- splitId
---     i <- freshIdM t
---     insertNewSymbolic i
---     let pc = [ PC.mkAssumePC m_id 1 $ ExtCond (mkEqPrimExpr t kv (Var i) v1) True
---              , PC.mkAssumePC m_id 2 $ ExtCond (mkEqPrimExpr t kv (Var i) l) True ]
---     addPC pc
---     return (Var i)
--- newMergeExpr kv l@(Lit _) v2@(Var (Id _ t)) = do
---     m_id <- splitId
---     i <- freshIdM t
---     insertNewSymbolic i
---     let pc = [ PC.mkAssumePC m_id 1 $ ExtCond (mkEqPrimExpr t kv (Var i) l) True
---              , PC.mkAssumePC m_id 2 $ ExtCond (mkEqPrimExpr t kv (Var i) v2) True ]
---     addPC pc
---     return (Var i)
--- newMergeExpr kv l1@(Lit _) l2@(Lit _) = do
---     m_id <- splitId
---     let t = typeOf l1
---     i <- freshIdM t
---     insertNewSymbolic i
---     let pc = [ PC.mkAssumePC m_id 1 $ ExtCond (mkEqPrimExpr t kv (Var i) l1) True
---              , PC.mkAssumePC m_id 2 $ ExtCond (mkEqPrimExpr t kv (Var i) l2) True ]
---     addPC pc
---     return (Var i)
-
--- newMergeExpr kv e1 e2
---     | Prim _ _:_ <- unApp e1 = do
---         let t = typeOf e1
---         b_id <- freshIdM t
---         insertSymbolic1 b_id
---         let pc = ExtCond (mkEqPrimExpr t kv e1 (Var b_id)) True
---         addPC [pc]
---         newMergeExpr kv (Var b_id) e2
--- newMergeExpr kv e1 e2
---     | Prim _ _:_ <- unApp e2 = do
---         let t = typeOf e2
---         b_id <- freshIdM t
---         insertSymbolic2 b_id
---         let pc = ExtCond (mkEqPrimExpr t kv e2 (Var b_id)) True
---         addPC [pc]
---         newMergeExpr kv e1 (Var b_id)
-
--- newMergeExpr kv v@(Var (Id n t)) e2 = do
---     eenv1 <- exprEnv1
---     eenv2 <- exprEnv2
---     if  | Just e1 <- smnfVal eenv1 v
---         , Var vi@(Id n' _) <- e1
---         , isSMNF eenv2 e2 -> do
---             new_e1 <- arbDCCase1 vi
---             insertNewExprEnv n' new_e1
---             insertExprEnv1 n' new_e1
---             newMergeExpr kv new_e1 e2
---         | Just e1 <- smnfVal eenv1 v
---         , isSMNF eenv2 e2 ->
---             newMergeExpr kv e1 e2
---         | otherwise -> newCaseExpr' v e2
--- newMergeExpr kv e1 v@(Var (Id n t)) = do
---     eenv1 <- exprEnv1
---     eenv2 <- exprEnv2
---     if  | Just e2 <- smnfVal eenv2 v
---         , Var vi@(Id n' _) <- e2
---         , isSMNF eenv1 e1 -> do
---             new_e2 <- arbDCCase2 vi
---             insertNewExprEnv n' new_e2
---             insertExprEnv2 n' new_e2
---             newMergeExpr kv e1 new_e2
---         | Just e2 <- smnfVal eenv2 v
---         , isSMNF eenv1 e1 ->
---             newMergeExpr kv e1 e2
---         | otherwise -> newCaseExpr' e1 v
-
--- newMergeExpr kv (Lam lu1 i1 e1) (Lam lu2 (Id n _) e2) = do
---     let e2' = replaceVar n (Var i1) e2
---     e <- if e1 == e2' then return e1 else newCaseExpr' e1 e2'
---     assert (lu1 == lu2) . return $ Lam lu1 i1 e
-
--- newMergeExpr kv e1 e2
---     | d@(Data (DataCon n1 _)):es1 <- unApp e1
---     , Data (DataCon n2 _):es2 <- unApp e2 
---     , n1 == n2 = do
---         es <- mapM (uncurry (newMergeExpr kv)) $ zip es1 es2
---         return $ mkApp (d:es)
-
--- newMergeExpr kv e1 e2@(Case (Var i2) b2 as2)
---     | Data dc1:_ <- unApp e1
---     , isSMNFCase e2 = newMergeDataConCase kv dc1 e1 i2 as2
--- newMergeExpr kv e1@(Case (Var i1) b1 as1) e2
---     | Data dc2:_ <- unApp e2
---     , isSMNFCase e1 = newMergeCaseDataCon kv i1 as1 dc2 e2
--- newMergeExpr kv e1@(Case (Var i1) b1 as1) e2@(Case (Var i2) b2 as2) 
---     | isSMNFCase e1
---     , isSMNFCase e2 = newMergeCaseExprs kv i1 as1 i2 as2
--- newMergeExpr _ ty@(Type t) (Type t') = assert (t == t') return ty
-newMergeExpr _ e1 e2 = newCaseExpr' e1 e2
 
 -- | Digs through lone Vars to look for a symbolic merged normal form expression.
 -- Will not get stuck in an infinite loop, if there is a self recursive Var, or a
@@ -618,12 +446,8 @@ smnfVal eenv = go HS.empty
             | isSMNF eenv e = Just e
             | otherwise = Nothing
 
-newMergeExprs' :: KnownValues -> [Expr] -> MergeM t Expr
-newMergeExprs' kv (e:es) = foldM (newMergeExpr kv) e es
-newMergeExprs' _ _ = error  "newMergeExprs: empty list"
-
-newCaseExpr' :: Expr -> Expr -> MergeM t Expr
-newCaseExpr' e1 e2 = do
+newCaseExpr :: Expr -> Expr -> MergeM t Expr
+newCaseExpr e1 e2 = do
     m_id <- splitId
     let pc = mkBounds (Var m_id) 1 2
     addPC pc
@@ -640,90 +464,6 @@ mkBounds e l u =
     in
     [ ExtCond (App (App (Prim Le t) (Lit (LitInt l))) e) True
     , ExtCond (App (App (Prim Le t) e) (Lit (LitInt u))) True]
-
-newMergeDataConCase :: KnownValues
-                    -> DataCon
-                    -> Expr
-                    -> Id -- ^ Case bindee variable id
-                    -> [Alt] -- ^ Case Alts (with constructors in SMNF)
-                    -> MergeM t Expr
-newMergeDataConCase kv dc e bind2 as2 = do
-    m_id <- splitId
-    let as_info1 = (dc, m_id, 1, e)
-        as_info2 = map (smnfAltInfo bind2) as2
-    newMergeIntoCase' kv $ as_info1:as_info2
-
-newMergeCaseDataCon :: KnownValues
-                    -> Id -- ^ Case bindee variable id
-                    -> [Alt] -- ^ Case Alts (with constructors in SMNF)
-                    -> DataCon
-                    -> Expr
-                    -> MergeM t Expr
-newMergeCaseDataCon kv bind1 as1 dc e = do
-    m_id <- splitId
-    let as_info1 = map (smnfAltInfo bind1) as1
-        as_info2 = (dc, m_id, 2, e)
-    newMergeIntoCase' kv $ as_info1 ++ [as_info2]
-
-
-newMergeCaseExprs :: KnownValues
-                  -> Id -- ^ Case 1 bindee variable id
-                  -> [Alt] -- ^ Case 1 Alts (with constructors in SMNF)
-                  -> Id -- ^ Case 2 bindee variable id
-                  -> [Alt] -- ^ Case 2 Alts (with constructors in SMNF)
-                  -> MergeM t Expr
-newMergeCaseExprs kv bind1 as1 bind2 as2 =
-    let
-        as_info1 = map (smnfAltInfo bind1) as1
-        as_info2 = map (smnfAltInfo bind2) as2
-    in
-    newMergeIntoCase' kv $ as_info1 ++ as_info2
-
-newMergeIntoCase' :: KnownValues
-                  -> [(DataCon, Id, Integer, Expr)]
-                  -> MergeM t Expr
-newMergeIntoCase' kv b_as = do
-    let -- Group up by common data constructors
-        as_hm = foldr (\(DataCon n _, i, l, e) -> HM.insertWith (++) n [(i, l, e)]) HM.empty $ b_as
-        as_grouped = HM.toList as_hm :: [(Name, [(Id, Integer, Expr)])]
-
-        -- Form the new case
-        num_grouped = zip [1..] (map snd as_grouped) :: [(Integer, [(Id, Integer, Expr)])]
-
-    new_alts <- mapM (\(i, es) -> do
-                            me <- newMergeExprs' kv (map thd3 es)
-                            return (Alt (LitAlt $ LitInt i) me)) num_grouped
-
-    new_bndee <- freshIdM TyLitInt
-    new_bnd <- freshIdM TyLitInt
-
-    insertNewSymbolic new_bndee
-
-    let bndee_pc = mkBounds (Var new_bndee) 1 (toInteger $ length num_grouped)
-        link_pc = map (\(i, j_es) -> mkMergeCasePC kv new_bndee i
-                                   $ map (\(bnd_, lit_, _) -> (bnd_, lit_)) j_es)
-                      num_grouped
-
-    addPC (bndee_pc ++ link_pc)
-
-    return $ Case (Var new_bndee) new_bnd new_alts
-    where
-      fst3 (x, _, _) = x
-      snd3 (_, x, _) = x
-      thd3 (_, _, x) = x
-
-smnfAltInfo :: Id -> Alt -> (DataCon, Id, Integer, Expr)
-smnfAltInfo i (Alt (LitAlt (LitInt l)) e)
-    | Data dc:_ <- unApp e = (dc, i, l, e)
-smnfAltInfo _ a = error $ "smnfAltInfo: invalid Alt structure" ++ show a
-
-mkMergeCasePC :: KnownValues -> Id -> Integer -> [(Id, Integer)] -> PathCond
-mkMergeCasePC kv bind1 l bls =
-      flip ExtCond True
-    . mkEqExpr kv (mkEqExpr kv (Var bind1) (Lit $ LitInt l)) 
-    $ foldr (mkOrExpr kv)
-            (mkFalse kv)
-            (map (\(b2, jl) -> mkEqExpr kv (Var b2) (Lit (LitInt jl))) bls)
 
 ------------------------------------------------
 
