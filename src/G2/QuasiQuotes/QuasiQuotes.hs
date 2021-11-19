2b023c969b2fadcc4e5d1772958a9df99213733b
Bill Hallahan <billthallahan@gmail.com>
2021-11-18 23:50:23 -0500

    Eliminate spans before quasiquoter encoding

--- src/G2/QuasiQuotes/QuasiQuotes.hs
+++ src/G2/QuasiQuotes/QuasiQuotes.hs
@@ -29,6 +29,7 @@ import G2.QuasiQuotes.Parser
 import qualified Control.Concurrent.Lock as Lock
 
 import Data.Data
+import qualified Data.HashMap.Lazy as HM
 import qualified Data.HashSet as HS
 import Data.List
 import qualified Data.Map as M
@@ -152,10 +153,15 @@ parseHaskellQ str = do
 liftDataT :: Data a => a -> Q Exp
 liftDataT = dataToExpQ (\a -> case liftText <$> cast a of
                                     je@(Just _) -> je
-                                    Nothing -> liftLoc <$> cast a)
+                                    Nothing -> case liftLoc <$> cast a of
+                                                    jl@(Just _) -> jl
+                                                    Nothing -> liftMaybeSpan <$> cast a)
     where
         liftText txt = appE (varE 'T.pack) (stringE (T.unpack txt))
         liftLoc (G2.Loc l c f) = conE 'G2.Loc `appE` intE l `appE` intE c `appE` stringE f
+        
+        liftMaybeSpan :: Maybe Span -> Q Exp
+        liftMaybeSpan _ = conE 'Nothing
         intE i = [| i |]
 
 parseHaskellQ' :: QuotedExtract-> Q ExtractedG2
@@ -326,7 +332,6 @@ moveOutStatePieces tenv_name s = do
              , num_steps = $(num_steps_exp)
              , tags = $(tags_exp) 
              , track = $(track_exp) } |]
-
 
 -- Returns an Q Exp represeting a [(Name, Expr)] list
 regVarBindings :: [TH.Name] -> TypeEnvName -> CleanedNamesName -> InputIds -> Bindings -> Q Exp
