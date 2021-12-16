8819d16ebce59e912e543c19c33737c654b63431
Bill Hallahan <billthallahan@gmail.com>
2021-12-16 15:14:26 -0500

    Inline less

--- src/G2/Equiv/Summary.hs
+++ src/G2/Equiv/Summary.hs
@@ -55,7 +55,7 @@ trackName s =
 printPG :: PrettyGuide -> [Name] -> [Id] -> StateET -> String
 printPG pg ns sym_ids s =
   let h = expr_env s
-      e = inlineVars h $ exprExtract s
+      e = inlineVars ns h $ exprExtract s
       e_str = printHaskellPG pg s e
       sym_vars = varsFullList h ns sym_ids
       sym_str = printVars pg ns s sym_vars
@@ -69,14 +69,15 @@ printPG pg ns sym_ids s =
         _ -> "\nOther Variables:\n" ++ var_str
   in e_str ++ sym_print ++ var_print ++ "\n---"
 
-inlineVars :: ExprEnv -> Expr -> Expr
-inlineVars eenv = inlineVars' HS.empty eenv
+inlineVars :: [Name] -> ExprEnv -> Expr -> Expr
+inlineVars ns eenv = inlineVars' HS.empty ns eenv
 
-inlineVars' :: HS.HashSet Name -> ExprEnv -> Expr -> Expr
-inlineVars' seen eenv v@(Var (Id n _))
+inlineVars' :: HS.HashSet Name -> [Name] -> ExprEnv -> Expr -> Expr
+inlineVars' seen ns eenv v@(Var (Id n _))
+    | n `elem` ns = v
     | n `HS.member` seen = v
-    | Just e <- E.lookup n eenv = inlineVars' (HS.insert n seen) eenv e
-inlineVars' seen eenv e = modifyChildren (inlineVars' seen eenv) e
+    | Just e <- E.lookup n eenv = inlineVars' (HS.insert n seen) ns eenv e
+inlineVars' seen ns eenv e = modifyChildren (inlineVars' seen ns eenv) e
 
 data ChainEnd = Symbolic Id
               | Cycle Id
