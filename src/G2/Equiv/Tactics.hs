ee52ca935da78d73ee64696fd2c0410f059bb49d
Bill Hallahan <billthallahan@gmail.com>
2022-01-05 01:31:19 -0500

    Moved most type declarations out of G2.Equiv.Tactics

--- src/G2/Equiv/Tactics.hs
+++ src/G2/Equiv/Tactics.hs
@@ -1,26 +1,16 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module G2.Equiv.Tactics
-    ( StateH (..)
-    , PrevMatch (..)
-    , Marker (..)
-    , ActMarker (..)
-    , CoMarker (..)
-    , IndMarker (..)
-    , EqualMarker (..)
-    , Side (..)
+    ( module G2.Equiv.Types
     , TacticResult (..)
-    , Tactic (..)
+    , Tactic
 
     , Lemmas
     , Lemma (..)
     , ProposedLemma
     , ProvenLemma
     , DisprovenLemma
-
-    , newStateH
 
     , isSWHNF
     , tryEquality
@@ -52,10 +42,6 @@ module G2.Equiv.Tactics
 
 import G2.Language
 
-import G2.Config
-
-import G2.Interface
-
 import qualified Control.Monad.State.Lazy as CM
 
 import qualified G2.Language.ExprEnv as E
@@ -63,188 +49,31 @@ import G2.Language.Monad.AST
 import qualified G2.Language.Typing as T
 
 import GHC.Generics (Generic)
-import Data.Data (Typeable)
-import Data.List
 import Data.Maybe
 import Data.Tuple
-import qualified Data.Sequence as DS
-
+import Data.Hashable
 import qualified Data.HashSet as HS
 import qualified G2.Solver as S
 
 import qualified G2.Language.PathConds as P
 
-import G2.Equiv.InitRewrite
-import G2.Equiv.EquivADT
 import G2.Equiv.G2Calls
+import G2.Equiv.Types
 
 import Data.Either
 import Data.Either.Extra
-import Data.Hashable
 import qualified Data.HashMap.Lazy as HM
-import qualified Data.Map as M
-import G2.Execution.Memory
-import Data.Monoid (Any (..), (<>))
-
-import Debug.Trace
+import Data.Monoid ((<>))
 
 import G2.Execution.NormalForms
 import Control.Monad
 import Control.Monad.Extra
 
-import Data.Time
-
 import G2.Execution.Reducer
-import G2.Lib.Printers
 
 import qualified Control.Monad.Writer.Lazy as W
 
 import Control.Exception
-
-data StateH = StateH {
-      latest :: StateET
-    , history :: [StateET]
-    , inductions :: [IndMarker]
-    , discharge :: Maybe StateET
-  }
-  deriving (Eq, Generic)
-
-newStateH :: StateET -> StateH
-newStateH s = StateH {
-    latest = s
-  , history = []
-  , inductions = []
-  , discharge = Nothing
-  }
-
-
-instance Hashable StateH
-
-instance Named StateH where
-  names (StateH s h ims d) =
-    names s DS.>< names h DS.>< names ims DS.>< names d
-  rename old new (StateH s h ims d) =
-    StateH (rename old new s) (rename old new h) (rename old new ims) (rename old new d)
-
--- The container field is only relevant for induction.  When the expression for
--- one past state is actually an inner scrutinee of an expression that really
--- was encountered in the past, the container holds the full expression.
-data PrevMatch t = PrevMatch {
-    present :: (State t, State t)
-  , past :: (State t, State t)
-  , conditions :: (HM.HashMap Id Expr, HS.HashSet (Expr, Expr))
-  , container :: State t
-}
-
--- TODO new constructor for lemma proving
--- also have one for lemma application
-data ActMarker = Induction IndMarker
-               | Coinduction CoMarker
-               | Equality EqualMarker
-               | NoObligations (StateET, StateET)
-               | NotEquivalent (StateET, StateET)
-               | SolverFail (StateET, StateET)
-               | Unresolved (StateET, StateET)
-
-instance Named ActMarker where
-  names (Induction im) = names im
-  names (Coinduction cm) = names cm
-  names (Equality em) = names em
-  names (NoObligations s_pair) = names s_pair
-  names (NotEquivalent s_pair) = names s_pair
-  names (SolverFail s_pair) = names s_pair
-  names (Unresolved s_pair) = names s_pair
-  rename old new m = case m of
-    Induction im -> Induction $ rename old new im
-    Coinduction cm -> Coinduction $ rename old new cm
-    Equality em -> Equality $ rename old new em
-    NoObligations s_pair -> NoObligations $ rename old new s_pair
-    NotEquivalent s_pair -> NotEquivalent $ rename old new s_pair
-    SolverFail s_pair -> SolverFail $ rename old new s_pair
-    Unresolved s_pair -> Unresolved $ rename old new s_pair
-
-data Marker = Marker (StateH, StateH) ActMarker
-
-instance Named Marker where
-  names (Marker (sh1, sh2) m) =
-    names sh1 DS.>< names sh2 DS.>< names m
-  rename old new (Marker (sh1, sh2) m) =
-    Marker (rename old new sh1, rename old new sh2) $ rename old new m
-
-data Side = ILeft | IRight deriving (Eq, Show, Typeable, Generic)
-
-instance Hashable Side
-
-data IndMarker = IndMarker {
-      ind_real_present :: (StateET, StateET)
-    , ind_used_present :: (StateET, StateET)
-    , ind_past :: (StateET, StateET)
-    , ind_result :: (StateET, StateET)
-    , ind_present_scrutinees :: (Expr, Expr)
-    , ind_past_scrutinees :: (StateET, StateET)
-    , ind_side :: Side
-    , ind_fresh_name :: Name
-  }
-  deriving (Eq, Generic)
-
-instance Hashable IndMarker
-
--- TODO shouldn't need present scrutinees
-instance Named IndMarker where
-  names im =
-    let (s1, s2) = ind_real_present im
-        (q1, q2) = ind_used_present im
-        (p1, p2) = ind_past im
-        (s1', s2') = ind_result im
-        (r1, r2) = ind_past_scrutinees im
-        states = [s1, s2, q1, q2, p1, p2, s1', s2', r1, r2]
-    in foldr (DS.><) DS.empty $ map names states
-  rename old new im =
-    let r = rename old new
-    in im {
-      ind_real_present = r $ ind_real_present im
-    , ind_used_present = r $ ind_used_present im
-    , ind_past = r $ ind_past im
-    , ind_result = r $ ind_result im
-    , ind_present_scrutinees = rename old new $ ind_present_scrutinees im
-    , ind_past_scrutinees = r $ ind_past_scrutinees im
-    , ind_fresh_name = rename old new $ ind_fresh_name im
-    }
-
-data CoMarker = CoMarker {
-    co_real_present :: (StateET, StateET)
-  , co_used_present :: (StateET, StateET)
-  , co_past :: (StateET, StateET)
-}
-
-instance Named CoMarker where
-  names (CoMarker (s1, s2) (q1, q2) (p1, p2)) =
-    foldr (DS.><) DS.empty $ map names [s1, s2, q1, q2, p1, p2]
-  rename old new (CoMarker (s1, s2) (q1, q2) (p1, p2)) =
-    let r = rename old new
-        s1' = r s1
-        s2' = r s2
-        q1' = r q1
-        q2' = r q2
-        p1' = r p1
-        p2' = r p2
-    in CoMarker (s1', s2') (q1', q2') (p1', p2')
-
-data EqualMarker = EqualMarker {
-    eq_real_present :: (StateET, StateET)
-  , eq_used_present :: (StateET, StateET)
-}
-
-instance Named EqualMarker where
-  names (EqualMarker (s1, s2) (q1, q2)) =
-    foldr (DS.><) DS.empty $ map names [s1, s2, q1, q2]
-  rename old new (EqualMarker (s1, s2) (q1, q2)) =
-    let r = rename old new
-        s1' = r s1
-        s2' = r s2
-        q1' = r q1
-        q2' = r q2
-    in EqualMarker (s1', s2') (q1', q2')
 
 data TacticResult = Success (Maybe (Int, Int, StateET, StateET))
                   | NoProof (HS.HashSet Lemma)
@@ -260,10 +89,6 @@ type Tactic s = s ->
                 (StateH, StateH) ->
                 (StateET, StateET) ->
                 W.WriterT [Marker] IO TacticResult
-
-reverseCoMarker :: CoMarker -> CoMarker
-reverseCoMarker (CoMarker (s1, s2) (q1, q2) (p1, p2)) =
-  CoMarker (s2, s1) (q2, q1) (p2, p1)
 
 stripTicks :: Expr -> Expr
 stripTicks (Tick _ e) = e
