98dfcd9c3811c3c02de51faa8dba908cdda59788
Bill Hallahan <billthallahan@gmail.com>
2022-04-21 18:19:10 -0400

    Fixed MeasErrs benchmark for 8.6.5

--- tests/Liquid/MeasErr.hs
+++ tests/Liquid/MeasErr.hs
@@ -1,8 +1,8 @@
 module MeasErr where
 
-{-@ measure m  :: (Int, Int) -> Int
-    m (a,b)  = b
-  @-}
+{-@ measure m  :: (Int, Int) -> Int @-}
+m :: (Int, Int) -> Int
+m (_, b)  = b
 
 {-@ f :: {y:(Int, Int) | 0 = m y} -> Int @-}
 f :: (Int, Int) -> Int
