eb1fc82c4c460af7e435bf381e2d426631099e87
Bill Hallahan <billthallahan@gmail.com>
2022-04-22 00:08:29 -0400

    Updating CI

--- tests/Test.hs
+++ tests/Test.hs
@@ -267,9 +267,7 @@ liquidTests = testGroup "Liquid"
     , checkLiquid "tests/Liquid/Distance.lhs" "distance" 1000 [AtLeast 1]
 
     -- The below test generates a LiquidHaskell error with newer LiquidHaskell versions
-    #if !MIN_VERSION_liquidhaskell(0,8,6)
-    , checkLiquid "tests/Liquid/MultModules/CallZ.lhs" "callZ" 1000 [AtLeast 1]
-    #endif
+    -- , checkLiquid "tests/Liquid/MultModules/CallZ.lhs" "callZ" 1000 [AtLeast 1]
 
     , checkAbsLiquid "tests/Liquid/AddToEven.hs" "f" 2500
         [ AtLeast 1
