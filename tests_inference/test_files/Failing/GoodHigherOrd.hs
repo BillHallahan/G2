916889938aaa0cad80735f1f872b1a31cea30f60
Bill Hallahan <billthallahan@gmail.com>
2022-05-30 20:47:17 -0400

    inference tests

---
+++ tests_inference/test_files/Failing/GoodHigherOrd.hs
@@ -0,0 +1,16 @@
+{-@ LIQUID "--short-names" @-}
+{-@ LIQUID "--no-termination" @-}
+{-@ LIQUID "--prune-unsorted" @-}
+
+module Test where
+
+{-@ f :: n:Nat -> (Int, {x:Int | x == n}) -> (Int, {x:Int | x == n}) @-}
+f :: Int -> (Int, Int) -> (Int, Int)
+f n ps = call (g n) ps
+
+call :: (a -> a -> a) -> a -> a
+call op x = op x x
+
+{-@ g :: n:Int -> t1:(Int, { x:Int | x == n }) -> (Int, { y:Int | y == n }) -> (Int, { z:Int | z == n }) @-}
+g :: Int -> (Int, Int) -> (Int, Int) -> (Int, Int)
+g n (n1, p1) (n2, p2) = (n1, p1)
