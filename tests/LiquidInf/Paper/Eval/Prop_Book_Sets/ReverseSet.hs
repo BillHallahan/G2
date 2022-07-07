1e0a0d706ba665c5f72b1ddf6e18e77cab7f3a6f
Bill Hallahan <billthallahan@gmail.com>
2022-07-07 16:30:47 -0400

    rename

---
+++ tests/LiquidInf/Paper/Eval/Prop_Book_Sets/ReverseSet.hs
@@ -0,0 +1,18 @@
+
+module Reverse (reverse) where
+
+import Prelude hiding (reverse)
+import Data.Set
+
+{-@ type ListS a S = {v:[a] | listElts v = S} @-}
+{-@ type ListEq a X = ListS a {listElts X}    @-}
+
+{-@ reverse :: xs:[Int] -> ListEq Int xs @-}
+reverse :: [Int] -> [Int]
+reverse xs = revHelper [] xs
+
+revHelper :: [Int] -> [Int] -> [Int]
+revHelper acc []     = acc
+revHelper acc (x:xs) = revHelper (x:acc) xs
+
+{-@ LIQUID "--no-termination" @-}
