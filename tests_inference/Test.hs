52a8c2872b2f8a9e98150310a8185709180c61db
Bill Hallahan <billthallahan@gmail.com>
2022-07-16 12:35:43 -0400

    New test

--- tests_inference/Test.hs
+++ tests_inference/Test.hs
@@ -85,6 +85,7 @@ posTests = testGroup "Tests"
             , posTestInference "tests_inference/test_files/Pos/Test43.hs"
             , posTestInference "tests_inference/test_files/Pos/Test44.hs"
             , posTestInference "tests_inference/test_files/Pos/Test45.hs"
+            , posTestInference "tests_inference/test_files/Pos/Test46.hs"
 
             , posTestInference "tests_inference/test_files/Pos/Sets1.hs"
             , posTestInference "tests_inference/test_files/Pos/Sets2.hs"
