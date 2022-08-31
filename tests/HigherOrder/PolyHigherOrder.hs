545f4146f413a06f7d82e1fdc3adf5d6d0064a74
Bill Hallahan <billthallahan@gmail.com>
2022-08-31 16:30:24 -0400

    Making test more polymorphic

--- tests/HigherOrder/PolyHigherOrder.hs
+++ tests/HigherOrder/PolyHigherOrder.hs
@@ -23,5 +23,5 @@ streamTail (Stream _ s) = s
 sf :: (Stream a -> Int) -> Stream a -> Bool
 sf f s = f s == f (streamTail s)
 
-tupleTest :: ((Int, Int) -> (Int, Int)) -> Bool
+tupleTest :: (Num a, Ord a) => ((a, a) -> (a, a)) -> Bool
 tupleTest f = let (a,b) = f (3,6) in a <= b
