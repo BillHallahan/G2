8ce448679bc980f3ce8b3f56b2cd07d7e2922f4e
John Charles Kolesar <john.kolesar@yale.edu>
2022-11-15 16:05:53 -0500

    More debugging, idenfitied variable error source

--- src/G2/Equiv/Tactics.hs
+++ src/G2/Equiv/Tactics.hs
@@ -72,6 +72,8 @@ import qualified Control.Monad.Writer.Lazy as W
 
 import Control.Exception
 
+import Debug.Trace
+
 -- the Bool value for Failure is True if a cycle has been found
 data TacticResult = Success (Maybe (Int, Int, StateET, StateET))
                   | NoProof [Lemma]
@@ -171,6 +173,16 @@ moreRestrictivePC solver s1 s2 hm = do
     S.UNSAT () -> return True
     _ -> return False
 
+-- same number, but rest of name not constant
+problemId80 :: Id
+problemId80 = Id (Name "xs" Nothing 6989586621679024315 Nothing) (TyApp (TyCon (Name "[]" (Just "GHC.Types") 0 Nothing) (TyFun TYPE TYPE)) (TyVar (Id (Name "a" Nothing 6989586621679029046 (Just (Span {start = Loc {line = 628, col = 1, file = "tests/RewriteVerify/Correct/TestZeno.hs"}, end = Loc {line = 628, col = 77, file = "tests/RewriteVerify/Correct/TestZeno.hs"}}))) TYPE)))
+
+problemId55 :: Id
+problemId55 = Id (Name "xs" Nothing 6989586621679024315 Nothing) (TyApp (TyCon (Name "[]" (Just "GHC.Types") 0 Nothing) (TyFun TYPE TYPE)) (TyVar (Id (Name "a" Nothing 6989586621679028912 (Just (Span {start = Loc {line = 616, col = 1, file = "tests/RewriteVerify/Correct/TestZeno.hs"}, end = Loc {line = 616, col = 77, file = "tests/RewriteVerify/Correct/TestZeno.hs"}}))) TYPE)))
+
+problemName :: Name
+problemName = Name "xs" Nothing 6989586621679024315 Nothing
+
 -- s1 is the old state, s2 is the new state
 -- If any recursively-defined functions or other expressions manage to slip
 -- through the cracks with the other mechanisms in place for avoiding infinite
@@ -241,7 +253,13 @@ moreRestrictive s1@(State {expr_env = h1}) s2@(State {expr_env = h2}) ns hm acti
                                                           "\n" ++ (show $ lookupBoth (idName i) h1 h1') ++
                                                           "\n" ++ (show $ lookupBoth (idName i) h2 h2') ++
                                                           "\n" ++ (folder_name $ track s1) ++
-                                                          "\n" ++ (folder_name $ track s2)
+                                                          "\n" ++ (folder_name $ track s2) ++
+                                                          "\n" ++ (show $ curr_expr s1) ++
+                                                          "\n" ++ (show $ curr_expr s2) ++
+                                                          "\nSIZES:\n" ++ (show $ E.size h1) ++
+                                                          "\n" ++ (show $ E.size h1') ++
+                                                          "\n" ++ (show $ E.size h2) ++
+                                                          "\n" ++ (show $ E.size h2')
     (_, Var i) | isSymbolicBoth (idName i) h2 h2' -> Left [] -- sym replaces non-sym
                | not $ (idName i, e1) `elem` n2
                , not $ HS.member (idName i) ns -> error $ "unmapped variable " ++ (show i)
@@ -273,7 +291,9 @@ moreRestrictive s1@(State {expr_env = h1}) s2@(State {expr_env = h2}) ns hm acti
                                     -- with the order swapped, p55 gives an error
                                     -- something is in h2_' that isn't in h2_alt
                                     h2_alt = envMerge h2_' h2_
-                                    et' = (track s2) { opp_env = E.empty }
+                                    et' = trace ("DIFF " ++ (show $ E.size $ E.difference h2_alt h2_')) $ (track s2) { opp_env = E.empty }
+                                    -- making the expr env here h2_alt is wrong
+                                    -- removes p80 error but throws off lemmas
                                     ls1 = s2 { expr_env = h2_', curr_expr = CurrExpr Evaluate e1', track = et' }
                                     -- changing this from h2_ to h2_' removes error but slows things down?
                                     -- changing it back to h2_ removes the error for p55 and reintroduces for p80
@@ -404,6 +424,8 @@ inlineEquiv acc h h' ns v@(Var (Id n _))
 inlineEquiv acc h h' ns e = modifyChildren (inlineEquiv acc h h' ns) e
 
 -- ids are the same between both sides; no need to insert twice
+-- TODO this check becomes true at times in final iteration before error on p80
+-- likewise for p55 in the error scenario
 moreRestrictiveAlt :: StateET
                    -> StateET
                    -> HS.HashSet Name
@@ -417,7 +439,7 @@ moreRestrictiveAlt :: StateET
 moreRestrictiveAlt s1 s2 ns hm active n1 n2 (Alt am1 e1) (Alt am2 e2) =
   if altEquiv am1 am2 then
   case am1 of
-    DataAlt _ t1 -> let ns' = foldr HS.insert ns $ map idName t1
+    DataAlt _ t1 -> let ns' = trace ("PROBLEM? " ++ (show $ problemName `elem` (map idName t1))) $ foldr HS.insert ns $ map idName t1
                     in moreRestrictive s1 s2 ns' hm active n1 n2 e1 e2
     _ -> moreRestrictive s1 s2 ns hm active n1 n2 e1 e2
   else Left []
