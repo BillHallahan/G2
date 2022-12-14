1d250bd7a53877715daf395bb3a0860ea3a23865
Bill Hallahan <whallahan@binhamton.edu>
2022-12-14 16:42:04 -0500

    New test

---
+++ tests_lh/test_files/Pos/Test50.hs
@@ -0,0 +1,16 @@
+module Test50 (prop) where
+
+import Prelude hiding (length, zip)
+
+{-@ prop :: [a] -> { v:Bool | v } @-}
+prop xs =
+    let rs = walk xs xs in
+    length rs == length xs
+
+length :: [a] -> Int
+length [] = 0
+length (x:xs) = 1
+
+walk :: [a] -> [b] -> [a]
+walk (a:as) (b:bs) = a : walk as bs
+walk _ _          = []
