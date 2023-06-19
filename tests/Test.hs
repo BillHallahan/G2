f12b36c200414a5f6d60b490241a39bac8f5b814
Bill Hallahan <whallahan@binhamton.edu>
2023-06-19 15:20:34 -0400

    New failing test case

--- tests/Test.hs
+++ tests/Test.hs
@@ -363,7 +363,9 @@ extensionTests = testGroup "Extensions"
     , checkInputOutputs "tests/TestFiles/Extensions/ViewPatterns1.hs" [ ("shapeToNumSides", 4000, [Exactly 4]) ]
     , checkInputOutputs "tests/TestFiles/Extensions/FlexibleContexts1.hs" [ ("callF", 400, [AtLeast 2])
                                                                           , ("callF2", 400, [AtLeast 2])
-                                                                          , ("callF3", 400, [AtLeast 2]) ]
+                                                                          , ("callF3", 400, [AtLeast 2])
+                                                                          , ("callG", 400, [AtLeast 1])
+                                                                          , ("callG2", 400, [AtLeast 1]) ]
     ]
 
 baseTests ::  TestTree
--- tests/TestFiles/Extensions/FlexibleContexts1.hs
+++ tests/TestFiles/Extensions/FlexibleContexts1.hs
@@ -21,3 +21,18 @@ callF2 xs = f xs
 
 callF3 :: F [a] => [a] -> [a]
 callF3 = f
+
+class G a where
+    g :: a -> a
+
+instance G (Int, Float) where
+    g (x, y) = (x + 1, y)
+
+instance G (Char, Int) where
+    g (x, y) = (x, y + 1)
+
+callG :: G a => a -> a
+callG = g
+
+callG2 :: G (a, b) => (a, b) -> (a, b)
+callG2 = g
