1d8d71748af9626929cc5403e29607b9250d5a9e
QHWU1228 <qwu22@binghamton.edu>
2024-07-22 11:22:16 -0400

    creating new test case for instTypes

--- tests/TestFiles/InstTypes1.hs
+++ tests/TestFiles/InstTypes1.hs
@@ -43,3 +43,63 @@ data MyEither l r = MyLeft l | MyRight r
 
 mkLeft :: l -> MyEither l r
 mkLeft l = MyLeft l
+
+id2 :: a -> a 
+id2 x = x
+
+
+-- type that have one type varaible
+data MyList a where
+  Ni :: MyList a
+  Cons :: a -> MyList a -> MyList a
+
+-- recursion on recursive GADT 
+lengthList :: MyList a -> Int
+lengthList Ni        = 0
+lengthList (Cons _ xs) = 1 + lengthList xs
+
+
+-- type that have no type variable 
+data Color = Red | Green | Blue | Yellow 
+
+instance Eq Color where
+    Red == Red = True
+    Green == Green = True
+    Blue == Blue = True
+    Yellow == Yellow = True
+    _ == _ = False
+
+complement :: Color -> Color
+complement Red = Green
+complement Green = Red
+complement Blue = Yellow 
+complement Yellow = Red
+
+data MyBool = MyTrue | MyFalse 
+
+
+checkeq :: Eq a => a -> a -> Bool
+checkeq a a1 = a == a1
+
+idlr :: Either l r -> Either l r 
+idlr x = x
+
+extractLeft :: Either a b -> Maybe a
+extractLeft (Left x)  = Just x
+extractLeft (Right _) = Nothing
+
+extractRight :: Either a b -> Maybe b
+extractRight (Right x)  = Just x
+extractRight (Left _) = Nothing
+
+
+-- Suggestion from professor William
+myTuple :: (MyList a, MyList b) => a -> b -> (a, b)
+myTuple x y = (x, y)
+
+myMap :: (MyList a, MyList b) => [a] -> (a -> b) -> [b]
+myMap a f = map f a 
+
+myid :: MyList a => a -> a
+myid x = x
+
