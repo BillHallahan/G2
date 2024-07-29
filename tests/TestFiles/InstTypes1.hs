eef91e4faae317da8d23eba1c780ca288ea3d767
QHWU1228 <qwu22@binghamton.edu>
2024-07-29 12:59:12 -0400

    Formatting test cases in InstTypes1.hs for clarity

--- tests/TestFiles/InstTypes1.hs
+++ tests/TestFiles/InstTypes1.hs
@@ -48,18 +48,17 @@ module InstTypes1 where
 -- id2 x = x
 
 
---Actual test case that's useful
+-- Actual test case that's useful
 
--- type that have one type varaible
-data MyList a where
-  Ni :: MyList a
-  Cons :: a -> MyList a -> MyList a
+-- template test cases in the beginning
+myTuple ::  a -> b -> (a,b)
+myTuple x y = (x, y)
 
--- recursion on recursive GADT 
-lengthList :: MyList a -> Int
-lengthList Ni        = 0
-lengthList (Cons _ xs) = 1 + lengthList xs
+myMap :: [a] -> (a -> b) -> [b]
+myMap a f = map f a 
 
+myId :: a -> a
+myId x = x
 
 -- type that have no type variable 
 data Color = Red | Green | Blue | Yellow 
@@ -71,12 +70,6 @@ instance Eq Color where
     Yellow == Yellow = True
     _ == _ = False
 
-instance Eq a => Eq (MyList a) where
-  Ni == Ni = True
-  Cons x xs == Cons y ys = (x == y) && (xs == ys)
-  _ == _ = False
-
-
 complement :: Color -> Color
 complement Red = Green
 complement Green = Red
@@ -84,7 +77,6 @@ complement Blue = Yellow
 complement Yellow = Red
 
 data MyBool = MyTrue | MyFalse 
-
 
 checkeq :: Eq a => a -> a -> Bool
 checkeq a a1 = a == a1
@@ -100,8 +92,43 @@ extractRight :: Either a b -> Maybe b
 extractRight (Right x)  = Just x
 extractRight (Left _) = Nothing
 
+take2 :: Either l r -> Either l r -> Either l r -> Either l r -> Either l r 
+take2 _ _ _ x = x
 
--- Suggestion from professor William
+-- type that have one type varaible
+data MyList a where
+  Ni :: MyList a
+  Cons :: a -> MyList a -> MyList a
+
+-- recursion on recursive GADT 
+lengthList :: MyList a -> Int
+lengthList Ni        = 0
+lengthList (Cons _ xs) = 1 + lengthList xs
+
+instance Eq a => Eq (MyList a) where
+  Ni == Ni = True
+  Cons x xs == Cons y ys = (x == y) && (xs == ys)
+  _ == _ = False
+
+-- test cases with type that have one type varaible
+myListId :: MyList a -> MyList a 
+myListId x = x
+
+takeMyList :: MyList a -> MyList a -> MyList a -> MyList b -> MyList a
+takeMyList x _ _ _ = x 
+
+myListInt :: MyList Int -> MyList Int 
+myListInt x = x
+
+myListTuple ::  MyList a -> MyList a -> (MyList a, MyList a)
+myListTuple x y = (x, y)
+
+myListTuple2 ::  MyList a -> MyList b -> (MyList b, MyList a)
+myListTuple2 x y = (y, x)
+
+myListMap :: [MyList a] -> (MyList a -> MyList b) -> [MyList b]
+myListMap a f = map f a 
+
 contains :: Eq a => a -> MyList a -> Bool
 contains _ Ni = False
 contains x (Cons y ys) = (x == y) || contains x ys
@@ -109,30 +136,6 @@ contains x (Cons y ys) = (x == y) || contains x ys
 headMyList :: MyList a -> Maybe a
 headMyList Ni = Nothing
 headMyList (Cons x _) = Just x
-
-myTuple ::  a -> b -> (a,b)
-myTuple x y = (x, y)
-
-myListTuple ::  MyList a -> MyList a -> (MyList a, MyList a)
-myListTuple x y = (x, y)
-
-myListTuple2 ::  MyList a -> MyList b -> (MyList a, MyList b)
-myListTuple2 x y = (x, y)
-
-myMap :: [a] -> (a -> b) -> [b]
-myMap a f = map f a 
-
-myListMap :: [MyList a] -> (MyList a -> MyList b) -> [MyList b]
-myListMap a f = map f a 
-
-myId :: a -> a
-myId x = x
-
-myListId :: MyList a -> MyList a 
-myListId x = x
-
-myListInt :: MyList Int -> MyList Int 
-myListInt x = x
 
 -- test cases for three arguments
 data Tri a b c where
@@ -147,10 +150,18 @@ triInt (Tri x y z) = x
 triFuna :: Tri a b c -> a 
 triFuna (Tri x y z) = x
 
+triFunb :: Tri a b c -> b 
+triFunb (Tri x y z) = y
 
-takeMyList :: MyList a -> MyList a -> MyList a -> MyList b -> MyList a
-takeMyList x _ _ _ = x 
+triFunc :: Tri a b c -> Maybe c
+triFunc (Tri x y z) = z
 
+-- take3, takeMyList, myListTuple, myListMap has a bad argument passed error 
+-- however take2 with Either l r successfully work?
+take3 :: Tri a b c -> Tri a b c -> Tri a b c -> Tri a b c -> Tri a b c
+take3 _ _ _ x = x
+
+-- template test cases used for benchmark test cases 
 takeOne :: a -> a -> a -> b -> a 
 takeOne x _ _ _ = x
 
@@ -162,7 +173,6 @@ takeTwo _ x _ _ = x
 
 takeTwo2 :: a -> a -> a -> b -> c -> c 
 takeTwo2 _ _ _ _ x = x
-
 
 takeIntMul :: a -> a -> a -> b -> b
 takeIntMul _ _ _ x = x
@@ -176,15 +186,7 @@ takeInt x _ _ = x
 takeIntTwo :: Int -> a -> Int
 takeIntTwo x _ = x
 
--- take3, takeMyList, myListTuple, myListMap has a bad argument passed error 
--- however take2 with Either l r successfully work?
-take3 :: Tri a b c -> Tri a b c -> Tri a b c -> Tri a b c -> Tri a b c
-take3 _ _ _ x = x
-
-take2 :: Either l r -> Either l r -> Either l r -> Either l r -> Either l r 
-take2 _ _ _ x = x
-
-
+-- useful test cases for future 
 data Fou a b c d where
   Fou :: a -> b -> c -> Maybe d -> Fou a b c d 
 
