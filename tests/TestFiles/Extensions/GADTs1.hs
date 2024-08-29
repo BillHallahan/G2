659660cbde12f5c452bfd4528e2035085aa52f08
QHWU1228 <qwu22@binghamton.edu>
2024-08-29 16:58:48 -0400

    including test cases for GADTs1.hs and everything should be ready for pull request

--- tests/TestFiles/Extensions/GADTs1.hs
+++ tests/TestFiles/Extensions/GADTs1.hs
@@ -1,6 +1,11 @@
-{-# LANGUAGE DataKinds, GADTs,RankNTypes #-}
+{-# LANGUAGE GADTs, DataKinds, KindSignatures, TypeFamilies #-}
 
 module GADTS1 where
+
+
+import GHC.TypeLits 
+import Data.Kind
+
 data ShapeType = Circle | Rectangle
 
 data Shape where
@@ -13,12 +18,25 @@ area (RectangleShape width height) = width * height
 
 infixr :>
 
-data HList where 
-  Nil :: HList 
-  (:>) :: forall a . (Num a, Show a) => a -> HList -> HList
+-- data HList where 
+--   Nil :: HList 
+--   (:>) :: forall a . (Num a, Show a) => a -> HList -> HList
 
-hlistHeadStr :: HList -> String
-hlistHeadStr (x :> xs) = show (x + 1)
+-- hlistHeadStr :: HList -> String
+-- hlistHeadStr (x :> xs) = show (x + 1)
+
+data MyList2 a = Nis | Conss a (MyList2 a)
+
+lengthList2 :: MyList2 a -> Int
+lengthList2 Nis       = 0
+lengthList2 (Conss _ xs) = 1 + lengthList2 xs
+
+
+-- this above is having an error that says
+-- G2: No type found in typeWithStrName "MutVar#"
+-- CallStack (from HasCallStack):
+--   error, called at src/G2/Initialization/KnownValues.hs:127:10 in g2-0.2.0.0-inplace:G2.Initialization.KnownValues
+
 
 data MyList a where
   Ni :: MyList a
@@ -49,7 +67,6 @@ evalMyExpr (Add a1 a2) = evalMyExpr a1 + evalMyExpr a2
 testeval :: Int -> MyExpr Int 
 testeval a1 = testeval $ evalMyExpr $ Lt (2*a1)
 
-
 checkeq :: Eq a => a -> a -> Bool
 checkeq a a1 = a == a1
 
@@ -59,5 +76,43 @@ id2 x = x
 idlr :: Either l r -> Either l r 
 idlr x = x
 
+data Peano = Succ Peano | Zero 
+
+data Vec :: Peano -> Type -> Type where
+    VNil  :: Vec Zero a
+    VCons :: forall n a. a -> Vec n a -> Vec (Succ n) a
+
+vecLength :: Vec n a -> Integer
+vecLength VNil         = 0
+vecLength (VCons _ xs) = 1 + vecLength xs
+
+vecHead :: Vec (Succ n) a -> a 
+vecHead (VCons x _) = x
+
+vecZip :: Vec n a -> Vec n b -> Vec n (a, b)
+vecZip VNil _ =  VNil 
+vecZip (VCons x xs) (VCons y ys) = VCons (x, y) (vecZip xs ys)
+
 vecZipConc :: Vec (Succ Zero) (Int, Char)
 vecZipConc = vecZip (VCons 1 VNil) (VCons 'a' VNil) 
+
+vecMap :: (a -> b) -> Vec n a -> Vec n b 
+vecMap _ VNil = VNil 
+vecMap f (VCons x xs) = VCons (f x) (vecMap f xs)
+
+vecTail :: Vec (Succ n) a -> Vec n a 
+vecTail (VCons _ xs) = xs
+
+-- Return all the elements of a list except the last one
+vecInit :: Vec (Succ n) a -> Vec n a 
+vecInit (VCons x VNil) = VNil 
+vecInit (VCons x xs@(VCons y ys)) = VCons x (vecInit xs)
+
+
+data Term a where
+    Lit :: Int ->  Term Int
+    Pair :: Term a -> Term b -> Term (a,b)
+
+eval2 :: Term a -> a
+eval2 (Lit i)     = i
+eval2 (Pair a b)  = (eval2 a, eval2 b)
