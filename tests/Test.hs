77a342144e5b566dca101a76dfc668d269e6665a
Bill Hallahan <billthallahan@gmail.com>
2024-12-24 11:45:27 -0500

    Adjust test

--- tests/Test.hs
+++ tests/Test.hs
@@ -436,7 +436,7 @@ testFileTests = testGroup "TestFiles"
                                                       , ("decodeFloatConst", 5000, [Exactly 1])
                                                       , ("decodeFloatCheck", 2000, [AtLeast 2])
                                                       , ("exponentTest", 2000, [AtLeast 6])
-                                                      , ("encodeFloatTest1", 7000, [AtLeast 150]) 
+                                                      , ("encodeFloatTest1", 8000, [AtLeast 215]) 
                                                       , ("significandTest", 2000, [AtLeast 5])
                                                       , ("scaleFloatTest", 2000, [AtLeast 5])
                                                       , ("scaleFloatTest2", 2000, [AtLeast 3])
