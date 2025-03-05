15b548dacc0631d51b7ff1ab23b6fa5147143e7f
Bill Hallahan <whallahan@binhamton.edu>
2025-03-05 14:49:56 -0500

    Only import GHC.Util.Word64 when needed

--- src/G2/Translation/GHC.hs
+++ src/G2/Translation/GHC.hs
@@ -31,7 +31,6 @@ module G2.Translation.GHC ( module GHC
                           , module GHC.Unit.Module.Deps
                           , module GHC.Unit.Module.ModDetails
                           , module GHC.Unit.Module.ModGuts
-                          , module GHC.Utils.Word64
 
                           , HscTarget
 #else
@@ -40,6 +39,9 @@ module G2.Translation.GHC ( module GHC
 #endif
 #if MIN_VERSION_GLASGOW_HASKELL(9,3,0,0)
                           , module GHC.Driver.Config.Tidy
+#endif
+#if MIN_VERSION_GLASGOW_HASKELL(9,8,0,0)
+                          , module GHC.Utils.Word64
 #endif
                           ) where
 import GHC
@@ -79,7 +81,10 @@ import GHC.Types.SrcLoc
 import GHC.Types.Unique
 import GHC.Types.Var
 import GHC.Unit.Types
+
+#if MIN_VERSION_GLASGOW_HASKELL(9,8,0,0)
 import GHC.Utils.Word64
+#endif
 
 #if MIN_VERSION_GLASGOW_HASKELL(9,3,0,0)
 import GHC.Driver.Config.Tidy
