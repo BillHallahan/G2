26ce4c6ce75a2ad9b6beda8beacf5fcc431b694a
BillHallahan <billthallahan@gmail.com>
2025-07-06 00:11:40 -0400

    Nrpc gen pc soundness (#324)
    * Verifying 39
    
    * do deepLookup for argument NRPCs
    
    * Manual Eq
    
    * higher order
    
    * Minor cleanup, new tests
    
    * Comments
    
    * Comments, cleanup
    
    * Commenting out failing verifier tests (fail with timeout)
    
    * Commenting out tests that only passed because of unsound applySolver
    
    * More tests
    
    ---------
    
    Co-authored-by: Bill Hallahan <whallahan@binhamton.edu>

--- tests/Verify/NatList1.hs
+++ tests/Verify/NatList1.hs
@@ -16,14 +16,50 @@ import Prelude
   , ($)
   )
 
-{-# NOINLINE drop #-}
-drop [] = []
-drop (_:xs) = xs
+data Nat = S Nat | Z
+  deriving (Show,Ord)
 
-{-# NOINLINE map #-}
-map :: (a -> a) -> [a] -> [a]
-map f [] = []
-map f (x:xs) = (f x) : (map f xs)
+instance Eq Nat where
+  Z == Z = True
+  S p1 == S p2 = p1 == p2
+  _ == _ = False
 
-prop1 f xs
-  = (drop (map f xs) == map f (drop xs))
+(++) :: [a] -> [a] -> [a]
+[] ++ ys = ys
+(x:xs) ++ ys = x : (xs ++ ys)
+
+drop Z xs = xs
+drop _ [] = []
+drop (S x) (_:xs) = drop x xs
+
+take Z _ = []
+take _ [] = []
+take (S x) (y:ys) = y : (take x ys)
+
+prop1 n xs = take n xs ++ drop n xs == xs
+
+prop1False n xs = take n xs ++ drop n xs == (case xs of [x, y] -> [x, y, 3]; _ -> xs) 
+
+length :: [a] -> Nat
+length [] = Z
+length [_, _, _] = S (S Z)
+length (_:xs) = S (length xs)
+
+lengthBad :: [a] -> Nat
+lengthBad [] = Z
+lengthBad [_, _, _] = S (S Z)
+lengthBad (_:xs) = S (lengthBad xs)
+
+_     < Z     = False
+Z     < _     = True
+(S x) < (S y) = x < y
+
+Z     <= _     = True
+_     <= Z     = False
+(S x) <= (S y) = x <= y
+
+prop2 xs = length xs <= length xs
+
+prop2False xs = length xs < length xs
+
+prop2False' xs = lengthBad xs < lengthBad (():xs)
