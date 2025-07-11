a95e3bf2eb7c732fe05fc0114088477c0e3f18b0
ryanfanchiotti <112440089+ryanfanchiotti@users.noreply.github.com>
2025-07-11 16:46:19 -0400

    SMT string implementations of !?, unsnoc + tests for !=, splitAt (#327)
    * finished unsnoc
    
    * unsnoc tests
    
    * splitAt tests
    
    * tests for total index
    
    * tests for not equal to
    
    * changed base commit
    
    * conditional tests for unsnoc and total index
    
    * fixed conditional tests

--- tests/TestFiles/Strings/Strings1.hs
+++ tests/TestFiles/Strings/Strings1.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 module Strings1 where
 
 import Data.List
@@ -287,10 +289,10 @@ genericSplitAt1 xs n
 -- Note that there should be an fourth case here due to an invalid index error
 genericIndex1 :: String -> Integer -> (Char, Int)
 genericIndex1 xs n
-    | chr == 'Z' = (chr, 0) 
+    | chr == 'Z' = (chr, 0)
     | chr < 'Q' = (chr, 1)
     | otherwise = ('!', 2)
-    where 
+    where
         chr = xs `genericIndex` (n + 1)
 
 -- This doesn't lessen outputs, it only tests whether genericReplicate works with SMT Strings
@@ -324,3 +326,34 @@ isSuffixOf1 s1 s2
     | otherwise = (6, p)
     where
         p = isSuffixOf s1 s2
+
+#if MIN_VERSION_base(4,19,0)
+unsnoc1 :: String -> Maybe Int
+unsnoc1 xs
+    | Just (s, e) <- uc, s == "Hello" = Just 0
+    | Nothing == uc = Just 1
+    | Just (s, e) <- uc, length s == 0 = Just 2
+    | otherwise = Nothing
+    where
+        uc = unsnoc xs
+
+unsnoc2 :: String -> Maybe (String, Char)
+unsnoc2 = unsnoc
+
+totalIndex1 :: String -> [Maybe Char]
+totalIndex1 xs = [xs !? (-1), xs !? 0, xs !? 15]
+#endif
+
+splitAt1 :: String -> Maybe (String, Int)
+splitAt1 xs
+    | a == b = Just (a, 0)
+    | length a < 4 = Just (b, 1)
+    | b == (a ++ a) = Just (b, 2)
+    | otherwise = Nothing
+    where
+        (a, b) = splitAt 10 xs
+
+notEq1 :: String -> Int
+notEq1 s = case s /= "verylongstringverylongstringVERYLONGSTRINGVERYLONGSTRING" of
+                True -> 4
+                False -> 2
