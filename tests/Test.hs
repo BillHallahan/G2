197257d3b0b31327ef1299a3c38174f25dfb6605
Bill Hallahan <whallahan@binhamton.edu>
2025-07-26 23:19:46 -0400

    minor

--- tests/Test.hs
+++ tests/Test.hs
@@ -396,7 +396,7 @@ testFileTests = testGroup "TestFiles"
                                                               , ("reverse2", 2000, [AtLeast 8])
                                                               , ("reverse3", 1000, [ AtLeast 2
                                                                                    , RExists "rev3Returns1"])
-                                                              , ("insert1", 3000, [AtLeast 10])
+                                                              , ("insert1", 1500, [AtLeast 10])
                                                               , ("intersperse1", 3000, [AtLeast 10])
                                                               , ("replicate1", 4000, [AtLeast 5])
                                                               , ("minimum1", 1000, [AtLeast 6])
