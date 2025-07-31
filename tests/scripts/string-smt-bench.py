d577b12c70f9d30e73e85919c8dfcfe6de816646
Bill Hallahan <whallahan@binhamton.edu>
2025-07-31 16:35:03 -0400

    Updating script

--- tests/scripts/string-smt-bench.py
+++ tests/scripts/string-smt-bench.py
@@ -10,9 +10,8 @@ exe_name = str(subprocess.run(["cabal", "exec", "which", "G2"], capture_output =
 smt_solvers = ["z3", "cvc5", "ostrich", "z3-noodler"]
 
 # outputting latex
-def generate_latex(res_all):
+def cov_generate_latex(res_all):
     solvers = ["conc"] + smt_solvers
-
 
     print(r"\begin{tabular}{| l |" + "".join(map(lambda _ : " c | c |", solvers)) + "}")
     print("\hline")
@@ -24,7 +23,25 @@ def generate_latex(res_all):
 
     for (file_dir, bench_res) in res_all:
         line = file_dir + " & "
-        bench_res = map(lambda rl: str(rl[0]) + " & " + str(rl[1]), bench_res)
+        bench_res = map(lambda rl: rl[0] + " & " + rl[1], bench_res)
+        bench_res = " & ".join(bench_res)
+        print(line + bench_res + r"\\ \hline")
+
+    print(r"\end{tabular}")
+
+def cex_generate_latex(res_all):
+    solvers = ["conc"] + smt_solvers
+
+    print(r"\begin{tabular}{| l |" + "".join(map(lambda _ : " c |", solvers)) + "}")
+    print("\hline")
+    multi_smt = " & ".join(solvers)
+    print(" & " + multi_smt + r"\\ \hline")
+
+    found_lab = " & ".join(map(lambda s : r" Time (s)", solvers))
+    print("Benchmark & " + found_lab + r"\\ \hline")
+
+    for (file_dir, bench_res) in res_all:
+        line = file_dir + " & "
         bench_res = " & ".join(bench_res)
         print(line + bench_res + r"\\ \hline")
 
@@ -74,19 +91,19 @@ def call_g2_process(filename, func, var_settings, to):
     except subprocess.TimeoutExpired:
         return ""
 
-def run_bench(filename, var_settings, timeout, solver):
+def run_bench(filename, func, var_settings, timeout, solver):
     # --include nofib-symbolic/common --hpc --hpc-print-times --no-step-limit --search subpath --time 60
-    settings = ["--include", "nofib-symbolic/common", "--hpc", "--hpc-print-times"
-               , "--no-step-limit", "--search", "subpath", "--measure-coverage"
+    settings = ["--include", "nofib-symbolic/common"
+               , "--no-step-limit", "--search", "subpath"
                , "--time", str(timeout)
                , "--smt", solver] + var_settings
     print(*settings)
-    return run_g2(filename, "main", settings, timeout)
+    return run_g2(filename, func, settings, timeout)
 
-def run_bench_smt(filename, var_settings, timeout, solver):
-    return run_bench(filename, ["--smt-strings"] + var_settings, timeout, solver)
+def run_bench_smt(filename, func, var_settings, timeout, solver):
+    return run_bench(filename, func, ["--smt-strings"] + var_settings, timeout, solver)
 
-def process_output(out):
+def cov_process_output(out):
     reached = re.search(r"Ticks reached: (\d*)", out)
     total = re.search(r"Tick num: (\d*)", out)
     last = re.search(r"Last tick reached: ((\d|\.)*)", out)
@@ -108,7 +125,20 @@ def process_output(out):
         print("last time = " + last.group(1))
         print("all_times = " + str(all_times))
     
-    return (hpc_reached, last)
+    if last != None:
+        last = str(last[1])
+    else:
+        last = "-"
+    
+    return (str(hpc_reached), last)
+
+def cex_process_output(out):
+    found = re.search(r"State Accepted Time: ((\d|\.)*)", out)
+    if found != None:
+        print("Found counterexample = " + str(found[1]))
+        return str(found[1])
+    else:
+        return "-"
 
 def run_nofib_set(setname, var_settings, timeout):
         setpath = os.path.join("string-to-smt-benchmark/", setname)
@@ -117,6 +147,8 @@ def run_nofib_set(setname, var_settings, timeout):
         okasaki_bench = ["BinaryHeap.hs", "LeftistHeap.hs", "RedBlackTree.hs"]
 
         print(setpath)
+
+        cov_settings = [ "--hpc", "--hpc-print-times", "--measure-coverage" ] + var_settings
 
         res_all = []
 
@@ -128,23 +160,60 @@ def run_nofib_set(setname, var_settings, timeout):
                 final_path = os.path.join(bench_path, "Main.hs") if not isOkasaki else bench_path
                 if os.path.isfile(final_path):
                     print(file_dir);
-                    res_base = run_bench(final_path, var_settings, timeout, "z3")
+                    res_base = run_bench(final_path, "main", cov_settings, timeout, "z3")
                     print("Baseline:")
-                    last_reached = process_output(res_base)
+                    last_reached = cov_process_output(res_base)
                     res_bench.append(last_reached)
                     for solver_name in smt_solvers:
                         solver = "z3" if solver_name == "z3-noodler" else solver_name
-                        extra_var = ["--smt-path", "z3-noodler"] if solver == "z3-noodler" else []
-                        res_smt = run_bench_smt(final_path, var_settings + extra_var, timeout, solver)
+                        extra_var = ["--smt-path", "z3-noodler"] if solver_name == "z3-noodler" else []
+                        print(extra_var)
+                        res_smt = run_bench_smt(final_path, "main", cov_settings + extra_var, timeout, solver)
                         print("SMT " + solver_name + ":")
-                        last_reached = process_output(res_smt)
+                        last_reached = cov_process_output(res_smt)
                         res_bench.append(last_reached)
                 res_all.append((file_dir, res_bench))
 
         return res_all
 
-res_imag = run_nofib_set("nofib-symbolic/imaginary", [], 120)
-res_spec = run_nofib_set("nofib-symbolic/spectral", [], 120)
-res_progs = run_nofib_set("programs", [], 120)
+def run_properties(setname, filename, var_settings, timeout, properties):
+        setpath = os.path.join("string-to-smt-benchmark/", setname)
 
-generate_latex(res_imag + res_spec + res_progs)
+        bench_path = os.path.join(setpath, filename)
+        print(bench_path)
+
+        only_one = var_settings + ["--max-outputs", "1", "--accept-times"]
+
+        res_all = []
+
+        for prop in properties:
+            print(prop)
+            res_bench = []
+            res_base = run_bench(bench_path, prop, only_one, timeout, "z3")
+            print("Baseline:")
+            last_reached = cex_process_output(res_base)
+            res_bench.append(last_reached)
+            for solver_name in smt_solvers:
+                solver = "z3" if solver_name == "z3-noodler" else solver_name
+                extra_var = ["--smt-path", "z3-noodler"] if solver_name == "z3-noodler" else []
+                res_smt = run_bench_smt(bench_path, prop, only_one + extra_var, timeout, solver)
+                print("SMT " + solver_name + ":")
+                found_cex = cex_process_output(res_smt)
+                res_bench.append(found_cex)
+            res_all.append((prop, res_bench))
+
+        return res_all
+
+time_lim = 2
+
+res_imag = run_nofib_set("nofib-symbolic/imaginary", [], time_lim)
+# res_spec = run_nofib_set("nofib-symbolic/spectral", [], time_lim)
+# res_progs = run_nofib_set("programs", [], time_lim)
+
+cov_generate_latex(res_imag)
+# generate_latex(res_imag + res_spec + res_progs)
+
+props = map(lambda x : "prop" + str(x), list(range(1, 15)))
+res_props = run_properties("properties", "ParamProperties", [], time_lim, props)
+
+cex_generate_latex(res_props)
