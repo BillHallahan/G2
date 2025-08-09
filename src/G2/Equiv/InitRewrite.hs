680c339d4c261b31bc858123cb58efedd324a0e8
Bill Hallahan <whallahan@binhamton.edu>
2025-08-09 19:38:54 -0400

    Setting up TyVars correctly in Nebula

--- src/G2/Equiv/InitRewrite.hs
+++ src/G2/Equiv/InitRewrite.hs
@@ -1,17 +1,24 @@
 module G2.Equiv.InitRewrite (initWithRHS, initWithLHS) where
 
+import G2.Execution.Memory
 import G2.Language
 import qualified G2.Language.ExprEnv as E
 import qualified G2.Language.Typing as T
+import qualified G2.Language.TyVarEnv as TV
 import qualified G2.Language.Expr as X
 
-import G2.Execution.Memory
+import Data.List
+
+import Debug.Trace
 
 initWithRHS :: State t -> Bindings -> RewriteRule -> (State t, Bindings)
 initWithRHS s b r =
-  let s' = s {
+  let
+      (eenv', tv_env') = updateExprEnvAndTyVarEnv (ru_bndrs r) (expr_env s) (tyvar_env s)
+      s' = s {
              curr_expr = CurrExpr Evaluate (ru_rhs r)
-           , expr_env = foldr E.insertSymbolic (expr_env s) (ru_bndrs r)
+           , expr_env = eenv'
+           , tyvar_env = tv_env'
            }
       b' = b { input_names = map idName $ ru_bndrs r }
   in
@@ -29,10 +36,19 @@ initWithLHS s b r =
                   i = Id f_name t
                   v = Var i
                   app = X.mkApp (v:ru_args r)
+                  (eenv', tv_env') = updateExprEnvAndTyVarEnv (ru_bndrs r) (expr_env s) (tyvar_env s)
                   s' = s {
                          curr_expr = CurrExpr Evaluate app
-                       , expr_env = foldr E.insertSymbolic (expr_env s) (ru_bndrs r)
+                       , expr_env = eenv'
+                       , tyvar_env = tv_env'
                        }
                   b' = b { input_names = map idName $ ru_bndrs r }
               in
               (markAndSweepPreserving emptyMemConfig s' b', b')
+
+updateExprEnvAndTyVarEnv :: [Id] -> ExprEnv -> TyVarEnv -> (ExprEnv, TyVarEnv)
+updateExprEnvAndTyVarEnv is eenv tv_env =
+    let
+        (ty_is, v_is) = partition (hasTYPE . typeOf tv_env) is
+    in
+    (foldr E.insertSymbolic eenv v_is, foldr TV.insertSymbolic tv_env ty_is)
