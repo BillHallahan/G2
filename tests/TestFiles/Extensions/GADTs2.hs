fa4c9db293c6d97c97c2a0bdd5838aac10ca22b7
QHWU1228 <qwu22@binghamton.edu>
2025-08-22 15:12:53 -0400

    including comments for further instructions on idea for building test cases

--- tests/TestFiles/Extensions/GADTs2.hs
+++ tests/TestFiles/Extensions/GADTs2.hs
@@ -4,3 +4,8 @@ module GADTs2 where
 
 import GHC.TypeLits 
 import Data.Kind
+
+-- This file aims to provide more test cases to ensure that GADT works with G2
+-- When trying to verify GADT code, we always use the flag --inst-after, --validate
+-- It's also recommended that not to try gadt invovle recursion since it's will get pretty complicated pretty quick
+
