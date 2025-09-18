4fe7e6f9585f2346f1a5507956bb1a5a41b0f177
QHWU1228 <qwu22@binghamton.edu>
2025-09-18 16:27:55 -0400

    including additional test cases

--- tests/TestFiles/Extensions/GADTs3.hs
+++ tests/TestFiles/Extensions/GADTs3.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, DataKinds, KindSignatures, RankNTypes, TypeFamilies, FlexibleInstances, FlexibleContexts #-}
+{-# LANGUAGE GADTs, DataKinds, KindSignatures, RankNTypes, TypeFamilies, FlexibleInstances, FlexibleContexts, StandaloneDeriving #-}
 
 module GADTs3 where
 
@@ -44,10 +44,21 @@ data Pairy :: * -> * -> * -> * where
   PInt  :: Int  -> Pairy Int  Bool String
   PBool :: Bool -> Pairy Bool Char Double
 
+deriving instance Eq (Pairy Int Bool String)
+deriving instance Eq (Pairy Bool Char Double)
+
 -- G2: evalVar: bad input.Id (Name "$fShowBool" (Just "GHC.Show")
 showPairy :: Pairy a b c -> String
 showPairy (PInt n)  = "Int: "  ++ show n
 showPairy (PBool b) = "Bool: " ++ show b
+
+modifyPairy :: Pairy a b c -> Pairy a b c
+modifyPairy (PInt n)  = PInt (n + 1)      -- increment Int
+modifyPairy (PBool b) = PBool (not b)     -- flip Bool
+
+pairyToInt :: Pairy a b c -> Int
+pairyToInt (PInt n)  = if even n then 0 else 1
+pairyToInt (PBool b) = if b then 1 else 0
 
 pairyTag :: Pairy a b c -> Int
 pairyTag (PInt _)  = 1
@@ -69,6 +80,9 @@ data Tripley :: * -> * -> * -> * where
   Case3 :: [a]    -> Tripley [a]    [b]    [c]
 
 -- it seems like show is generally having a problem in validation and inst-after
+-- currently, almost all the function works beside showPairy and showTripley as well as countLeaves
+-- showPairy and showTripley all have G2: evalVar: bad input.Id error 
+-- while countLeaves is just executing for too long and the process kills it 
 showTripley :: Show a => Tripley a b c -> String
 showTripley (Case1 ch) = "Char: "   ++ show ch
 showTripley (Case2 d)  = "Double: " ++ show d
@@ -87,13 +101,27 @@ unwrapMaybe :: WrapPoly Maybe a -> Maybe a
 unwrapMaybe (MkWrap x) = x
 
 -- Refined parameter that is a newtype
-newtype WrapInt = WrapInt Int
+newtype WrapInt = WrapInt Int 
+    deriving (Eq, Show)
 
 data Newty where
-  MkNew :: WrapInt -> Newty
+  MkNew :: WrapInt -> Newty 
+deriving instance Eq Newty
 
 unwrapNewty :: Newty -> Int
 unwrapNewty (MkNew (WrapInt n)) = n
+
+-- Here's the G2 output:
+-- incrementNewty (MkNew ((coerce (0 :: Int)) :: WrapInt)) = MkNew ((coerce (1 :: Int)) :: WrapInt)
+-- TODO: I know it's correct but is this a valid response we can show to the user?
+incrementNewty :: Newty -> Newty
+incrementNewty (MkNew (WrapInt n)) = MkNew (WrapInt (n + 1))
+
+scaleNewty :: Int -> Newty -> Newty
+scaleNewty factor (MkNew (WrapInt n)) = MkNew (WrapInt (n * factor))
+
+isEvenNewty :: Newty -> Bool
+isEvenNewty (MkNew (WrapInt n)) = n `mod` 2 == 0
 
 -- Self-recursive and mutual GADTs
 data Tree a where
@@ -105,6 +133,7 @@ data Forest a where
   FCons :: Tree a -> Forest a -> Forest a
 
 -- mutual GADT is running way too long and it have its process being killed 
+-- TODO: should we even test this function?
 countLeaves :: Tree a -> Int
 countLeaves (Leaf _)   = 1
 countLeaves (Node l r) = countLeaves l + countLeaves r
