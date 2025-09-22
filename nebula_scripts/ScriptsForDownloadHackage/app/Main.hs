4bc3d57edc22c9a6a2b4f62b40ab97a367904778
QHWU1228 <qwu22@binghamton.edu>
2025-09-22 16:52:49 -0400

    reformat the scripts

--- nebula_scripts/ScriptsForDownloadHackage/app/Main.hs
+++ nebula_scripts/ScriptsForDownloadHackage/app/Main.hs
@@ -9,11 +9,9 @@ main :: IO ()
 main = do
   let toUrl (name, ver) = "http://hackage.haskell.org/package/" ++ name ++ "/" ++ name ++ "-" ++ ver ++ ".tar.gz"
   urls <- map (toUrl . last) . groupOn fst . map word1 . lines <$> systemOutput_ "cabal list --simple"
-  -- writeFile "_urls.txt" $ unlines urls
+  writeFile "_urls.txt" $ unlines urls
   -- Create the folder if it doesn't exist
   createDirectoryIfMissing True "all_hackage"
-  -- Write _urls.txt into all_hackage/
-  writeFile "all_hackage/_urls.txt" $ unlines urls
 
   -- Tell wget to dump files into "all_hackage/"
   system_ "wget -P all_hackage --input-file=_urls.txt"
