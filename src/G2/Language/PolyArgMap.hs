32923b8c0b269c7e5bcc543d09c05ef5af28bb28
JacobGH2 <jhelhoski253@gmail.com>
2025-09-27 18:22:55 -0400

    PAM contains fields, TARM not implemented

--- src/G2/Language/PolyArgMap.hs
+++ src/G2/Language/PolyArgMap.hs
@@ -5,7 +5,6 @@ module G2.Language.PolyArgMap ( PolyArgMap
                                , insertRename
                                , lookup
                                , member
-                               , remove
                                , toList
                                , fromList
                                , empty ) where
@@ -26,14 +25,14 @@ import Data.Maybe (isJust)
 -- If the name is a val of the TV type, we already know it's type.
 data ValOrFunc = Val | Func Type deriving (Show, Eq, Read, Data, Typeable, Generic)
 data PAMEntry = PAMEntry {envN :: Name, runN :: Name, vOrF :: ValOrFunc} deriving (Show, Eq, Read, Data, Typeable, Generic)
-newtype PolyArgMap = PolyArgMap (HM.HashMap Name [PAMEntry]) deriving (Show, Eq, Read, Data, Typeable, Generic)
+data PolyArgMap = PolyArgMap {poly_arg_map :: HM.HashMap Name [PAMEntry], run_to_env :: HM.HashMap Name Name} deriving (Show, Eq, Read, Data, Typeable, Generic)
 
 insertTV :: Name -> PolyArgMap -> PolyArgMap
-insertTV tv (PolyArgMap pargm) = PolyArgMap $ HM.insert tv [] pargm
+insertTV tv pam = pam {poly_arg_map = HM.insert tv [] $ poly_arg_map pam}
 
 insertRename :: Name -> Name -> Name -> Maybe Type -> PolyArgMap -> PolyArgMap
-insertRename tv env ren ty (PolyArgMap pargm) | HM.member tv pargm =
-    PolyArgMap $ HM.adjust (modifyPAMEntries env ren ty) tv pargm
+insertRename tv env ren ty pam | HM.member tv (poly_arg_map pam) =
+    pam {poly_arg_map = HM.adjust (modifyPAMEntries env ren ty) tv (poly_arg_map pam)} 
   | otherwise = error ("PolyArgMap.insertRename: trying to insert into set of TyVar not in map: " ++ show tv)
   where modifyPAMEntries :: Name -> Name -> Maybe Type -> [PAMEntry] -> [PAMEntry]
         modifyPAMEntries e r mt pes = case find (\pe -> envN pe == e) pes of
@@ -41,31 +40,26 @@ insertRename tv env ren ty (PolyArgMap pargm) | HM.member tv pargm =
             Nothing -> (PAMEntry {envN=e, runN=r, vOrF=maybe Val Func mt}):pes
 
 lookup :: Name -> PolyArgMap -> Maybe [(Name, Name, Maybe Type)]
-lookup tv (PolyArgMap pargm) = case HM.lookup tv pargm of
-                    Just entries -> Just (map (\ent -> (envN ent, runN ent, (\case Val -> Nothing; Func t -> Just t) (vOrF ent))) entries)
+lookup tv pam = case HM.lookup tv (poly_arg_map pam) of
+                    Just entries -> Just (map makeEntryTuple entries)
                     Nothing -> Nothing
 
 member :: Name -> PolyArgMap -> Bool
 member n pam = isJust (lookup n pam)
 
-remove :: Name -> PolyArgMap -> PolyArgMap
-remove i (PolyArgMap pargm) = let pargm' = HM.delete i pargm in
-            if pargm == pargm'
-            then error "PolyArgMap.remove: removing nonexistent TV"
-            else PolyArgMap pargm'
-
 toList :: PolyArgMap -> [(Name, [(Name, Name, Maybe Type)])]
-toList (PolyArgMap pargm) = map (second (map makeEntryTuple)) (HM.toList pargm)
-    where makeEntryTuple :: PAMEntry -> (Name, Name, Maybe Type)
-          makeEntryTuple (PAMEntry e r vorf) = (e, r, (\case Val -> Nothing; Func t -> Just t) vorf)
+toList pam = map (second (map makeEntryTuple)) (HM.toList $ poly_arg_map pam)
+             
+makeEntryTuple :: PAMEntry -> (Name, Name, Maybe Type)
+makeEntryTuple (PAMEntry e r vorf) = (e, r, (\case Val -> Nothing; Func t -> Just t) vorf)
 
 fromList :: [(Name, [(Name, Name, Maybe Type)])] -> PolyArgMap
-fromList l = PolyArgMap $ HM.fromList (map (second (map fromEntryTuple)) l)
+fromList l = PolyArgMap {poly_arg_map = HM.fromList (map (second (map fromEntryTuple)) l), run_to_env = HM.empty} 
     where fromEntryTuple :: (Name, Name, Maybe Type) -> PAMEntry
           fromEntryTuple (e, r, mt) = PAMEntry {envN=e, runN=r, vOrF=maybe Val Func mt}
 
 empty :: PolyArgMap
-empty = PolyArgMap HM.empty
+empty = PolyArgMap HM.empty HM.empty
 
 instance Hashable ValOrFunc
 instance Hashable PAMEntry
