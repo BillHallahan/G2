55a130019c93eea5bf4cfdc1d6ea94f8e5e0be5c
Bill Hallahan <whallahan@binhamton.edu>
2025-11-21 10:53:28 -0500

    New test

--- tests/Test.hs
+++ tests/Test.hs
@@ -486,6 +486,9 @@ testFileTests = testGroup "TestFiles"
                                         , ("lines1", 4000, [AtLeast 10])
 
                                         , ("showInt1", 4000, [Exactly 2])
+
+                                        , ("test1", 800, [ AtLeast 1
+                                                         , RExists "test1InitTailEq"])
                                         ]
     , checkInputOutputsSMTStringsWithSubPath "tests/TestFiles/Strings/Strings1.hs"
                                         [ ("lines2", 2000, [AtLeast 10])
