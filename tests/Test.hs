d66dc9d53e711e49929cfd081316b4f19de61e6d
Bill Hallahan <whallahan@binhamton.edu>
2025-12-23 18:42:22 -0500

    Tests, base

--- tests/Test.hs
+++ tests/Test.hs
@@ -520,11 +520,11 @@ testFileTests = testGroup "TestFiles"
                                                               , ("con2Int", 1000, [Exactly 3])
                                                               , ("con2Integer", 1000, [Exactly 3])
                                                               , ("con2Float", 1000, [Exactly 3])
-                                                              , ("con2Double", 1000, [Exactly 3])
-                                                              , ("con3Int", 1000, [Exactly 3])
-                                                              , ("con3Integer", 1000, [Exactly 3])
-                                                              , ("con3Float", 1000, [Exactly 3])
-                                                              , ("con3Double", 1000, [Exactly 3]) ]
+                                                              , ("con2Double", 1000, [Exactly 3]) ]
+                                                            --   , ("con3Int", 1000, [Exactly 3])
+                                                            --   , ("con3Integer", 1000, [Exactly 3])
+                                                            --   , ("con3Float", 1000, [Exactly 3])
+                                                            --   , ("con3Double", 1000, [Exactly 3]) ]
     , checkInputOutputsSMTListsWith "tests/TestFiles/Seq/Seq1.hs" "floatListEq" [ ("conFloat", 1000, [Exactly 1])
                                                                                 , ("conDouble", 1000, [Exactly 1])]
 
