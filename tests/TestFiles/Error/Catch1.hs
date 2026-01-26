6d9787a3851306b42403c5601d53685f28929081
Bill Hallahan <whallahan@binhamton.edu>
2026-01-25 20:01:01 -0500

    Tests

---
+++ tests/TestFiles/Error/Catch1.hs
@@ -0,0 +1,15 @@
+module Catch1 where
+
+import Control.Exception
+import System.IO.Unsafe
+
+f :: Int -> Int
+f x = unsafePerformIO $ do
+    catch (error "error") (\(_ :: SomeException) -> return x)
+
+g :: Int -> Int
+g x = unsafePerformIO $ do
+    catch (case x >= 0 of
+                True -> return 5
+                False -> error "Negative")
+          (\(_ :: SomeException) -> return x)
