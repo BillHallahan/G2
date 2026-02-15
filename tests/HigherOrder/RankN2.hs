5adc6b6333fc7c65baec6ac816498733e3067db4
JacobGH2 <jhelhoski253@gmail.com>
2026-02-15 13:18:38 -0500

    New tests

--- tests/HigherOrder/RankN2.hs
+++ tests/HigherOrder/RankN2.hs
@@ -2,14 +2,9 @@
 
 module RankN2 where
 
-identity :: (forall a. a -> a) -> Int
-identity f = f 1
-
-identity_in_tuple :: (forall a. a -> (a, a)) -> (forall a. a -> a) -> ((Int, Int), Int)
-identity_in_tuple f g = (f 3, g 5)
-
-twoArgs :: (forall a. a -> a -> a) -> Int
-twoArgs f = f 1 2
+-- Identity functions
+identityInTuple :: (forall a. a -> (a, a)) -> (forall a. a -> a) -> ((Int, Int), Int)
+identityInTuple f g = (f 3, g 5)
 
 intArg :: (forall a. a -> Int -> a) -> Int
 intArg f = f 1 2
@@ -17,33 +12,48 @@ intArg f = f 1 2
 identityTwoTvs :: (forall b a c. a -> a) -> Int
 identityTwoTvs f = f 1
 
-data Tree a = Leaf a | Inter (Tree a) (Tree a)
-data Boxed a = Box a
+-- ADT arguments
+data Tree a = Leaf a | Inter (Tree a) (Tree a) deriving (Eq, Show)
+data Boxed a = Box a deriving (Eq, Show)
 
+boxedArg :: (forall a. a -> Boxed a -> a) -> Int
+boxedArg f = f 4 (Box 1)
 
-adt_arg :: (forall a. a -> Boxed a -> a) -> Int
-adt_arg f = f 4 (Box 1)
+treeArg :: (forall a. Tree a -> a) -> Int
+treeArg f = f (Inter (Leaf 3) (Inter (Leaf 7) (Leaf 8)))
 
-adt_arg2 :: (forall a. Boxed a -> Boxed a -> a) -> Int
-adt_arg2 f = f (Box 1) (Box 2)
+-- ADT return values
+boxedRet :: (forall a. a -> Boxed a) -> Boxed Int
+boxedRet f = f 3
 
-adt_maybe :: (forall a. Maybe a -> a) -> Int 
-adt_maybe f = f (Just 4)
+treeRet :: (forall a. a -> a -> Tree a) -> Tree Int
+treeRet f = f 5 7
 
-adt_tree :: (forall a. Tree a -> a) -> Int
-adt_tree f = f (Inter (Leaf 3) (Inter (Leaf 7) (Leaf 8)))
+treeRet2 :: (forall a b. a -> b -> (Boxed Int, (Boxed (Boxed b), Boxed (Boxed (Boxed a))))) -> (Boxed Int, (Boxed (Boxed Bool), Boxed (Boxed (Boxed Int))))
+treeRet2 f = f 5 False
 
-adt_ret :: (forall a. a -> Boxed a) -> Boxed Int
-adt_ret f = f 3
+listRet :: (forall a. a -> a -> [a]) -> [Int]
+listRet f = f 4 5
 
-ret_tree :: (forall a. a -> a -> Tree a) -> Tree Int
-ret_tree f = f 5 7
+literalRet :: (forall a. Boxed a -> a -> Int) -> Int
+literalRet f = case f (Box 5) 23 of 
+                5 -> 10
+                4 -> 8
+                _ -> 3
 
-ret_list :: (forall a. a -> a -> [a]) -> [Int]
-ret_list f = f 4 5
+literalAndBoxedRet :: (forall a. a -> (Boxed Int, Boxed a)) -> (Boxed Int, Boxed Bool)
+literalAndBoxedRet f = f True
 
-func_arg :: (forall a. (a -> a) -> a -> a) -> Int
-func_arg f = f (\x -> x + 1) 3
+-- Function arguments
+funcArg :: (forall a. (a -> a) -> a -> a) -> Int
+funcArg f = f (\x -> x + 1) 3
 
-func_arg_adt :: (forall a. (Boxed a -> a) -> Boxed a -> a) -> Int
-func_arg_adt f = f (\(Box x) -> x) (Box 3)
+funcArgBoxedArg :: (forall a. (Boxed a -> a) -> Boxed a -> a) -> Int
+funcArgBoxedArg f = f (\(Box x) -> x) (Box 3)
+
+funcArgBoxedLitArg :: (forall a. a -> (Boxed Int -> a -> a) -> a) -> Bool
+funcArgBoxedLitArg f = f False (\(Box i) x -> x)
+
+-- Composition required
+twoApplicationsNeeded :: (forall a b c. (a -> b) -> (b -> c) -> a -> c) -> Int
+twoApplicationsNeeded f = f (\x -> x) (\x -> x) 7
