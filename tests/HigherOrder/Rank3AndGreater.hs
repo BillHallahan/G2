34a25255c8ca7f962c1bb1f486d38ab8a5abdbfa
JacobGH2 <jhelhoski253@gmail.com>
2026-02-26 11:42:19 -0500

    Rank2A tests check exact number of outputs.

--- tests/HigherOrder/Rank3AndGreater.hs
+++ tests/HigherOrder/Rank3AndGreater.hs
@@ -46,6 +46,6 @@ forallWithPolyFuncArgBox f = f (\x -> Box x) True
 forallWithPolyFuncArgTup :: (forall a b. (forall c d. c -> d -> (c, d)) -> a -> b -> (a, b)) -> (Bool, Int)
 forallWithPolyFuncArgTup f = f (\x y -> (x, y)) True 7
 
--- TODO: think partial application
-adtForall :: (forall a. (forall m. a -> a -> m a) -> a -> (a -> ma)) -> (Int -> Boxed Int)
-adtForall f = f (\x wx -> wx) 4
+{- -- TODO: think about partial application
+adtForall :: (forall a. (forall m. a -> a -> m a) -> a -> (a -> m a)) -> (Int -> Boxed Int)
+adtForall f = f (\x wx -> wx) 4 -}
