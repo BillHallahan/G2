6a20214a5769d0e2748d75161a34b679bc7fd226
Bill Hallahan <whallahan@binhamton.edu>
2026-03-01 21:12:01 -0500

    Fixing order of matchPairs- have to check for symbolic variables before coercions

--- src/G2/Execution/Rules.hs
+++ src/G2/Execution/Rules.hs
@@ -1235,12 +1235,6 @@ matchPairs tvnv kv e1 e2 eenv_pc_ee@(eenv, pc, ees)
     
     | e1 == e2 = Just eenv_pc_ee
 
-    | Cast e1' c1 <- e1
-    , Cast e2' c2 <- e2
-    , T.tyVarSubst tvnv c1 == T.tyVarSubst tvnv c2 =  matchPairs tvnv kv e1' e2' eenv_pc_ee
-    | Cast e1' _ <- e1 =  matchPairs tvnv kv e1' e2 eenv_pc_ee
-    | Cast e2' _ <- e2 =  matchPairs tvnv kv e1 e2' eenv_pc_ee
-
     | isExprValueForm eenv e1
     , isExprValueForm eenv e2
     , t1 <- typeOf tvnv e1
@@ -1261,6 +1255,12 @@ matchPairs tvnv kv e1 e2 eenv_pc_ee@(eenv, pc, ees)
     , E.isSymbolic n eenv
     , not (isPrimType t || t == tyBool kv) =
         Just (E.insert n e1 eenv, pc, ees)
+
+    | Cast e1' c1 <- e1
+    , Cast e2' c2 <- e2
+    , T.tyVarSubst tvnv c1 == T.tyVarSubst tvnv c2 =  matchPairs tvnv kv e1' e2' eenv_pc_ee
+    | Cast e1' _ <- e1 =  matchPairs tvnv kv e1' e2 eenv_pc_ee
+    | Cast e2' _ <- e2 =  matchPairs tvnv kv e1 e2' eenv_pc_ee
 
     | Data dc1:es1 <- unApp e1
     , Data dc2:es2 <- unApp e2 =
