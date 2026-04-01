ff18c1b76f48335f3ee14319dde43d5c719f5a69
Bill Hallahan <whallahan@binhamton.edu>
2026-04-01 14:26:53 -0400

    Strictness, eliminate conversion to Strings

--- src/G2/Translation/Haskell.hs
+++ src/G2/Translation/Haskell.hs
@@ -44,10 +44,11 @@ import qualified G2.Translation.TransTypes as G2
 import G2.Translation.GHC
 
 import Control.Monad
-import qualified Control.Monad.State.Lazy as SM
+import qualified Control.Monad.State.Strict as SM
 
 import qualified Data.Array as A
 import qualified Data.ByteString.Char8 as C
+import Data.Coerce
 import Data.Foldable
 import Data.List
 import Data.List.Split
@@ -55,6 +56,7 @@ import Data.Maybe
 import qualified Data.HashMap.Lazy as HM
 import qualified Data.HashSet as HS
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Data.Word
 import System.FilePath
 import System.Directory
@@ -559,11 +561,11 @@ mkIdLookup i = do
 mkName :: Name -> G2.Name
 mkName name = G2.Name occ mdl unq sp
   where
-    occ = T.pack . occNameString . nameOccName $ name
+    occ = T.decodeUtf8Lenient . bytesFS . getOccFS $ name
     unq = convertUnq . getKey . nameUnique $ name
     mdl = case nameModule_maybe name of
               Nothing -> Nothing
-              Just md -> switchModule (T.pack . moduleNameString . moduleName $ md)
+              Just md -> switchModule (T.decodeUtf8Lenient . bytesFS . coerce . moduleName $ md)
 
     sp = mkSpan $ getSrcSpan name
 
@@ -591,11 +593,11 @@ nameLookup nm name = do
                           Just (G2.Name n' m i _) -> (G2.Name n' m i sp, nm)
                           Nothing -> let n = G2.Name occ mdl unq sp in (n, HM.insert (occ, mdl) n nm)
     where
-        occ = T.pack . occNameString . nameOccName $ name
+        occ = T.decodeUtf8Lenient . bytesFS . getOccFS $ name
         unq = convertUnq . getKey . nameUnique $ name
         mdl = case nameModule_maybe name of
                   Nothing -> Nothing
-                  Just md -> switchModule (T.pack . moduleNameString . moduleName $ md)
+                  Just md -> switchModule (T.decodeUtf8Lenient . bytesFS . coerce . moduleName $ md)
 
         sp = mkSpan $ getSrcSpan name
 
