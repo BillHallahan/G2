e7845b7420baaa9487e91870bd279f104b960471
BillHallahan <billthallahan@gmail.com>
2026-06-01 23:43:39 -0400

    Test for flip, updating base (#510)
    * Test for flip, updating base
    
    * Updating base
    
    * Updating test
    
    * Further adjustments to test
    
    ---------
    
    Co-authored-by: Bill Hallahan <whallahan@binhamton.edu>

--- tests/BaseTests/Other.hs
+++ tests/BaseTests/Other.hs
@@ -4,3 +4,9 @@ import Data.List
 
 check4VeryEasy2 :: Bool
 check4VeryEasy2 = all (0 <=) [0]
+
+callFlip :: Int -> Int -> Bool
+callFlip = flip p
+    where
+        p 0 1 = True
+        p _ _ = False
