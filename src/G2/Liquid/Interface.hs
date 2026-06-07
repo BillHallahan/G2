f19de8fcc0b3ad18a060c25aec3042e7cd17569c
Ryan Fanchiotti <ryan@rur.com>
2026-06-06 20:02:58 -0400

    test run for removal in prim eval

--- src/G2/Liquid/Interface.hs
+++ src/G2/Liquid/Interface.hs
@@ -211,7 +211,7 @@ fromLiquidReadyState :: State ()
 fromLiquidReadyState init_state ifi bindings ghci ph_tyvars lhconfig memconfig config = do
     let init_state' = (markAndSweepPreserving (reqNames init_state `mappend` memconfig) init_state bindings)
         use_lams = using_smt_lams config == UseSMTLams && smt config == ConZ3
-        use_lts = literal_tables config
+        use_lts = literal_tables config == UseLiteralTables
         cleaned_state = init_state' { type_env = type_env init_state }
 
         cleaned_state' = cleaned_state { known_values = recalcSmtStringFuncs
