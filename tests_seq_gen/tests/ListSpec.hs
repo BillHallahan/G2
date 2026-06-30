7d0f7ac3d525c92cd58851cab89e8a903edb8da7
Abha Chaudhary <60589332+abhachaudhary@users.noreply.github.com>
2026-06-30 14:38:46 -0400

    Synthesize specification for library functions (#533)
    * Synthesize specification for library functions
    
    * Resolved failed checks
    
    * Another run failed error

---
+++ tests_seq_gen/tests/ListSpec.hs
@@ -0,0 +1,34 @@
+module Test where 
+
+-- import Data.List
+import GHC.Stack
+import Control.Exception
+import Prelude hiding (reverse, last, elem, map)
+
+my_reverse :: String -> String
+my_reverse []     = []
+my_reverse (x:xs) = my_reverse xs ++ [x]
+
+elem :: Char -> String -> Bool
+elem _ []     = False
+elem y (x:xs)
+  | y == x    = True
+  | otherwise = elem y xs
+
+-- TO-DO: Synthesizer for specification, does not support exception yet
+-- last :: String -> Char
+-- last [x]    = x
+-- last (_:xs) = last xs
+
+delete :: Char -> String -> String
+delete  _ []        = []
+delete x (y:ys)    = if x == y then ys else y : delete x ys
+
+
+-- Throwinf error for Ints: (error "invalid grammar, must have at least one rule for each non-terminal symbol")
+insert :: Char -> [Char] -> [Char]
+insert x [] = [x]
+insert x ys@(y:ys')
+ = case compare x y of
+     GT -> y : insert x ys'
+     _  -> x : ys
