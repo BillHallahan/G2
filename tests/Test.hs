a76a582c926bc9ebbfc0108bd36bc5bbb09b06b1
Bill Hallahan <whallahan@binhamton.edu>
2026-07-17 17:23:06 -0400

    Minor fixes to tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -657,11 +657,11 @@ testFileTests = testGroup "TestFiles"
                                                         , ("listLen3", 1000, [Exactly 2])
                                                         , ("listApp", 10000, [Exactly 3])
                                                         , ("take1", 5000, [Exactly 2])
-                                                        , ("drop1", 5000, [Exactly 3])
+                                                        , ("drop1", 5000, [Exactly 4])
                                                         , ("compLen", 5000, [Exactly 4]) ]
 
     , checkInputOutputs "tests/TestFiles/Seq/SeqADT.hs" [ ("conLen", 2000, [Exactly 5])
-                                                        , ("pairExtract", 2000, [Exactly 5]) ]
+                                                        , ("pairExtract", 2000, [AtLeast 6, AtMost 8]) ]
 
     , checkInputOutputsSMTListsWith "tests/TestFiles/Seq/Seq1.hs" "floatListEq" [ ("conFloat", 1000, [Exactly 1])
                                                                                 , ("conDouble", 1000, [Exactly 1])]
--- tests/TestFiles/Seq/SeqADT.hs
+++ tests/TestFiles/Seq/SeqADT.hs
@@ -4,6 +4,8 @@ data Nat = S Nat | Z deriving Eq
 
 data Pair a b = Pair a b deriving Eq
 
+-- CONFIG: --smt-lists --smt-adts Nat,Pair
+
 conLen :: [Pair Nat Int] -> [Pair Nat Int] -> [Pair Nat Int]
 conLen xs ys | length xs > length ys = xs ++ ys
              | length xs > 3, length ys > length xs = ys ++ xs ++ ys
@@ -14,6 +16,7 @@ conLen xs ys | length xs > length ys = xs ++ ys
 pairExtract :: [Pair Nat Int] -> Nat
 pairExtract [Pair Z _] = S Z
 pairExtract [Pair x _] = S (S x)
+pairExtract xs | length xs > 10 = S (S (S Z))
 pairExtract (Pair Z _:_) = S (S (S (S Z)))
 pairExtract (Pair x _:_) = x
 pairExtract [] = S (S Z) 
