d750e99ba988b67e69841fccce4739b242dd6e7a
Bill Hallahan <whallahan@binhamton.edu>
2026-09-12 14:34:29 -0400

    Trying to get dropWhile test to pass on CI

--- tests/G2Plugin/Strings/src/Zeno.hs
+++ tests/G2Plugin/Strings/src/Zeno.hs
@@ -218,7 +218,7 @@ takeWhileSMT p xs =
 --                                     smtFoldLeft (\acc e -> acc && not (p e)) True (smtAt bs 0))
 --   in as'
 
-{-# ANN dropWhile (SMTEquivIsWithConfig "dropWhileSMT" "--smt-timeout 20")
+{-# ANN dropWhile (SMTEquivIsWithConfig "dropWhileSMT" "--smt-timeout 40 --print-smt")
   #-}
 dropWhile :: (Nat -> Bool) -> [Nat] -> [Nat]
 dropWhile _ [] = []
