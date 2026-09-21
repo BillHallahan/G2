b57afa6582f1c1ae83771cd71cebf79c675b4320
Bill Hallahan <whallahan@binhamton.edu>
2026-09-20 22:39:33 -0400

    Minor

--- tests/G2Plugin/Strings/src/Zeno.hs
+++ tests/G2Plugin/Strings/src/Zeno.hs
@@ -299,7 +299,7 @@ insort n (x:xs) =
     True -> n : x : xs
     _ -> x : (insort n xs)
 
-{-# ANN ins (SMTEquivIsWithConfig "insSMT" "--smt cvc5 --smt-timeout 30")
+{-# ANN ins (SMTEquivIsWithConfig "insSMT" "--smt cvc5 --smt-timeout 30 --time 600")
   #-}
 ins :: Nat -> [Nat] -> [Nat]
 ins n [] = [n]
