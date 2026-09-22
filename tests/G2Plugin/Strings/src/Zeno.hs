7058688f0f4593e54deb30ad554c93d989e85470
Bill Hallahan <whallahan@binhamton.edu>
2026-09-22 11:34:08 -0400

    Increasing dropWhile test timeout

--- tests/G2Plugin/Strings/src/Zeno.hs
+++ tests/G2Plugin/Strings/src/Zeno.hs
@@ -218,7 +218,7 @@ takeWhileSMT p xs =
 --                                     smtFoldLeft (\acc e -> acc && not (p e)) True (smtAt bs 0))
 --   in as'
 
-{-# ANN dropWhile (SMTEquivIsWithConfig "dropWhileSMT" "--smt-timeout 40")
+{-# ANN dropWhile (SMTEquivIsWithConfig "dropWhileSMT" "--smt-timeout 50 --time 480")
   #-}
 dropWhile :: (Nat -> Bool) -> [Nat] -> [Nat]
 dropWhile _ [] = []
